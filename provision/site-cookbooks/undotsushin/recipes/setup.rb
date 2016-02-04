61ef4efbd4095a325ac5038230c9d93602cdff30
Yoshiaki Fujimori <info@2244.jp>
2016-02-05 01:39:38 +0900

    vagrant - provision時にpostgre上に db : ut をつくる #85

--- provision/site-cookbooks/undotsushin/recipes/setup.rb
+++ provision/site-cookbooks/undotsushin/recipes/setup.rb
@@ -35,3 +35,22 @@ bash "create-ssl-keys" do
   notifies :restart, "service[apache2]"
 end
 
+
+# postgress
+# ------------------------------
+include_recipe "database::postgresql"
+
+postgresql_database 'ut' do
+  connection(
+    :host     => '127.0.0.1',
+    :port     => 5432,
+    :username => 'postgres',
+    :password => node['postgresql']['password']['postgres']
+  )
+  template 'DEFAULT'
+  encoding 'DEFAULT'
+  tablespace 'DEFAULT'
+  connection_limit '-1'
+  owner 'postgres'
+  action :create
+end
