412949b1b085059d665c546de3c873d33cc63c16
Yoshiaki Fujimori <info@2244.jp>
2016-02-08 20:43:03 +0900

    vagrant - provision時にDBを一旦削除 #85

--- provision/provision-post.sh
+++ provision/provision-post.sh
@@ -3,4 +3,7 @@
 set -ex
 
 # リポジトリルートのut.dumpをVM上のpostgresに読み込む
-cd /vagrant && sudo -u postgres psql -h localhost -U ut -d ut < ut.dump
+#cd /vagrant && sudo -u postgres psql -h localhost -U ut -d ut < ut.dump
+
+# 二重に読み込まないように一度消去する
+cd /vagrant && sudo -u postgres dropdb ut && sudo -u postgres createdb ut && sudo -u postgres psql -h localhost -U ut -d ut < ut.dump
