cdfb1b5d49a21ec891a11d94dc8d93757d73f988
taikiken <denkosekka@gmail.com>
2016-02-09 19:02:46 +0900

    [JS] 進捗

--- _src/babels/src/view/header/ViewHeaderMember.js
+++ _src/babels/src/view/header/ViewHeaderMember.js
@@ -9,3 +9,59 @@
  * This notice shall be included in all copies or substantial portions of the Software.
  *
  */
+'use strict';
+
+import {View} from '../View';
+import {User} from '../../app/User';
+import {UserStatus} from '../../event/UserStatus';
+
+export class ViewHeaderMember extends View {
+  constructor( element:Element, option:Object = {} ) {
+    super( element, option );
+    this._action = new UsersSelf( this.done.bind( this ), this.fail.bind( this ) );
+  }
+  /**
+   * Ajax request を開始します
+   */
+  start():void {
+
+    this.action.start();
+
+  }
+  /**
+   * Ajax response success
+   * @param {Result} result Ajax データ取得が成功しパース済み JSON data を保存した Result instance
+   */
+  done( result:Result ):void {
+
+    let response = result.response;
+
+    if ( typeof response === 'undefined' ) {
+
+      // articles undefined
+      // JSON に問題がある
+      let error = new Error( '[MEMBER:UNDEFINED]サーバーレスポンスに問題が発生しました。' );
+      this.executeSafely( View.UNDEFINED_ERROR, error );
+      // this.showError( error.message );
+
+    } else {
+
+      this.render( response );
+
+    }
+
+  }
+  /**
+   * Ajax response error
+   * @param {Error} error Error instance
+   */
+  fail( error:Error ):void {
+
+    this.executeSafely( View.RESPONSE_ERROR, error );
+    // ここでエラーを表示させるのは bad idea なのでコールバックへエラーが起きたことを伝えるのみにします
+    // this.showError( error.message );
+
+  }
+
+
+}
