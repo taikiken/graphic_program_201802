8714f100254feedc7fe2944cf0ad1bef25d19994
Yoshiaki Fujimori <info@2244.jp>
2016-02-10 16:04:38 +0900

    slim - 記事詳細/コメント/返信コメント #84

---
+++ app/routes/p.router.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+
+/p/:article_id/
+/p/:article_id/comment/:comment_id
+/p/:article_id/comment/:comment_id/:reply_commend_id
+
+*/
+
+$app->group('/p/{article_id:[0-9]+}', function () {
+
+  $this->map(['GET'], '[/]', function ($request, $response, $args) {
+
+    $args['page'] = array(
+      'title'    => 'article - '.$args['article_id'],
+      'template' => 'p.php',
+      'path'     => $args,
+    );
+
+    return $this->renderer->render($response, "default.php", $args);
+
+  });
+
+
+  $this->get('/comment/{commend_id:[0-9]+}[/]', function ($request, $response, $args) {
+
+    $args['page'] = array(
+      'title'    => 'article - '.$args['article_id'].' / commment - '.$args['commend_id'],
+      'template' => 'p.php',
+      'path'     => $args,
+    );
+
+    return $this->renderer->render($response, "default.php", $args);
+
+  });
+
+
+  $this->get('/comment/{commend_id:[0-9]+}/{reply_id:[0-9]+}[/]', function ($request, $response, $args) {
+
+    $args['page'] = array(
+      'title'    => 'article - '.$args['article_id'].' / commment - '.$args['commend_id'].' / '.$args['reply_id'],
+      'template' => 'p.php',
+      'path'     => $args,
+    );
+
+    return $this->renderer->render($response, "default.php", $args);
+
+  });
+
+
+
+});
+
+
+?>
