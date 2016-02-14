b097c71b58ce50e7a76a07bc750ab86673b335a7
Yoshiaki Fujimori <info@2244.jp>
2016-02-14 23:07:12 +0900

    slim - settingsのインデント調整

--- app/config/local.php
+++ app/config/local.php
@@ -1,17 +1,19 @@
 <?php
 return [
-    'settings' => [
-        'displayErrorDetails' => true, // set to false in production
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
+  'settings' => [
+    'displayErrorDetails' => true, // set to false in production
 
-        // Monolog settings
-        'logger' => [
-            'name' => 'undotsuhin.local',
-            'path' => __DIR__ . '/../logs/app.log',
-        ],
+    // Renderer settings
+    'renderer' => [
+      'template_path' => __DIR__ . '/../templates/',
     ],
+
+    // Monolog settings
+    'logger' => [
+      'name' => 'undotsuhin.local',
+      'path' => __DIR__ . '/../logs/app.log',
+    ],
+  ],
+
 ];
