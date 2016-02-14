6e2f240b7d2a10632d76dc47287977f99c173078
Yoshiaki Fujimori <info@2244.jp>
2016-02-14 23:05:59 +0900

    demo - indexに記載していた各ページ一覧を/demo/にうつす

--- app/templates/demo.php
+++ app/templates/demo.php
@@ -6,10 +6,9 @@
         <style>
             body {
                 margin: 50px 0 0 0;
-                padding: 0;
+                padding: 20px;
                 width: 100%;
                 font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-                text-align: center;
                 color: #aaa;
                 font-size: 18px;
             }
@@ -34,8 +33,124 @@
     <body>
         <h1>Slim</h1>
         <div>a microframework for PHP</div>
-
         <p>Try <a href="/SlimFramework">/SlimFramework</a>
+
+<hr />
+
+
+<h2>ホーム</h2>
+
+<ul>
+  <li>
+    <a href="/">ホーム</a>
+  </li>
+  <li>
+    <a href="/">一面 ( ログイン後のホーム) - URLはホームと同じ</a>
+  </li>
+</ul>
+
+
+
+<h2>記事</h2>
+
+
+<ul>
+<?php
+$categories = file_get_contents('http://undotsushin.com/api/v1/category');
+if ( $categories ) :
+  $categories = json_decode($categories, true);
+  foreach( $categories['response']['categories'] as $key => $value ) :
+?>
+  <li>
+    <a href="/category/<?php echo $value['slug']; ?>/">記事一覧 - <?php echo $value['label']; ?></a>
+    <ul>
+      <li>
+        <a href="/category/<?php echo $value['slug']; ?>/ranking/">記事一覧 - <?php echo $value['label']; ?>/ランキング</a>
+      </li>
+      <li>
+        <a href="/category/<?php echo $value['slug']; ?>/video/">記事一覧 - <?php echo $value['label']; ?>/動画</a>
+      </li>
+    </ul>
+  </li>
+<?php
+  endforeach;
+endif;
+?>
+
+</ul>
+
+
+<h2>記事詳細</h2>
+
+<ul>
+  <li>
+    <a href="/p/<?php echo rand(); ?>/">記事詳細</a>
+  </li>
+  <li>
+    <a href="/p/<?php echo rand(); ?>/comment/<?php echo rand(); ?>/">コメント詳細</a>
+  </li>
+  <li>
+    <a href="/p/<?php echo rand(); ?>/comment/<?php echo rand(); ?>/<?php echo rand(); ?>/">返信コメント詳細</a>
+  </li>
+</ul>
+
+
+
+<h2>新規登録・ログイン系</h2>
+
+<ul>
+  <li>
+    <a href="/signup/">新規登録</a>
+  </li>
+  <li>
+    <a href="/signup/account/">新規登録 - 基本情報入力</a>
+  </li>
+  <li>
+    <a href="/signup/interest/">新規登録 - 興味のある競技を選択</a>
+  </li>
+  <li>
+    <a href="/login/">ログイン</a>
+  </li>
+  <li>
+    <a href="/logout/">ログアウト</a>
+  </li>
+  <li>
+    <a href="/reset_password/">パスワードリセット</a>
+  </li>
+  <li>
+    <a href="/reset_password/resetting/">パスワードリセット - 再設定</a>
+  </li>
+</ul>
+
+
+
+<h2>マイページ系</h2>
+
+<ul>
+  <li>
+    <a href="/mypage/">ユーザーマイページ (デフォルトでブックマーク一覧)</a>
+  </li>
+  <li>
+    <a href="/mypage/activities/">アクティビティ</a>
+  </li>
+  <li>
+    <a href="/notifications/">お知らせ</a>
+  </li>
+  <li>
+    <a href="/settings/">プロフィール設定</a>
+  </li>
+  <li>
+    <a href="/settings/interest/">好きな競技を設定</a>
+  </li>
+  <li>
+    <a href="/settings/social/">ソーシャル設定</a>
+  </li>
+  <li>
+    <a href="/settings/deactivate/">退会</a>
+  </li>
+</ul>
+
+<hr />
 
 
         <h2>$request</h2>
