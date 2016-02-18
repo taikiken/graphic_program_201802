6dec17a67915c671aca7c4a9f2f1170581c9f898
saegusamakoto <saegusa@kiito.co.jp>
2016-02-19 08:22:48 +0900

    最終更新日を追加する必要あり

--- public/api/batch_index.php
+++ public/api/batch_index.php
@@ -7,10 +7,11 @@ $o->connect();
 
 $minute=60*24*30;
 
-$sql=sprintf("select id,title,(select name from pm_ where id=m1) as category,body,t1,t10,t11,t12,t13,t14,t15,keyword from repo_n where cid=1 and flag=1",$minute);
+$sql=sprintf("select id,title,(select name from pm_ where id=m1) as category,body,t1,t10,t11,t12,t13,t14,t15,keyword from repo_n where cid=1 and flag=1 order by n desc",$minute);
 $o->query($sql);
 
-$s[]="delete from u_index;";
+$s[]="drop table u_index;";
+$s[]="create table u_index(id int not null primary key,txt text);";
 
 while($f=$o->fetch_array()){
 	$s[]=sprintf("insert into u_index(id,txt) values(%s,'%s');",
