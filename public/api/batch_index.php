61bcfeab9539e11a56cfab44ca13fe97907e8add
saegusamakoto <saegusa@kiito.co.jp>
2016-02-18 21:36:36 +0900

    検索用インデックスバッチ

--- public/api/batch_index.php
+++ public/api/batch_index.php
@@ -7,11 +7,19 @@ $o->connect();
 
 $minute=60*24*30;
 
-$sql=sprintf("insert into u_ranking select t1.id,(case when t2.n>0 then t2.n else 0 end) as n from (select id from repo_n where cid=1) as t1 left join (select pageid as id,count(pageid) as n from (select pageid,regitime from u_bookmark union all select pageid,regitime from u_comment union all select t1.pageid,t2.regitime from u_comment as t1,u_reaction as t2 where t1.id=t2.commentid) as t where regitime > now() - interval '%s minute' group by pageid) as t2 on t1.id=t2.id",$minute);
+$sql=sprintf("select id,title,(select name from pm_ where id=m1) as category,body,t1,t10,t11,t12,t13,t14,t15,keyword from repo_n where cid=1 and flag=1",$minute);
 $o->query($sql);
 
-$sql=sprintf("update u_ranking set n=t.n from (select t1.id,(case when t2.n>0 then t2.n else 0 end) as n from (select id from repo_n where cid=1) as t1 left join (select pageid as id,count(pageid) as n from (select pageid,regitime from u_bookmark union all select pageid,regitime from u_comment union all select t1.pageid,t2.regitime from u_comment as t1,u_reaction as t2 where t1.id=t2.commentid) as t where regitime > now() - interval '%s minute' group by pageid) as t2 on t1.id=t2.id) as t where u_ranking.id=t.id",$minute);
-$o->query($sql);
+$s[]="delete from u_index;";
+
+while($f=$o->fetch_array()){
+	$s[]=sprintf("insert into u_index(id,txt) values(%s,'%s');",
+		$f["id"],
+		esc($f["title"].strip_tags($f["body"]).$f["category"].$f["t1"].$f["t10"].$f["t11"].$f["t12"].$f["t13"].$f["t14"].$f["t15"].$f["keyword"])
+	);
+}
+
+$o->query(implode("\n",$s));
 
 
 ?>
