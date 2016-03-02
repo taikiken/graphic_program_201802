fecd59f320ab076d5b8bf6f7650019058fcb585a
saegusamakoto <saegusa@kiito.co.jp>
2016-03-02 17:13:38 +0900

    生成から24時間以上経過したのパスワードリマインダーハッシュ値削除バッチ

--- public/api/batch_ranking.php
+++ public/api/batch_ranking.php
@@ -5,7 +5,8 @@ include $INCLUDEPATH."local.php";
 $o=new db;
 $o->connect();
 
-$sql="delete from u_view where regitime < now() - interval '3 day' ";
+$sql="delete from u_view where regitime < now() - interval '3 day';\n";
+$sql.="delete from u_reminder where regitime < now() - interval '1 day';";
 $o->query($sql);
 
 ?>
