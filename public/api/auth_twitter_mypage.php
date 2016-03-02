037460197410eb5deb6778e68ee19b4ebbf87491
saegusamakoto <saegusa@kiito.co.jp>
2016-03-02 17:10:47 +0900

    Twitter本番Secret Key変更

--- public/api/auth_twitter_mypage.php
+++ public/api/auth_twitter_mypage.php
@@ -2,9 +2,9 @@
 
 session_start();
 
-define('CONSUMER_KEY','00gnA3emAHJfARBvUf2DIFFXe');
-define('CONSUMER_SECRET','q9X3PYPcCPDYC5rOAfGuBrFtFiZXPwlYcvQ43If2EC2iUebEHd');
-define('OAUTH_CALLBACK','http://dev.undotsushin.com/api/auth_twitter_callback.php');
+define('CONSUMER_KEY','6pZoN3NLkEoNae8VDP0j7MHCK');
+define('CONSUMER_SECRET','PiKgV2QDIY3TnfwSXnVaFP3bqWGHnSWT9qSsN5mJtDR6ZHEMLG');
+define('OAUTH_CALLBACK','http://www.undotsushin.com/api/auth_twitter_callback.php');
 
 require_once 'vendor/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
@@ -20,13 +20,14 @@ $userinfo = $connection->get("account/verify_credentials");
 //(ここらへんは、Twitter の API ドキュメントをうまく使ってください)
 
 //GETしたユーザー情報をvar_dump
-var_dump(array(
-	"token"=>$access_token["oauth_token"],
+$y=array(
+	"token"=>$access_token["oauth_token_secret"],
 	"id"=>$userinfo->id,
 	"name"=>$userinfo->name,
 	"email"=>"",
 	"profile"=>$userinfo->description,
 	"icon"=>$userinfo->profile_image_url
-));
+);
 
 ?>
+<pre><?php print_r(json_encode($y,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)); ?></pre>
