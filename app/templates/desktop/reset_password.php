0ed970033ee59ec7ba682834a7c2f79b4fed46e2
saegusamakoto <saegusa@kiito.co.jp>
2016-03-02 13:19:19 +0900

    パスワードリマインダー

--- app/templates/desktop/reset_password.php
+++ app/templates/desktop/reset_password.php
@@ -9,6 +9,79 @@
 
 
 */
+
+$err="";
+$complete=0;
+
+if(isset($_POST["setting-form-mail"])){
+	
+	$email=trim(htmlspecialchars($_POST["setting-form-mail"]));
+	
+	$o=new db;
+	$o->connect();
+
+	function mailreminder($email,$name,$hash){
+
+		$to=$email;
+		$subject="【運動通信】パスワード再設定のお知らせ";
+		$body=sprintf("%s様
+
+		この度は運動通信 Crazy for sports をご利用いただきまして、誠にありがとうございます。
+		
+		お客様のパスワード再設定用URLをお送りいたしますので、下記URLより再設定をお願いいたします。
+		http://dev.undotsushin.com/reset_password/resetting/?m=%s
+		
+		ご不明な点等ございましたら、以下の運動通信カスタマーセンターまでお問い合わせください。
+		info@undotsushin.com
+		
+		[ご注意]
+		こちらのメールアドレスは送信専用のため、直接返信されても返答できませんので予めご了承ください。",$name,$hash);
+		$from="noreply@undotsushin.com";
+		$reply="info@undotsushin.com";
+
+		return sendmail($to,$subject,preg_replace("/\t/","",$body),$from,$reply);
+		
+	}
+
+	if(strlen($email)==0){
+		$err="メールアドレスは必須項目です。";
+	}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$err="正しいメールアドレスを入力してください。";
+	}else{
+		if($conf==0){
+			$sql=sprintf("select id,title from repo_n where t1='%s' and flag=1",$email);
+			$o->query($sql);
+			$f=$o->fetch_array();
+			
+			if(strlen($f["id"])>0){
+				
+				$hash=md5($email.date("YmdHis"));
+				$e=mailreminder($email,$f["title"],$hash);
+				if($e){
+					$sql=sprintf("insert into u_reminder(userid,hash,regitime) values(%s,'%s',now())",$f["id"],$hash);
+					$o->query($sql);
+					
+					$e=$o->affected_rows2();
+					
+					if($e){
+						$complete=1;
+					}else{
+						$err="データベースへの接続に失敗しました。時間をおいてもう一度お試しください。";
+					}
+					
+				}else{
+					$err="メールを送信できませんでした。時間をおいてもう一度お試しください。";
+				}
+			}else{
+				$err=sprintf("入力されたメールアドレス %s では登録がございません。",$email);
+			}
+		}
+	}
+
+}
+
+if($complete==0){
+
 ?>
   <header class="signup-header">
     <h1 class="heading">運動通信</h1>
@@ -22,13 +95,13 @@
 
       <div class="pw-reset setting-form">
         <p class="mt40">あなたのアカウントに登録されているメールアドレスにパスワード再発行の手順を送信します。下記のフォームにご登録のメールアドレスを入力して送信するボタンをクリックしてください。</p>
-        <form action="/reset_password/complete/" method="post" enctype="application/x-www-form-urlencoded">
+        <form action="/reset_password/" method="post" enctype="application/x-www-form-urlencoded">
           <div class="register-mail setting-form">
             <form action="">
               <fieldset class="fieldset-step-1">
                 <span class="form-parts">
                   <span class="setting-form-mail">
-                    <input type="text" id="setting-form-mail" name="setting-form-mail" placeholder="メールアドレスを入力">
+                    <input type="text" id="setting-form-mail" name="setting-form-mail" placeholder="メールアドレスを入力" value="<?=$email?>">
                   </span>
                 </span>
                 <span className="form-parts align-right">
@@ -39,7 +112,15 @@
               </fieldset>
             </form>
           </div><!-- /.register-mail -->
+  		<?php if($err!=""){ ?>
+  		<p class="mt20"><?=$err?></p>
+        <?php } ?>
         </form>
       </div><!-- /.pw-reset -->
     </div>
   </div><!-- /.body-sec -->
+<?php 
+}else{
+	include str_replace("public","",$_SERVER["DOCUMENT_ROOT"])."/app/templates/desktop/reset_password.complete.php";
+}
+?>
--- app/templates/desktop/reset_password.resetting.php
+++ app/templates/desktop/reset_password.resetting.php
@@ -1,11 +1,68 @@
 <?php
-/*
 
+$o=new db;
+$o->connect();
 
-完了画面URLわけてますが処理的に同一でも問題ないかと思います。
+$err="";
+$err2="";
+$complete=0;
 
+if(isset($_GET["m"])){
+	
+	$hash=preg_replace('/\s/','',$_GET["m"]);
+	
+	$sql=sprintf("select userid,case when regitime > now() - interval '24 hour' then 1 else 0 end as expire from u_reminder where hash='%s';",$hash);
+	$o->query($sql);
+	$f=$o->fetch_array();
+	
+	$userid=$f["userid"];
+	
+	if(strlen($f["userid"])>0){
+		if($f["expire"]==1){
+			
+		}else{
+			$err="パスワードリセットの有効期限が過ぎています。";
+		}
+	}else{
+		$err="指定されたURLは無効です。";
+	}
+	
+}elseif(isset($_POST["setting-form-pw1"])){
+	
+	$userid=$_POST["userid"];
+	$passwd=trim($_POST["setting-form-pw1"]);
+	$err2=check_passwd($passwd);
+	
+	if($err2==""){
+		if($_POST["setting-form-pw1"]==$_POST["setting-form-pw2"]){
+			
+			include "conf/config.php";
+			
+			$sql=sprintf("select t1 from repo_n where id=%s",$userid);
+			$o->query($sql);
+			$f=$o->fetch_array();
+			
+			$pass1=md5($f["t1"].$passwd);
+			$pass2=md5($MAGIC_STRING.$passwd);
+			
+			$sql=sprintf("update repo_n set a15='%s',passwd='%s' where id=%s",$pass1,$pass2,$userid);
+			$o->query($sql);
+			$e=$o->affected_rows2();
+			
+			if($e){
+				$complete=1;
+			}else{
+				$err2="データベースへの接続に失敗しました。時間をおいてもう一度お試しください。";
+			}
+			
+		}else{
+			$err2="パスワードが一致しません。";
+		}
+	}
+}
 
-*/
+if($complete==0){
+
 ?>
   <header class="signup-header">
     <h1 class="heading">運動通信</h1>
@@ -16,20 +73,21 @@
       <div class="signup-heading">
         <h1>パスワードをリセットする</h1>
       </div><!-- /.signup-heading -->
-
       <div class="pw-reset setting-form">
-        <p class="mt40">新しいパスワードを入力してください。</p>
-        <form action="/reset_password/resetting/complete/" method="post" enctype="application/x-www-form-urlencoded">
+<?php if($err==""){ ?>
+        <p class="mt40"><?php echo $err2==""?"新しいパスワードを入力してください。":$err2; ?></p>
+        <form action="/reset_password/resetting/" method="post" enctype="application/x-www-form-urlencoded">
           <div class="setting-form">
             <form action="">
               <span class="form-parts">
                 <span class="setting-form-pw">
-                  <input type="password" id="setting-form-pw1" name="setting-form-pw1" placeholder="新しいパスワードを入力 (半角英数8文字以上)">
+                  <input type="password" id="setting-form-pw1" name="setting-form-pw1" placeholder="新しいパスワードを入力 (半角英数8文字以上)" value="<?=htmlspecialchars(trim($_POST["setting-form-pw1"]))?>">
                 </span>
               </span>
               <span class="form-parts">
                 <span class="setting-form-pw">
-                  <input type="password" id="setting-form-pw2" name="setting-form-pw2" placeholder="再度パスワードを入力">
+                  <input type="password" id="setting-form-pw2" name="setting-form-pw2" placeholder="再度パスワードを入力" value="<?=htmlspecialchars(trim($_POST["setting-form-pw2"]))?>">
+          		  <input type="hidden" name="userid" value="<?=$userid?>">
                 </span>
               </span>
               <div class="form-parts form-submit-parts">
@@ -40,6 +98,16 @@
             </form>
           </div><!-- /.setting-form -->
         </form>
-      </div><!-- /.pw-reset -->
+<?php }else{ ?>
+		<p class="mt40"><?=$err?></p>
+<?php } ?>
+        </div><!-- /.pw-reset -->
     </div>
   </div><!-- /.body-sec -->
+<?php
+
+}else{
+	include include str_replace("public","",$_SERVER["DOCUMENT_ROOT"])."/app/templates/desktop/reset_password.resetting.complete.php";
+}
+
+?>
