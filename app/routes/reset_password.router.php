c843f0e5b8eefe24907b0268803ef0ef306dc2dc
Yoshiaki Fujimori <info@2244.jp>
2016-03-13 19:00:40 +0900

    パスワードリセットの完了画面が表示されない問題対応 #187

--- app/routes/reset_password.router.php
+++ app/routes/reset_password.router.php
@@ -4,7 +4,7 @@ $app->group('/reset_password', function () use ($app) {
 
   // パスワードリセット - /reset_password/
   // ==============================
-  $this->map(['GET','POST'], '[/]', function ($request, $response, $args) use ($app) {
+  $this->any('[/]', function ($request, $response, $args) use ($app) {
 
     $args['page'] = $app->model->set(array(
       'title'    => 'パスワードをリセットする',
@@ -18,25 +18,25 @@ $app->group('/reset_password', function () use ($app) {
   });
 
 
-  // パスワードリセット 送信完了 - /reset_password/complete/
-  // ==============================
-  $this->post('/{slug:complete}[/]', function ($request, $response, $args) use ($app) {
+  // // パスワードリセット 送信完了 - /reset_password/complete/
+  // // ==============================
+  // $this->post('/{slug:complete}[/]', function ($request, $response, $args) use ($app) {
 
-    $args['page'] = $app->model->set(array(
-      'title'    => 'パスワードをリセットする',
-      'template' => 'reset_password.complete',
-      'path'     => $args,
-      'template_classname' => 'signup',
-    ));
+  //   $args['page'] = $app->model->set(array(
+  //     'title'    => 'パスワードをリセットする',
+  //     'template' => 'reset_password.complete',
+  //     'path'     => $args,
+  //     'template_classname' => 'signup',
+  //   ));
 
-    return $this->renderer->render($response, "default.php", $args);
+  //   return $this->renderer->render($response, "default.php", $args);
 
-  });
+  // });
 
 
   // パスワードリセット/再設定 - /reset_password/resetting/
   // ==============================
-  $this->get('/{slug:resetting}[/]', function ($request, $response, $args) use ($app)  {
+  $this->any('/{slug:resetting}[/]', function ($request, $response, $args) use ($app)  {
 
     $args['page'] = $app->model->set(array(
       'title'    => '再設定 | パスワードをリセットする',
@@ -52,18 +52,18 @@ $app->group('/reset_password', function () use ($app) {
 
   // パスワードリセット/再設定完了 - /reset_password/resetting/complete/
   // ==============================
-  $this->post('/{slug:resetting/complete}[/]', function ($request, $response, $args) use ($app)  {
+  // $this->post('/{slug:resetting/complete}[/]', function ($request, $response, $args) use ($app)  {
 
-    $args['page'] = $app->model->set(array(
-      'title'    => '再設定 | パスワードをリセットする',
-      'template' => 'reset_password.resetting.complete',
-      'path'     => $args,
-      'template_classname' => 'signup',
-    ));
+  //   $args['page'] = $app->model->set(array(
+  //     'title'    => '再設定 | パスワードをリセットする',
+  //     'template' => 'reset_password.resetting.complete',
+  //     'path'     => $args,
+  //     'template_classname' => 'signup',
+  //   ));
 
-    return $this->renderer->render($response, "default.php", $args);
+  //   return $this->renderer->render($response, "default.php", $args);
 
-  });
+  // });
 
 
 });
