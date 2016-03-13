b23c30f2443ac2177dd372111bbb5ccbeeffd7cc
Yoshiaki Fujimori <info@2244.jp>
2016-03-13 19:42:03 +0900

    スマホ版のパスワードリセット組み込み #187

---
+++ app/templates/mobile/reset_password.resetting.complete.php
@@ -0,0 +1,21 @@
+<?php
+// --------------------------------------------------------
+//  パスワードをリセットする
+// --------------------------------------------------------
+?>
+<header class="signup-header">
+  <h1 class="heading">運動通信</h1>
+</header><!-- /.signup-header -->
+
+<div class="body-sec">
+  <div class="body-sec-inner">
+    <div class="signup-heading">
+      <h1>パスワードをリセットする</h1>
+    </div><!-- /.signup-heading -->
+
+    <div class="pw-reset setting-form">
+      <p class="lead">新しいパスワードを設定しました。</p>
+      <div class="mod-btnB01 mt30"><a href="/">ホーム</a></div>
+    </div><!-- /.pw-reset -->
+  </div>
+</div><!-- /.body-sec -->
