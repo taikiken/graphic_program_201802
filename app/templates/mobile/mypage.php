a203a7fc279166b59c0c50ff1a33b904e65eb630
taikiken <denkosekka@gmail.com>
2016-03-15 12:59:30 +0900

    my page

--- app/templates/mobile/mypage.php
+++ app/templates/mobile/mypage.php
@@ -1,5 +1,32 @@
-<h1>
-  <?php echo $page['title']; ?>
-</h1>
+<?php
+// SP ユーザーページのブックマーク一覧
+?>
+<div class="body-sec">
+  <div class="body-sec-inner">
 
-<?php var_dump($page); ?>
+    <div class="category-heading">
+      <h1>マイページ</h1>
+      <a class="user-btn-setting" href="/setting/">設定</a>
+    </div><!-- /.category-heading -->
+
+    <div class="user-config">
+      <div id="mypage-profile-container-extend"></div>
+
+      <div class="user-config-control">
+        <nav class="user-config-nav">
+          <a class="user-config-nav-bookmark current" href="/mypage/"><span>ブックマーク</span></a>
+          <a class="user-config-nav-activity" href="/mypage/activities/"><span>アクティビティ</span></a>
+        </nav>
+      </div>
+    </div><!-- /.user-config -->
+
+    <section class="main-sec mypage mypage-bookmark">
+
+      <div id="board-container"></div><!--/archive-->
+
+      <div id="board-container-more"></div><!--/archive-more-->
+
+    </section><!-- /.main-sec -->
+
+  </div>
+</div><!-- /.body-sec -->
