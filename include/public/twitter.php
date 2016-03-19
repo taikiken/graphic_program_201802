ffab84244fc3ac28decd91be7b324ea57a2ed3bf
saegusamakoto <saegusa@kiito.co.jp>
2016-03-20 04:46:59 +0900

    no message

--- include/public/twitter.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('CONSUMER_KEY','6pZoN3NLkEoNae8VDP0j7MHCK');
-define('CONSUMER_SECRET','PiKgV2QDIY3TnfwSXnVaFP3bqWGHnSWT9qSsN5mJtDR6ZHEMLG');
-define('OAUTH_CALLBACK','https://www.undotsushin.com/api/auth_twitter_callback.php');
-
-?>
