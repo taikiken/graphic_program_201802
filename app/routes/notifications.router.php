c2f8483ffc131fc13489e5f0295921bf2a110b41
Yoshiaki Fujimori <info@2244.jp>
2016-03-24 05:43:03 +0900

    要ログインへの非ログインアクセスはログインページに転送  #460

--- app/routes/notifications.router.php
+++ app/routes/notifications.router.php
@@ -4,6 +4,8 @@
 // ==============================
 $app->get('/notifications[/]', function ($request, $response, $args) use ($app) {
 
+  $app->user_helper->check_logged_in();
+
   $args['page'] = $app->model->set(array(
     'title'    => 'お知らせ',
     'template' => 'notifications',
