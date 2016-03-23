ff743ebd1e0f546376585ed3d034d6ab8cbf5d3e
Kakoi Hiroki <k@parachute-canopy.org>
2016-03-23 19:43:54 +0900

    新規登録 / ログインの`Twitter` / `Facebook` の表記修正

--- app/templates/mobile/login.php
+++ app/templates/mobile/login.php
@@ -15,8 +15,8 @@
     <div class="register">
       <div class="linkage-sns">
         <ul class="linkage-sns-list">
-          <li class="linkage-sns-item"><a class="linkage-sns-link linkage-sns-tw" href="/api/v1/auth/twitter"><span>twitterでログイン</span></a></li>
-          <li class="linkage-sns-item"><a class="linkage-sns-link linkage-sns-fb" href="/api/v1/auth/facebook"><span>facebookでログイン</span></a></li>
+          <li class="linkage-sns-item"><a class="linkage-sns-link linkage-sns-tw" href="/api/v1/auth/twitter"><span>Twitterでログイン</span></a></li>
+          <li class="linkage-sns-item"><a class="linkage-sns-link linkage-sns-fb" href="/api/v1/auth/facebook"><span>Facebookでログイン</span></a></li>
         </ul>
       </div><!-- /.linkage-sns -->
 
