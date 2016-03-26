202f5cbc28ecb8dad29129298fe10d296eec5370
Hiroaki Okubo <denkosekka@gmail.com>
2016-03-26 23:25:06 +0900

    ESDocs 加筆

--- _src/babels/src/action/Offset.js
+++ _src/babels/src/action/Offset.js
@@ -49,6 +49,7 @@ export class Offset extends Action {
   //  GETTER / SETTER
   // ---------------------------------------------------
   /**
+   * 件数
    * @return {Number|*} total件数を返します
    */
   get total():Number {
@@ -62,6 +63,7 @@ export class Offset extends Action {
     this._total = total;
   }
   /**
+   * 取得件数
    * @return {Number|*} lengths 取得件数を返します
    */
   get length():Number {
@@ -75,6 +77,7 @@ export class Offset extends Action {
     this._length = length;
   }
   /**
+   * 取得開始位置
    * @return {Number|*} offset 取得開始位置を返します
    */
   get offset():Number {
@@ -118,7 +121,7 @@ export class Offset extends Action {
 
   }
   /**
-   * 残り数
+   * 残り数, total から 次の offset を」引いた数
    * @return {Number} total から 次の offset を」引いた数を返します
    */
   rest():Number {
