494381b98aff8d3fc09f8107299954c6124306be
taikiken <denkosekka@gmail.com>
2016-05-21 18:43:35 +0900

    検証コード

---
+++ _src/babels/src/ui/Brightcove.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2011-2016 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2016/05/21 - 17:09
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ *
+ */
+
+export class Brightcove {
+  constrctor() {
+    
+  }
+  
+  dispose() {
+    
+  }
+}
