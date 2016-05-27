34f10dd6dae34ac1228b91a223621f1ab286e682
Yoshiaki Fujimori <info@2244.jp>
2016-05-27 17:31:23 +0900

    api/v1/category/[:category_slug] でひとまず何かしら返す #642

--- public/api/ver1/read/dummy-category.php
+++ public/api/ver1/read/dummy-category.php
@@ -2,7 +2,8 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-echo <<<__EOL__
+
+$json_crazy = <<<__EOL__
 
 {
   "status": {
@@ -20,7 +21,7 @@ echo <<<__EOL__
     "title"       : "CRAZY ATHLETES",
     "description" : "自らの人生を、その「一瞬」にかけるスポーツアスリート達。多くの犠牲、忍耐と引き換えに彼らが起こす「奇跡」、彼らが我々に提供してくれる「感動」が、人々をスポーツに熱狂させる根源である。",
     "theme"        : {
-      "base"             : "black",
+      "base"             : "dark",
       "background-color" : "#000000",
       "images"      : {
         "pc" : "https://www.undotsushin.com/img/crazy/crazy-pc-index.png",
@@ -52,5 +53,87 @@ echo <<<__EOL__
 }
 
 __EOL__;
+
+
+$json_other = <<<__EOL__
+
+{
+  "status": {
+      "code": 200,
+      "user_message": "",
+      "developer_message": ""
+  },
+  "response": {
+    "id"          : 0,
+    "label"       : "",
+    "slug"        : "",
+    "url"         : "",
+    "title_img"   : "",
+
+    "title"       : "",
+    "description" : "",
+    "theme"        : {
+      "base"             : "",
+      "background-color" : "#eeeeee",
+      "images"      : {
+        "pc" : "",
+        "sp" : ""
+      }
+    },
+    "banner"      : {
+      "pc" : {
+        "text"  : "",
+        "image" : "",
+        "link"  : ""
+      },
+      "sp" : {
+        "text"  : "",
+        "image" : "",
+        "link"  : ""
+      }
+    },
+    "ad" : {
+      "ios"     : "33504",
+      "android" : "34424",
+      "sp"      : "35244",
+      "pc"      : {
+        "sidebar-top"    : "",
+        "sidebar-bottom" : ""
+      }
+    }
+  }
+}
+
+__EOL__;
+
+
+if ( isset($_REQUEST['category']) && $_REQUEST['category'] ) :
+
+  $category = $_REQUEST['category'];
+
+else :
+
+  exit;
+
+endif;
+
+
+
+if ( $category == 'crazy' ) :
+
+  $array = json_decode($json_crazy);
+
+else :
+
+  $array                 = json_decode($json_other);
+  $array->response->slug = $category;
+  $array->response->url  = "http://dev.undotsushin.com/category/{$category}/";
+
+endif;
+
+
+echo json_encode($array,JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+
+
 
 ?>
