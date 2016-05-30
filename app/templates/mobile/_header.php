8a708c76bd9e5eff6cd2216352386430a53f2c25
taikiken <denkosekka@gmail.com>
2016-05-30 16:47:21 +0900

    mobile main menu: crazy 表示

--- app/templates/mobile/_header.php
+++ app/templates/mobile/_header.php
@@ -170,10 +170,14 @@ if ( $page_has_header ) :
     <div id="gnav-sec-inner" class="gnav-sec-inner">
       <ul id="gnav-sec-list">
         <li id="home" class="gnav-home"><a href="/">一面</a></li>
-        <?php if (0) : ?>
-          <li id="crazy" class="gnav-crazy"><a href="/crazy/"><img src="/assets/images/common/gnav-crazy.png" alt="CRAZY"></a></li>
-        <?php endif;// crazy remove ?>
-        <?php foreach( $page[ 'site_categories' ] as $category ) { ?>
+
+          <li id="crazy" class="gnav-crazy"><a href="/category/crazy/">CRAZY</a></li>
+
+        <?php foreach( $page[ 'site_categories' ] as $category ) {
+          if ( $category[ 'slug' ] == 'crazy' ) {
+            continue;
+          }
+          ?>
           <li id="<?php echo $category[ 'slug' ]; ?>" class="gnav-<?php echo $category[ 'slug' ]; ?>">
             <a href="/category/<?php echo $category[ 'slug' ]; ?>/"><?php echo $category[ 'label' ]; ?></a>
           </li>
