e810eee2183c4d7416f607bc8323e923949a830c
taikiken <denkosekka@gmail.com>
2016-05-31 13:49:32 +0900

    記事一覧 SP banner

--- app/templates/mobile/category.php
+++ app/templates/mobile/category.php
@@ -7,7 +7,7 @@
   <div class="body-sec-inner">
     <?php
     // ----------------------------------------------------
-    // 記事一覧: pc
+    // 記事一覧: sp theme.images
     if ( !empty($page['category']) && !empty($page['category']['theme']) && !empty($page['category']['theme']['images']) && !empty($page['category']['theme']['images']['sp']) ) :
       ?>
       <div class="special-summary" style="<?php echo $page['category']['theme']['background_color'] ? 'background-color: ' . $page[ 'category' ][ 'theme' ][ 'background_color' ] : ''; ?>">
@@ -15,9 +15,21 @@
       </div>
       <?php
     endif;
-    // eof: 記事一覧: pc
+    // eof: 記事一覧: sp theme.images
     // ---------------------------------------------------- ?>
     <section class="main-sec">
+      <?php
+      // ----------------------------------------------------
+      // 記事一覧: sp banner
+      if ( !empty($page['category']) && !empty($page['category']['banner']) && !empty($page['category']['banner']['sp']) && !empty($page['category']['banner']['sp']['image']) && !empty($page['category']['banner']['sp']['link']) ) :
+        ?>
+        <div class="sponsor-link">
+          <a href="<?php echo $page['category']['banner']['sp']['link']; ?>" target="_blank"><img src="<?php echo $page['category']['banner']['sp']['image']; ?>" alt="<?php echo $page['category']['banner']['sp']['text'] ? $page['category']['banner']['sp']['text'] : '' ?>"></a>
+        </div>
+        <?php
+      endif;
+      // eof: 記事一覧: sp banner
+      // ---------------------------------------------------- ?>
       <div id="category-container"></div>
     </section><!-- /.main-sec -->
   </div>
