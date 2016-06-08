cc2808a939400d4a96065aba6a7ae61ea32f921f
Yoshiaki Fujimori <info@2244.jp>
2016-06-08 14:26:19 +0900

    divタグ閉じ漏れ対応

--- app/templates/desktop/settings.php
+++ app/templates/desktop/settings.php
@@ -5,8 +5,8 @@
 <div class="body-sec">
   <div class="body-sec-inner setting-bg">
     <section class="main-sec">
-
       <div class="setting-sec">
+
         <div class="setting-side">
           <nav class="setting-side-nav">
             <ul class="setting-side-nav-list">
@@ -28,6 +28,7 @@
 
         </div><!-- /.setting-main -->
 
+      </div>
     </section><!-- /.main-sec -->
 
     <section class="side-sec">
