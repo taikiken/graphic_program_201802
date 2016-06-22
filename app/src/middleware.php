3629e5646c60ec1882b1c2fc746723d6bc25bb48
Yoshiaki Fujimori <info@2244.jp>
2016-06-22 13:48:24 +0900

    トレイリングスラッシュで統一するパターン #852

--- app/src/middleware.php
+++ app/src/middleware.php
@@ -2,3 +2,17 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app->add(function (Request $request, Response $response, callable $next) {
+    $uri = $request->getUri();
+    $path = $uri->getPath();
+    if ($path != '/' && substr($path, -1) !== '/') {
+        $uri = $uri->withPath( $path.'/' );
+        return $response->withRedirect((string)$uri, 301);
+    }
+
+    return $next($request, $response);
+});
