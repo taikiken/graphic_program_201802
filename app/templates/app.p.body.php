662b60bfa4150770fcddb493d8a1c3771e5e03d0
Yoshiaki Fujimori <info@2244.jp>
2016-08-12 10:26:37 +0900

    webview - 本文のみ返す処理追加 #1014

---
+++ app/templates/app.p.body.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+  <title><?php echo $page['title']; ?></title>
+  <link rel="stylesheet" href="/assets/sp/css/ui.css?v=<?php echo $page['version']; ?>">
+</head>
+<body>
+<div id="page" class="whole post-single ">
+<div class="body-sec" style="margin: 0;">
+  <div class="body-sec-inner">
+    <div class="main-sec">
+
+      <div class="post-detail">
+
+        <?php if ( $page['post']['is_readmore'] ) :
+          // -------------------------- [記事本文] --------------------------
+          ?>
+          <div id="post-content-container" class="post-content">
+            <p><?php echo $page['post']['description']; ?></p>
+            <p><a id="readMore-external" class="post-content-btn-readMore" href="<?php echo $page['post']['readmore']['url']; ?>" onclick="ga('send', 'event', 'external_link', 'click', '<?php echo $page['post']['readmore']['url']; ?>', 0, {nonInteraction: true});">続きを読む(外部サイトへ)</a></p>
+          </div>
+        <?php else : ?>
+          <div id="post-content-container" class="post-content">
+            <?php print_r($page['post']['body']); ?>
+          </div><!-- /.post-content -->
+        <?php
+        // -------------------------- [/記事本文] --------------------------
+        endif; ?>
+        <?php
+        // ----------------------------------------------------
+        // 記事詳細: pc 媒体ロゴ
+        if ( !empty( $page['post'] ) && !empty( $page['post']['user'] ) ) :
+
+          $is_post_usr_logo = !empty( $page['post']['user']['logo'] );
+
+          $post_user_logo_link = '';
+          if ( $is_post_usr_logo && !empty( $page['post']['user']['logo']['link'] ) ) {
+            $post_user_logo_link = $page['post']['user']['logo']['link'];
+          }
+          ?>
+          <div class="provider mt30">
+            <?php
+            // user.logo.image
+            if ( $is_post_usr_logo && !empty( $page['post']['user']['logo']['img'] ) ) :
+              if ( empty($post_user_logo_link) ) :
+                // link が存在しないので画像だけ表示します ?>
+                <i class="provider-logo"><img src="<?php echo $page['post']['user']['logo']['img']; ?>" alt=""></i>
+              <?php else: // link + image を表示 ?>
+                <a href="<?php echo $post_user_logo_link; ?>" onclick="ga('send', 'event', 'provider_link', 'click', '<?php echo $post_user_logo_link; ?>', 0, {nonInteraction: true});"><i class="provider-logo"><img src="<?php echo $page['post']['user']['logo']['img']; ?>" alt=""></i></a>
+              <?php endif; ?>
+            <?php endif; //----[image] ?>
+            <div class="provider-data">
+              <?php
+              // user.name
+              if ( !empty($page['post']['user']['name']) ) : ?>
+                <p class="provider-name"><?php echo $page['post']['user']['name']; ?></p>
+              <?php endif; //----[name]
+
+              // user.logo.link
+              // link が存在する時のみ表示します
+              if ( !empty( $page['post']['user']['logo'] ) && !empty( $page['post']['user']['logo']['link'] ) ) : ?>
+                <p class="provider-url"><a href="<?php echo $page['post']['user']['logo']['link']; ?>" onclick="ga('send', 'event', 'provider_link', 'click', '<?php echo $page['post']['user']['logo']['link']; ?>', 0, {nonInteraction: true});">ウェブサイト</a></p>
+              <?php endif; //----[link] ?>
+            </div>
+          </div><!-- /.provider -->
+        <?php endif;
+        // eof: 記事詳細: pc 媒体ロゴ
+        // ---------------------------------------------------- ?>
+      </div><?php //.post-detail ?>
+
+    </div><?php //.main-sec ?>
+  </div><?php //.body-sec-inner ?>
+</div><?php //.body-sec ?>
+</div><?php //#page ?>
+
+
+</body>
+</html>
