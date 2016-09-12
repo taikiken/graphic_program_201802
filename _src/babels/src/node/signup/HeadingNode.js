a4f9d778c03f5d5777214a5163ae0c52c4f26dfd
taikiken <o@parachute-canopy.org>
2016-09-12 19:48:58 +0900

    会員登録 lead 文 PC: 復活

--- _src/babels/src/node/signup/HeadingNode.js
+++ _src/babels/src/node/signup/HeadingNode.js
@@ -10,42 +10,46 @@
  *
  */
 
+// event
 import {SignupStatus} from '../../event/SignupStatus';
 
+// app
 import {Url} from '../../app/const/Url';
 
 // React
-let React = self.React;
+const React = self.React;
+
+// Sagen
+const Sagen = self.Sagen;
 
 // lead 文
 // step 1 だけ表示
-// /**
-//  * <p>ユーザー登録ウイザード</p>
-//  * <p>lead 文,  step 1 だけ表示</p>
-//  * @private
-//  * @type {ReactClass}
-//  */
-// let LeadNode = React.createClass( {
-//   propTypes: {
-//     step: React.PropTypes.number.isRequired
-//   },
-//   render: function() {
-//
-//     if ( this.props.step === 1 ) {
-//       return (
-//         <p className="lead">
-//           会員登録は無料です。<br />
-//           あなたの興味のあるニュースをお届けします。
-//         </p>
-//       );
-//     } else {
-//
-//       return null;
-//
-//     }
-//
-//   }
-// } );
+/**
+ * <p>ユーザー登録ウイザード</p>
+ * <p>lead 文,  step 1 だけ表示</p>
+ * @private
+ * @type {ReactClass}
+ */
+let LeadNode = React.createClass( {
+  propTypes: {
+    step: React.PropTypes.number.isRequired
+  },
+  render: function() {
+    // sp なし
+    // @since 2016-09-12
+    if (this.props.step === 1 || !Sagen.Browser.Mobile.phone()) {
+      return (
+        <p className="lead">
+          会員登録は無料です。<br />
+          あなたの興味のあるニュースをお届けします。
+        </p>
+      );
+    } else {
+      // それ以外は空にする
+      return null;
+    }
+  }
+} );
 /**
  * <p>ユーザー登録ウイザード</p>
  * <p>signup page 内 title<br>
@@ -73,20 +77,22 @@ export let HeadingNode = React.createClass( {
   },
   render: function() {
 
-    // return (
-    //   <header className="signup-header">
-    //     <h1 className="heading"><a href={Url.index()}>SPORTS BULL</a></h1>
-    //     <LeadNode step={this.state.step} />
-    //   </header>
-    // );
-    // .signup-header 内の p.lead はトルツメ
-    // https://paper.dropbox.com/doc/SPORTS-BULL-DRIEolzKHNECwXGwZDYM2
-    // @since 2016-09-09
     return (
       <header className="signup-header">
         <h1 className="heading"><a href={Url.index()}>SPORTS BULL</a></h1>
+        <LeadNode step={this.state.step} />
       </header>
     );
+    // @since 2016-09-12
+    // トルツメをキャンセル
+    // // .signup-header 内の p.lead はトルツメ
+    // // https://paper.dropbox.com/doc/SPORTS-BULL-DRIEolzKHNECwXGwZDYM2
+    // // @since 2016-09-09
+    // return (
+    //   <header className="signup-header">
+    //     <h1 className="heading"><a href={Url.index()}>SPORTS BULL</a></h1>
+    //   </header>
+    // );
   },
   componentDidMount: function() {
     this.status.on( SignupStatus.SIGNUP_STEP, this.stepChange );
