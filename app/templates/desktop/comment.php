3fd67a1b11b043ad19188152b500f09cedeb5fd9
Yoshiaki Fujimori <info@2244.jp>
2016-09-23 17:07:54 +0900

    category-headingの内容、一覧、詳細、コメント詳細で共通ぽいので切り出して共通化 #1010

--- app/templates/desktop/comment.php
+++ app/templates/desktop/comment.php
@@ -1,28 +1,8 @@
-<?Php if ( !$page['theme']['images']['pc'] ) : ?>
-<div class="category-heading">
-  <h1>
-    <?php echo $page['category']['label']; ?>
-    <?php if ( isset($page['category']['title_img']) && $page['category']['title_img'] ) : ?>
-      <span class="category-heading-image">
-        <?php
-        // @since 2016-09-17
-        $title_img_link = false;
-        if (isset($page['category']['title_img_link']) && $page['category']['title_img_link']) :
-          $title_img_link = true;
-        ?>
-          <a href="<?php echo $page['category']['title_img_link']; ?>" target="_blank">
-        <?php endif; ?>
-        <img src="<?php echo $page['category']['title_img']; ?>" alt="" />
-        <?php
-        if ($title_img_link) :
-        ?>
-          </a>
-        <?php endif; ?>
-      </span>
-    <?php endif; ?>
-  </h1>
-</div><!-- /.category-heading -->
-<?php endif; ?>
+<?php
+
+include_once __DIR__.'/_category-heading.php';
+
+?>
 
 <div class="body-sec">
   <div class="body-sec-inner">
--- app/templates/desktop/p.php
+++ app/templates/desktop/p.php
@@ -1,36 +1,8 @@
 <?php
-// ----------------------------------------------------
-// PC版で冒頭画像ある場合のカテゴリータイトル表示を省略する
-// https://github.com/undotsushin/undotsushin/issues/645#issuecomment-223527274
-if ( !$page['theme']['images']['pc'] ) :
+
+include_once __DIR__.'/_category-heading.php';
+
 ?>
-<div class="category-heading">
-  <h1>
-    <?php echo $page['category']['label']; ?>
-    <?php if ( isset($page['category']['title_img']) && $page['category']['title_img'] ) : ?>
-      <span class="category-heading-image">
-        <?php
-        // @since 2016-09-17
-        $title_img_link = false;
-        if (isset($page['category']['title_img_link']) && $page['category']['title_img_link']) :
-          $title_img_link = true;
-        ?>
-          <a href="<?php echo $page['category']['title_img_link']; ?>" target="_blank">
-        <?php endif; ?>
-        <img src="<?php echo $page['category']['title_img']; ?>" alt="" />
-        <?php
-        if ($title_img_link) :
-        ?>
-          </a>
-        <?php endif; ?>
-      </span>
-    <?php endif; ?>
-  </h1>
-</div><!-- /.category-heading -->
-<?php
-endif;
-// eof: PC版で冒頭画像ある場合のカテゴリータイトル表示を省略する
-// ---------------------------------------------------- ?>
 
 <div class="body-sec">
   <div class="body-sec-inner">
