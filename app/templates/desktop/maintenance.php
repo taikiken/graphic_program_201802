2585fa9e46c1df7c902f9c81cc8abe4ab898d01c
k@parachute-canopy.org <Parachute@SWAN.local>
2016-10-05 00:45:06 +0900

    #1145 SPORTS BULL版 メンテナンス画面

--- app/templates/desktop/maintenance.php
+++ app/templates/desktop/maintenance.php
@@ -4,16 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <script src="/assets/js/libs/sagen/sagen.min.js" id="sagen" data-browser="true"></script>
-  <title>メンテナンス中です | 運動通信</title>
+  <title>メンテナンス中です | スポーツブル / SPORTS BULL</title>
   <link rel="stylesheet" href="/assets/css/ui.css">
 </head>
 <body>
 <div class="whole maintenance">
   <div class="body-sec">
     <div class="body-sec-inner">
-      <figure class="maintenance-logo"><img src="/assets/images/maintenance/logo.png" alt="運動通信 / CRAZY FOR SPORTS"></figure>
       <div class="maintenance-text">
-        <h1 class="maintenance-heading" style="color:#e1438b;">ただいまメンテナンス中です</h1>
+        <p class="maintenance-logo"><img src="/assets/images/maintenance/logo.png" alt="スポーツブル / SPORTS BULL"></p>
+        <h1 class="maintenance-heading">ただいまメンテナンス中です</h1>
         <div class="maintenance-message">
           <p>
             ご迷惑をお掛けしますが、<br />
@@ -28,9 +28,10 @@
           ?>
         </div>
       </div><!-- /.maintenance-text -->
+      <figure class="maintenance-mascot"><img src="/assets/images/maintenance/bull.png" alt=""></figure>
     </div>
 
-    <p class="copyright">Copyright &copy; UNDO TSUSHIN inc. All rights reserved.</p>
+    <p class="copyright">Copyright &copy; SPORTS BULL All rights reserved.</p>
   </div><!-- /.body-sec -->
 </div><!-- /.whole -->
 </body>
