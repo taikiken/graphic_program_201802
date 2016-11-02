d2b5b423c496040c3ea2d0085d5249c152be2f2f
taikiken <o@parachute-canopy.org>
2016-11-02 21:09:57 +0900

    出力JSONキーを `title_banner` へ変更 #1211

--- app/templates/mobile/_category-heading.php
+++ app/templates/mobile/_category-heading.php
@@ -1,11 +1,12 @@
 <?php
 // ----------------------------------------------------
-// SP版 Powerd by エリアの追加
+// SP版 Powered by エリアの追加
 // https://github.com/undotsushin/undotsushin/issues/1211
+/*
 if ( !$page['theme']['images']['sp'] ) :
 ?>
 <?php if ( isset($page['category']['title_img']) && $page['category']['title_img'] ) : ?>
-<div class="title_banner-powerdby">
+<div class="title_banner-powered_by">
   <?php
   // @since 2016-09-17
   $title_img_link = false;
@@ -24,5 +25,37 @@ if ( !$page['theme']['images']['sp'] ) :
 <?php endif; ?>
 <?php
 endif;
-// eof: SP版 Powerd by エリアの追加
+*/
+// eof: SP版 Powered by エリアの追加
+// ---------------------------------------------------- ?>
+<?php
+// @since 2016-11-02
+// JSON key 変更に伴い変更します
+// https://github.com/undotsushin/undotsushin/issues/1211#issuecomment-255299583
+// https://docs.google.com/spreadsheets/d/1Vngb6I2khKtkFBezsvUy0Fc1ZofYkHDJMgD0aTIYkHw/edit#gid=848283478
+$title_banner = $page['category']['title_banner']['sp'];
+if (!empty($title_banner['image'])) :
+  $is_title_banner_link = false;
+?>
+<div class="title_banner-powered_by">
+  <?php
+  if (!empty($title_banner['link'])) :
+  $title_banner_link = true;
+  ?>
+    <a href="<?php echo $title_banner['link']; ?>" target="_blank">
+  <?php
+  endif;
+  ?>
+    <img src="<?php echo $title_banner['image']; ?>" alt="<?php echo $title_banner['text']; ?>">
+  <?php
+  if ($title_banner_link) :
+  ?>
+    </a>
+  <?php
+  endif;
+  ?>
+</div>
+<?php
+endif;
+// eof: PC版 Powered by エリアの追加
 // ---------------------------------------------------- ?>
