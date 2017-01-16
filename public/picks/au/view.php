1159408bba3904ffe55b548c92258276425c127b
taikiken <o@parachute-canopy.org>
2017-01-16 17:22:04 +0900

    JS 追加 #1464

--- public/picks/au/view.php
+++ public/picks/au/view.php
@@ -37,7 +37,8 @@
   endif;
   // -----------------------------------------
   ?>
-  <link rel="stylesheet" href="/assets/sp/css/picks/au/ui.css">
+  <link rel="stylesheet" href="/assets/sp/css/picks/au/ui.css?v=<?php echo $page['version']; ?>">
+  <script src="/assets/js/libs/jquery2/jquery.min.js?v=<?php echo $page['version']; ?>"></script>
 
   <script>
    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -54,6 +55,41 @@
 </head>
 <body class="appbnr-disable">
 <div class="whole picks picks--au">
+  <nav class="category-nav">
+    <ul class="category-nav--main">
+      <li class="category-nav__item"><a class="category-nav__link category-nav__link--baseball" href="https://sportsbull.jp/category/baseball/"><span>野球</span></a></li>
+      <li class="category-nav__item"><a class="category-nav__link category-nav__link--koshien" href="https://sportsbull.jp/category/highschoolbaseball/"><span>高校野球</span></a></li>
+      <li class="category-nav__item"><a class="category-nav__link category-nav__link--soccer" href="https://sportsbull.jp/category/soccer/"><span>サッカー</span></a></li>
+      <li class="category-nav__item"><a class="category-nav__link category-nav__link--basketball" href="https://sportsbull.jp/category/basketball/"><span>バスケ</span></a></li>
+      <li class="category-nav__item"><a class="category-nav__link category-nav__link--golf" href="https://sportsbull.jp/category/golf/"><span>ゴルフ</span></a></li>
+      <li class="category-nav__item"><a class="category-nav__link category-nav__link--tenis" href="https://sportsbull.jp/category/tennis/"><span>テニス</span></a></li>
+    </ul><!-- /.category-nav--main -->
+
+    <dl class="category-nav--sub">
+      <dd class="category-nav__accordion__body">
+        <div id="js-category-nav__accordion__body__inner" class="category-nav__accordion__body__inner">
+          <ul class="category-nav__list">
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/battle/"><span>相撲<br>格闘技</span></a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/volleyball/">バレーボール</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/rugby/">ラグビー</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/americanfootball/">アメフト</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/athletics/">陸上競技</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/swimming/">水泳</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/pingpong/">卓球</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/motorsports/">モーター<br>スポーツ</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/extremesports/">アクション<br>スポーツ</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/wintersports/">ウィンター<br>スポーツ</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/parasports/">パラ<br>スポーツ</a></li>
+            <li class="category-nav__item"><a class="category-nav__link" href="https://sportsbull.jp/category/etc/">その他</a></li>
+          </ul>
+        </div><!-- /.category-nav__accordion__body__inner -->
+      </dd><!-- /.category-nav__accordion__body -->
+      <dt id="js-category-nav__accordion__trigger" class="category-nav__accordion__trigger off">
+        <span class="category-nav__accordion__trigger--on">その他の種目を見る</span>
+        <span class="category-nav__accordion__trigger--off">閉じる</span>
+      </dt><!-- /.category-nav__accordion__trigger -->
+    </dl><!-- /.category-nav--sub -->
+  </nav><!-- /.category-nav -->
 
   <div class="body-sec">
     <div class="body-sec-inner">
@@ -201,6 +237,6 @@ if (!$from_webview) :
 endif;
 // -----------------------------------------
 ?>
-
+<script src="/assets/js/au/index.js"></script>
 </body>
 </html>
