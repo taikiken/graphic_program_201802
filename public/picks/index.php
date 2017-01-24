a9e7ada463c3367259c2d8b384981a317f4e4333
taikiken <o@parachute-canopy.org>
2017-01-24 14:41:15 +0900

    dev -> dev-img.sportsbull.jp から XML 取得します #1426

--- public/picks/index.php
+++ public/picks/index.php
@@ -93,19 +93,15 @@ $model = new ViewModel($o);
 // - 公開 : https://img.sportsbull.jp/xml/picks.xml
 
 // host name 取得
-//$app_host_name = $page['apiRoot'];
-//if ($app_host_name == '') {
-//  $app_host_name = 'https://dev.sportsbull.jp';
-//}
-//
-//// host name から xml host name 設定
-//$xml_host_name = 'https://dev-img.sportsbull.jp';
-//if ($app_host_name != 'https://dev.sportsbull.jp') {
-//  $xml_host_name = 'https://img.sportsbull.jp';
-//}
+$app_host_name = $model->get_site_url();
 
-// @since 2017-01-17
+// host name から xml host name 設定
+// default `https://img.sportsbull.jp` - 本番サーバー
 $xml_host_name = 'https://img.sportsbull.jp';
+// dev のみ `https://dev-img.sportsbull.jp` から取得
+if ($app_host_name == 'https://dev.sportsbull.jp' || $app_host_name == 'https://dev.sportsbull.jp/') {
+  $xml_host_name = 'https://dev-img.sportsbull.jp';
+}
 
 // xml data を設定する配列
 $articles = array();
@@ -170,3 +166,5 @@ endif;
 
 // 確認用dumpデータ - テンプレ組み込みおわったら削除
 //print_r($page['apiRoot']);
+//print_r($xml_host_name);
+//print_r($app_host_name);
