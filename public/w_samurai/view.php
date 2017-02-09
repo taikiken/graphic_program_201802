8246dd61b51598d6c12a88807c86eaa72f664fc9
Kakoi <k@parachute-canopy.org>
2017-02-09 18:10:46 +0900

    #1536 PC版 進捗

--- public/w_samurai/view.php
+++ public/w_samurai/view.php
@@ -23,7 +23,8 @@
   <link rel="shortcut icon" href="/favicon.ico">
 
   <link rel="canonical" href="https://sportsbull.jp/w_samurai/">
-  <script src="/assets/js/libs/jquery2/jquery.min.js?v=<?php echo $page['version']; ?>"></script>
+  <script src="/assets/js/libs/vendor.react.js?v=<?php echo $page['version']; ?>"></script>
+  <script src="/assets/js/bundle/main.bundle.js?v=<?php echo $page['version']; ?>"></script>
   <link rel="stylesheet" href="/assets/css/w_samurai/ui.css?v=<?php echo $page['version']; ?>">
 
   <script>
@@ -59,7 +60,88 @@
       <section class="side-sec">
         <div id="sidebar-moving-container">
 
-          <?php include_once __DIR__."/_sidebar_ad.php"; ?>
+          <?php
+          /*
+           * https://github.com/undotsushin/undotsushin/issues/720
+           * 広告 / PC版画像バナー広告をDFP管理下にする
+           */
+          ?>
+          <?php if ( $page['ad']['pc']['sidebar_top'] ) : ?>
+          <div class="sponsor-link">
+
+            <script type='text/javascript'>
+              googletag.cmd.push(function() {
+                googletag.defineSlot('/531683568/<?php echo $page['ad']['pc']['sidebar_top']; ?>', [300, 250], 'div-gpt-ad-pc_sidebar_top').addService(googletag.pubads());
+                googletag.pubads().enableSingleRequest();
+                googletag.pubads().collapseEmptyDivs();
+                googletag.enableServices();
+              });
+            </script>
+            <div id='div-gpt-ad-pc_sidebar_top' style='height:250px; width:300px;'>
+            <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-pc_sidebar_top'); });
+            </script>
+            </div>
+
+          </div>
+          <?php endif; ?>
+
+          <div class="app-bnr"><a href="/about/"><img src="/assets/images/common/bnr-side-app.png" alt="SPORTS BULLアプリ版(iPhone/Android対応) アプリでサクサク楽しむ！"></a></div>
+          <div class="synSearch-bnr mt20"><a href="http://pickup.syndot.jp/about/?utm_source=undou_pc&utm_medium=banner&utm_campaign=search" target="_blank"><img src="/assets/images/common/bnr-side-synsearch.png" alt="Syn.search チャットで検索？"></a></div>
+
+          <?php
+          // ------------------------------------
+          // sidebar recommend, オススメ記事
+          if (
+            $page['template'] == 'category' ||
+            $page['template'] == 'search' ||
+            $page['template'] == 'p'
+          ) : ?>
+            <div id="widget-recommend-list-container"></div><!--/recommend-->
+          <?php endif; ?>
+          <div id="widget-ranking-container"></div><!--/ranking-->
+
+          <?php if ( $page['category']['slug'] !== 'crazy' ) : ?>
+          <div id="sponsor-link-ranking" class="sponsor-link sponsor-link-ranking">
+            <script src="https://ssl.socdm.com/sdk/js/adg-script-loader.js?id=35250&targetID=adg_35250&displayid=2&adType=PC&width=0&height=0&sdkType=3&async=true&tagver=2.0.0"></script>
+          </div>
+          <?php endif; ?>
+
+          <?php
+          // ------------------------------------
+          // sidebar videos, オススメ動画
+          // crazy 表示しない
+          // https://github.com/undotsushin/undotsushin/issues/862#issuecomment-229568814
+          if ( $page['category']['slug'] !== 'crazy' ) :
+          ?>
+          <div id="widget-recommend-container"></div><!--/videos-->
+          <div id="sponsor-link-recommend" class="sponsor-link sponsor-link-recommend">
+            <script src="https://ssl.socdm.com/sdk/js/adg-script-loader.js?id=35251&targetID=adg_35251&displayid=2&adType=PC&width=0&height=0&sdkType=3&async=true&tagver=2.0.0"></script>
+          </div>
+          <?php endif; ?>
+          <?php
+          // ------------------------------------
+          // sidebar bottom
+          ?>
+          <?php if ( $page['ad']['pc']['sidebar_bottom'] ) :?>
+          <div class="sponsor-link nadir">
+
+            <script type='text/javascript'>
+              googletag.cmd.push(function() {
+                googletag.defineSlot('/531683568/<?php echo $page['ad']['pc']['sidebar_bottom']; ?>', [300, 600], 'div-gpt-ad-pc_sidebar_bottom').addService(googletag.pubads());
+                googletag.pubads().enableSingleRequest();
+                googletag.pubads().collapseEmptyDivs();
+                googletag.enableServices();
+              });
+            </script>
+            <div id='div-gpt-ad-pc_sidebar_bottom' style='height:600px; width:300px;'>
+            <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-pc_sidebar_bottom'); });
+            </script>
+            </div>
+
+          </div>
+          <?php endif; ?>
 
           <div id="widget-ranking-container-2"></div><!--/ranking-->
 
@@ -145,10 +227,30 @@
 </script>
 
 <?php
-if (0) :
-//w_samurai.bundle.js 不要
+/*
+ * $page['apiRoot'] を元に API request 先を決定します
+ * php側で任意の apiRoot が 設定されている時に
+ * UT.app.App.develop(); を行います
+ *
+*/
+if ( $page['apiRoot'] != '' ) :
+  // develop mode
+  // dev, stg, local から起動の時のみ script tag を有効にします
 ?>
-<script src="/assets/js/w_samurai.bundle.js?v=<?php echo $page['version']; ?>"></script>
+<script>
+( function () {
+
+  'use strict';
+
+  var UT = self.UT;
+  // リクエスト先を変更します
+//  UT.app.App.develop( 'https://dev.sportsbull.jp' );
+  UT.app.App.develop();
+
+}() );
+</script>
 <?php endif; ?>
+<script id="js-exe"<?php echo !empty($page['category']['label']) ? ' data-label="' . $page['category']['label'] . '" ' : ''; ?>src="/assets/js/bundle/exe.bundle.js?v=<?php echo $page['version']; ?>"></script>
+
 </body>
 </html>
