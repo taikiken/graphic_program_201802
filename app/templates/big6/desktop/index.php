3170fb28ee2dc80a2eafaab91a0f0a33cd413263
Yoshiaki Fujimori <info@2244.jp>
2017-02-13 21:12:14 +0900

    big6 - `/big6/` でテンプレ返すまで #1528

---
+++ app/templates/big6/desktop/index.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once __DIR__."/../../desktop/_header.php";
+
+?>
+
+
+<div class="body-sec">
+  <div class="body-sec-inner">
+
+    <section class="main-sec">
+      <div class="board-large">
+        # 日程表のPC版
+      </div><!-- /.board-large -->
+    </section><!-- /.main-sec -->
+
+    <section class="side-sec">
+      <?php include_once __DIR__."/../../desktop/_sidebar_ad.php"; ?>
+    </section><!-- /.side-sec -->
+
+
+  </div>
+</div><!-- /.body-sec -->
+
+
+
+<?php
+
+include_once __DIR__."/../../desktop/_footer.php";
+include_once __DIR__."/../../_debug.php";
+
+?>
