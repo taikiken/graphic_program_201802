231bf9835612892ab92872d0d5c1db72eb8f191d
Yoshiaki Fujimori <info@2244.jp>
2017-02-24 09:44:18 +0900

    big6tv - webview用のルーティング追加 #1528

--- app/routes/big6tv.router.php
+++ app/routes/big6tv.router.php
@@ -2,41 +2,78 @@
 
 // big6tv
 // ==============================
-$app->get('/{slug:big6|big6tv}[/]', function ($request, $response, $args) use ($app) {
+$app->group('/{slug:big6|big6tv}[/]', function ($request, $response, $args) use ($app) {
 
 
-  $args['page'] = $app->model->set(array(
-    'title'              => '六大学野球のタイトルです(仮)',
-    'og_title'           => '大学野球のタイトルです(仮) | '.$app->model->property('title'),
-    'og_url'             => $app->model->property('site_url').'big6tv/',
-    'path'               => $args,
-    'template'           => 'category',
-    'template_classname' => '',
-  ));
+  // /big6tv/
+  // ==============================
+  $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app) {
+    $args['page'] = $app->model->set(array(
+      'title'              => '六大学野球のタイトルです(仮)',
+      'og_title'           => '大学野球のタイトルです(仮) | '.$app->model->property('title'),
+      'og_url'             => $app->model->property('site_url').'big6tv/',
+      'path'               => $args,
+      'template'           => 'category',
+      'template_classname' => '',
+    ));
 
 
-  // debug用に不要なmodel削除
-  // unset($args['page']['site_categories']);
-  // unset($args['page']['ad']);
-  // unset($args['page']['category']);
-  // unset($args['page']['post']);
+    // debug用に不要なmodel削除
+    // unset($args['page']['site_categories']);
+    // unset($args['page']['ad']);
+    // unset($args['page']['category']);
+    // unset($args['page']['post']);
 
 
-  // スケジュール表を取得する
-  $big6tvSchedule = @file_get_contents($app->model->property('site_url').'/api/big6tv/schedule');
-  $args['page']['big6tv']['scheduleData'] = json_decode($big6tvSchedule, true)['response'];
+    // スケジュール表を取得する
+    $big6tvSchedule = @file_get_contents($app->model->property('site_url').'/api/big6tv/schedule');
+    $args['page']['big6tv']['scheduleData'] = json_decode($big6tvSchedule, true)['response'];
 
-  // ランキングデータを取得する
-  $big6tvRanking = @file_get_contents($app->model->property('site_url').'/api/big6tv/ranking');
-  $args['page']['big6tv']['rankingData'] = json_decode($big6tvRanking, true)['response'];
+    // ランキングデータを取得する
+    $big6tvRanking = @file_get_contents($app->model->property('site_url').'/api/big6tv/ranking');
+    $args['page']['big6tv']['rankingData'] = json_decode($big6tvRanking, true)['response'];
 
 
-  if ( $app->model->property('ua') === 'desktop' ) :
-    return $this->renderer->render($response, 'big6tv/desktop/index.php', $args);
-  else :
-    return $this->renderer->render($response, 'big6tv/mobile/index.php', $args);
-  endif;
+    if ( $app->model->property('ua') === 'desktop' ) :
+      return $this->renderer->render($response, 'big6tv/desktop/index.php', $args);
+    else :
+      return $this->renderer->render($response, 'big6tv/mobile/index.php', $args);
+    endif;
 
-});
+  });
+
+
+  // WebView
+  // ==============================
+  $this->get('/webview[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+      'title'              => 'BIG6TV',
+      'og_title'           => 'BIG6TV | '.$app->model->property('title'),
+      'og_url'             => $app->model->property('site_url').'big6tv/',
+      'path'               => $args,
+      'template'           => 'category',
+      'template_classname' => '',
+    ));
+
+
+    // debug用に不要なmodel削除
+    // unset($args['page']['site_categories']);
+    // unset($args['page']['ad']);
+    // unset($args['page']['category']);
+    // unset($args['page']['post']);
+
+
+    // スケジュール表を取得する
+    $big6tvSchedule = @file_get_contents($app->model->property('site_url').'/api/big6tv/schedule');
+    $args['page']['big6tv']['scheduleData'] = json_decode($big6tvSchedule, true)['response'];
+
+    // ランキングデータを取得する
+    $big6tvRanking = @file_get_contents($app->model->property('site_url').'/api/big6tv/ranking');
+    $args['page']['big6tv']['rankingData'] = json_decode($big6tvRanking, true)['response'];
+
+    return $this->renderer->render($response, 'big6tv/webveiw.php', $args);
+
+  });
 
 ?>
