5bf08cb858e8e128d5b03d951ea276bd36869513
Yoshiaki Fujimori <info@2244.jp>
2017-03-06 15:14:33 +0900

    vk - 今年用のテンプレートを用意 - 今年の指定に書き換え #1602
    player IDとか去年と違う

--- app/templates/specific/_vk_brightcove_2017.php
+++ app/templates/specific/_vk_brightcove_2017.php
@@ -3,14 +3,14 @@
 #958 バーチャル高校野球2016用 brightcove埋め込みコード
 
 ?>
-<link href="/assets/vk_brightcove/css/Player.css" rel="stylesheet"/>
+<link href="/assets/vk_brightcove_2017/css/Player.css" rel="stylesheet"/>
 
 <div style="display: block; position: relative;">
   <div style="padding-top: 56.25%;">
     <video
       id="ABCplayer"
       data-account="4802324450001"
-      data-player="rJIcYM98"
+      data-player="Bymj9RCx"
       data-embed="default"
       class="video-js"
       style="width: 100%; height: 100%; position: absolute; top: 0px; bottom: 0px; right: 0px; left: 0px;"
@@ -19,30 +19,19 @@
   </div>
 </div>
 
-<script src="//players.brightcove.net/4802324450001/rJIcYM98_default/index.min.js"></script>
+<script src="//players.brightcove.net/4802324450001/Bymj9RCx_default/index.min.js"></script>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="/assets/vk_brightcove/js/bc-mapi.js"></script>
-<script src="/assets/vk_brightcove/js/Player.js"></script>
+<script src="/assets/vk_brightcove_2017/js/bc-mapi.js"></script>
+<script src="/assets/vk_brightcove_2017/js/Player.js"></script>
 <script type="text/javascript">
   videojs('ABCplayer').PlayerControl({
     "videoId" : "<?php echo $page['post']['media_vk_refid']; ?>",
-    //"videoId" : "koya98_homerun1_hls",
+    //"videoId" : "test97_kantoku1_mobile",
     "stillImageUrl" : "<?php echo $page['post']['media']['images']['large']; ?>",
-    //"stillImageUrl" : "",
+    //"stillImageUrl" : "http://www.asahicom.jp/koshien/97/digest/20150808_1d.jpg",
     "autoPlay" : true,
     "showRelatedVideos" : true,
-    "relatedVideosTags": ['ut'],
+    "relatedVideosTags": ['hls_vod'],
   });
-
-  //AdobeAnalyics再生開始計測メソッド
-  sc_asa_digi_koshien_videotype = null;
-
-  function videoStartTracking(videoType) {
-    sc_asa_digi_koshien_videotype = videoType;
-    console.log("VideoStartTracking, videoType=" + videoType);
-    if("_satelite" in window){
-      _satellite.track("trackKoshienVideoStart");
-    }
-  }
 </script>
 
