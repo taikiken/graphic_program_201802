b5bfacaaae99ca07dc122b6e42fae76908c07643
taikiken <o@parachute-canopy.org>
2017-03-15 19:31:13 +0900

    SP `/big6tv` Syn.search 非表示 #1546

--- app/templates/big6tv/mobile/index.php
+++ app/templates/big6tv/mobile/index.php
@@ -233,8 +233,20 @@
         <div class="fb-page-plugin">
           <div class="fb-page" data-href="https://www.facebook.com/sportsbull/" data-width="500" data-height="154" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/sportsbull/"><a href="https://www.facebook.com/sportsbull/">スポーツブル（SPORTS BULL）</a></blockquote></div></div>
         </div>
-
+<?php
+// 六大学 / 広告表示 調整（Web） #1546
+// > アドネットワーク関連の広告（ネイティブアド？）を消したい
+// @see https://github.com/undotsushin/undotsushin/issues/1546
+// category.slug 'big6tv' search
+// @since 2017-03-15
+if (0) :
+?>
         <div class="foot-pr-bnr"><a href="http://pickup.syndot.jp/about/?utm_source=undou_sp&utm_medium=banner&utm_campaign=search" target="_blank"><img src="/assets/sp/images/common/bnr-footer-synsearch.png" alt="Syn.search チャットで検索？"></a></div>
+<?php
+endif;
+// not big6tv の時のみ広告を表示する
+// --------------------------------
+?>
       </div><!-- /.foot-pr -->
 
       <div id="js-page_top" class="pagetop"><a href="#"><span>このページの先頭へ</span></a></div>
