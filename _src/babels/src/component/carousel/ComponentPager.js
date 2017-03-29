614708eb3d74de46faec233a9f414278f1b06ad0
taikiken <o@parachute-canopy.org>
2017-03-29 17:29:19 +0900

    関数位置を ESLint 準拠し変更 #1462

--- _src/babels/src/component/carousel/ComponentPager.js
+++ _src/babels/src/component/carousel/ComponentPager.js
@@ -18,41 +18,6 @@ const React = self.React;
  * @since 2016-09-15
  */
 export class ComponentPager extends React.Component {
-  /**
-   * プロパティを保存し必要な関数・変数を準備します
-   * @param {Object} props プロパティ {@link ComponentPager.propTypes}
-   */
-  constructor(props) {
-    super(props);
-    /**
-     * bind 済み onClick
-     * @type {function}
-     */
-    this.boundClick = this.onClick.bind(this);
-  }
-  /**
-   * カルーセル・ページャーの1つを作成します
-   * @return {XML} カルーセル・ページャーの1つを返します
-   */
-  render() {
-    const props = this.props;
-    const no = props.index;
-    return (
-      <li className={`pager-item pager-${no}`}>
-        <a href={`#pickup-${props.index}`} className="pager-link" onClick={this.boundClick} >{no}</a>
-      </li>
-    );
-  }
-  /**
-   * ページャー click event handler<br>
-   * コールバックにページャー内数字を通知します
-   * @param {Event} event ページャー click event
-   */
-  onClick(event) {
-    event.preventDefault();
-    // pager html 内数字をコールバックに通知します
-    this.props.onPager(event.target.innerHTML);
-  }
   // ---------------------------------------------------
   //  STATIC GETTER / SETTER
   // ---------------------------------------------------
@@ -68,16 +33,46 @@ export class ComponentPager extends React.Component {
       onPager: React.PropTypes.func.isRequired
     };
   }
+  // ---------------------------------------------------
+  //  CONSTRUCTOR
+  // ---------------------------------------------------
+  /**
+  /**
+   * プロパティを保存し必要な関数・変数を準備します
+   * @param {Object} props プロパティ {@link ComponentPager.propTypes}
+   */
+  constructor(props) {
+    super(props);
+    /**
+     * bind 済み onClick
+     * @type {function}
+     */
+    this.boundClick = this.onClick.bind(this);
+  }
+  // ---------------------------------------------------
+  //  METHOD
+  // ---------------------------------------------------
+  /**
+   * ページャー click event handler<br>
+   * コールバックにページャー内数字を通知します
+   * @param {Event} event ページャー click event
+   */
+  onClick(event) {
+    event.preventDefault();
+    // pager html 内数字をコールバックに通知します
+    this.props.onPager(event.target.innerHTML);
+  }
+  /**
+   * カルーセル・ページャーの1つを作成します
+   * @return {XML} カルーセル・ページャーの1つを返します
+   */
+  render() {
+    const props = this.props;
+    const no = props.index;
+    return (
+      <li className={`pager-item pager-${no}`}>
+        <a href={`#pickup-${props.index}`} className="pager-link" onClick={this.boundClick} >{no}</a>
+      </li>
+    );
+  }
 }
-//
-// /**
-//  * プロパティ
-//  * @static
-//  * @type {{index: number, id: string, length: number, onPager: function}}
-//  */
-// ComponentPager.propTypes = {
-//   index: React.PropTypes.number.isRequired,
-//   id: React.PropTypes.string.isRequired,
-//   length: React.PropTypes.number.isRequired,
-//   onPager: React.PropTypes.func.isRequired
-// };
