e130462602e3f310efc7c51360f5265cf840e49f
taikiken <o@parachute-canopy.org>
2017-03-31 14:23:27 +0900

    関数位置見直し

--- _src/babels/src/component/categories/ComponentHeadlineAd.js
+++ _src/babels/src/component/categories/ComponentHeadlineAd.js
@@ -30,6 +30,22 @@ const React = self.React;
  *
  * */
 export class ComponentHeadlineAd extends React.Component {
+  // ---------------------------------------------------
+  //  STATIC GETTER / SETTER
+  // ---------------------------------------------------
+  /**
+   * propTypes
+   * @return {{browser: string, ad: HeadlineAdDae}} React props
+   */
+  static get propTypes() {
+    return {
+      browser: React.PropTypes.string.isRequired,
+      ad: React.PropTypes.object.isRequired
+    };
+  }
+  // ---------------------------------------------------
+  //  CONSTRUCTOR
+  // ---------------------------------------------------
   /**
    * プロパティを保存し必要な関数・変数を準備します
    * @param {{browser: string, ad: HeadlineAdDae}} props プロパティ
@@ -37,60 +53,28 @@ export class ComponentHeadlineAd extends React.Component {
    */
   constructor(props) {
     super(props);
+    /**
+     * div.sponsor-link 広告タグ挿入 Element
+     * @type {?Element}
+     */
+    this.sponsorLink = null;
   }
-  /**
-   * アドジェネ広告を作成します
-   * @return {?XML} アドジェネ広告を返す, 無い時は null を返します
-   */
-  render() {
-    switch (this.props.browser) {
-      case 'sp': {
-        if (!this.props.ad.sp) {
-          return null;
-        }
-        break;
-      }
-      case 'pc':
-      default: {
-        if (!this.props.ad.pc) {
-          return null;
-        }
-        break;
-      }
-    }
-
-    return (
-      <div className="sponsor-link" ref="sponsorLink" />
-    );
-  }
-  /**
-   * div.sponsor-link マウント後に「アドジェネ広告」タグを作成します
-   */
-  componentDidMount() {
-    switch (this.props.browser) {
-      case 'sp': {
-        this.sp();
-        break;
-      }
-      case 'pc':
-      default: {
-        this.pc();
-        break;
-      }
-    }
-  }
+  // ---------------------------------------------------
+  //  METHOD
+  // ---------------------------------------------------
   /**
    * アドジェネ広告 `script` tag を作成し wrapper div に `appendChild` します<br>
    * さらに div.sponsor-link へ `appendChild` します
    * @param {string} path アドジェネ広告パス
    */
   script(path) {
-    const div = document.createElement('div');
-    const script = document.createElement('script');
-    script.src = path;
-    div.appendChild(script);
-
-    this.refs.sponsorLink.appendChild(div);
+    if (this.sponsorLink) {
+      const div = document.createElement('div');
+      const script = document.createElement('script');
+      script.src = path;
+      div.appendChild(script);
+      this.sponsorLink.appendChild(div);
+    }
   }
   /**
    * sp: アドジェネ広告を差し込む、この値がなければ広告は表示しない
@@ -114,30 +98,52 @@ export class ComponentHeadlineAd extends React.Component {
 
     this.script(`${Ad.host()}/sdk/js/adg-script-loader.js?id=${id}&targetID=adg_${id}&displayid=2&adType=PC&width=0&height=0&sdkType=3&async=true&tagver=2.0.0`);
   }
-  // ---------------------------------------------------
-  //  STATIC GETTER / SETTER
-  // ---------------------------------------------------
+  // ------
+  // delegate
   /**
-   * propTypes
-   * @return {{browser: string, ad: HeadlineAdDae}} React props
+   * div.sponsor-link マウント後に「アドジェネ広告」タグを作成します
    */
-  static get propTypes() {
-    return {
-      browser: React.PropTypes.string.isRequired,
-      ad: React.PropTypes.object.isRequired
-    };
+  componentDidMount() {
+    switch (this.props.browser) {
+      case 'sp': {
+        this.sp();
+        break;
+      }
+      case 'pc':
+      default: {
+        this.pc();
+        break;
+      }
+    }
+  }
+  /**
+   * アドジェネ広告を作成します
+   * @return {?XML} アドジェネ広告を返す, 無い時は null を返します
+   */
+  render() {
+    switch (this.props.browser) {
+      case 'sp': {
+        if (!this.props.ad.sp) {
+          return null;
+        }
+        break;
+      }
+      case 'pc':
+      default: {
+        if (!this.props.ad.pc) {
+          return null;
+        }
+        break;
+      }
+    }
+
+    return (
+      <div
+        className="sponsor-link"
+        ref={(component) => {
+          this.sponsorLink = component;
+        }}
+      />
+    );
   }
 }
-//
-// /**
-//  * プロパティ
-//  * @typedef {Object} propTypes
-//  * @property {string} browser browser 種類 'sp' or 'pc'
-//  * @property {HeadlineAdDae} ad アドジェネ広告 JSON data
-//  * @static
-//  * @type {{browser: string, ad: HeadlineAdDae}}
-//  */
-// ComponentHeadlineAd.propTypes = {
-//   browser: React.PropTypes.string.isRequired,
-//   ad: React.PropTypes.object.isRequired
-// };
