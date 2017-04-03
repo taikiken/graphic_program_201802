3cad90803a66e6a146a1dfe7a7a2d54c09c562f9
Kakoi <k@parachute-canopy.org>
2017-04-03 22:12:22 +0900

    アプリではhtmlタグにclassを書き込めないようなので少し変更 #1757

--- app/templates/big6tv/mobile/category.php
+++ app/templates/big6tv/mobile/category.php
@@ -25,10 +25,11 @@
     <a href="/big6tv/" id="js-matches-btn-link">全試合日程を見る</a>
   </div>
 </section><!-- /.matches -->
+
 <?php
 // app detector - html tag に class 追加 undotsushin-android or undotsushin-ios
 ?>
-<script type="text/javascript" src="/assets/js/app_divide.bundle.js?v=<?php echo $page['version']; ?>"></script>
+<!-- <script type="text/javascript" src="/assets/js/app_divide.bundle.js?v=<?php echo $page['version']; ?>"></script> -->
 <script type="text/javascript" charset="utf-8">
 (function ( window ){
   'use strict';
@@ -36,12 +37,19 @@
   var Sagen = window.Sagen;
   var html = document.documentElement;
   var banner;
-  if (Sagen.Dom.hasClass(html, 'undotsushin-android') || Sagen.Dom.hasClass(html, 'undotsushin-ios')) {
+
+  if (navigator.userAgent.match(/undotsushin-(android|ios)/)) {
     banner = document.getElementById('js-matches-btn-link');
     if (banner) {
       banner.href += '?app=ios';
     }
   }
+  // if (Sagen.Dom.hasClass(html, 'undotsushin-android') || Sagen.Dom.hasClass(html, 'undotsushin-ios')) {
+  //   banner = document.getElementById('js-matches-btn-link');
+  //   if (banner) {
+  //     banner.href += '?app=ios';
+  //   }
+  // }
 }( window ));
 </script>
 
