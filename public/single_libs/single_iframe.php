b8b5c56a35e919add0f249eedcc95403a4aef2da
taikiken <o@parachute-canopy.org>
2017-04-11 20:41:26 +0900

    view PHP(empty) 追加 #1818

---
+++ public/single_libs/single_iframe.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 記事詳細「続きを読む」本文を iframe 化する
+ * User: @taikiken
+ * Date: 2017/04/11
+ * Time: 20:39
+ * @see https://github.com/undotsushin/undotsushin/issues/1475#issuecomment-293174913
+ * @see https://github.com/undotsushin/undotsushin/issues/1818
+ */
