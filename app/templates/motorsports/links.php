de35ab0b4afcc850f286abe8555988af26634e78
Yoshiaki Fujimori <info@2244.jp>
2017-05-17 19:15:36 +0900

    motorsports - webviewsテンプレの中身空にしておく #1918

--- app/templates/motorsports/links.php
+++ app/templates/motorsports/links.php
@@ -6,4 +6,22 @@
 
 */
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title><?php echo $page['title']; ?></title>
+  <style type="text/css">
+    * {
+      padding : 0;
+      margin  : 0;
+    }
+  </style>
+</head>
+
+<body>
 <!-- motorsports -->
+</body>
+</html>
