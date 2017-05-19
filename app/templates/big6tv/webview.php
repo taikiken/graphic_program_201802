fb84c6451d4ef9563fad17cc341df617ac4fb9c4
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-05-20 02:58:33 +0900

    WebViewにfb用のスクリプト追加

--- app/templates/big6tv/webview.php
+++ app/templates/big6tv/webview.php
@@ -20,6 +20,14 @@
 </div><?php //.body-sec ?>
 </div><?php //#page ?>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.9&appId=588648481289313";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 </body>
 </html>
