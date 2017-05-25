6a94f42a69538fa5bcb78852445088fd9a273c9a
taikiken <o@parachute-canopy.org>
2017-05-25 14:17:18 +0900

    依存ライブラリ追加 #1914 #1915

---
+++ _src/babels_carousel/src/moku/tick/Cycle.js
@@ -0,0 +1,170 @@
+/**
+ * @license inazumatv.com
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2016/07/03
+ *
+ * Copyright (c) 2011-2015 inazumatv.com, inc.
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ */
+
+// event
+import EventDispatcher from '../event/EventDispatcher';
+import Events from '../event/Events';
+
+/**
+ * new を許可しないための Symbol
+ * @type {Symbol}
+ * @private
+ */
+const singletonSymbol = Symbol('singleton instance');
+/**
+ * singleton instance, nullable
+ * @type {?Cycle}
+ * @private
+ * @static
+ */
+let instance = null;
+
+/**
+ * <p>requestAnimationFrame を使用しループイベントを発生させます</p>
+ * <p>singleton なので new ではなく factory を使用し instance を作成します</p>
+ *
+ * ```
+ * const loop = Cycle.factory();
+ * const update = () => {
+ *  // code here, something do
+ * };
+ * loop.on(Cycle.UPDATE, update);
+ * loop.start();
+ * ```
+ *
+ * <p>requestAnimationFrame は tab が active(focus) な時のみ発生します</p>
+ */
+export default class Cycle extends EventDispatcher {
+  /**
+   * singleton です
+   * @param {Symbol} checkSymbol singleton を保証するための private instance
+   * @returns {Cycle} singleton instance を返します
+   */
+  constructor(checkSymbol) {
+    // checkSymbol と singleton が等価かをチェックします
+    if (checkSymbol !== singletonSymbol) {
+      throw new Error('don\'t use new, instead use static factory method.');
+    }
+    // instance 作成済みかをチェックし instance が null の時 this を設定します
+    if (instance !== null) {
+      return instance;
+    }
+    super();
+    // -------------------------------
+    // onetime setting
+    instance = this;
+    const events = new Events(Cycle.UPDATE, this, this);
+    const boundUpdate = this.update.bind(this);
+    /**
+     * Cycle.UPDATE Events instance
+     * @type {Events}
+     */
+    this.events = events;
+    /**
+     * bound update requestAnimationFrame callback
+     * @type {function}
+     */
+    this.boundUpdate = boundUpdate;
+    /**
+     * requestAnimationFrame ID
+     * @type {number}
+     */
+    this.id = 0;
+    /**
+     * start 済みフラッグ
+     * @type {boolean}
+     */
+    this.started = false;
+    // 設定済み instance を返します
+    return instance;
+  }
+  // ----------------------------------------
+  // EVENT
+  // ----------------------------------------
+  /**
+   * requestAnimationFrame 毎に発生するイベントを取得します
+   * @event UPDATE
+   * @returns {string} event, cycleUpdate を返します
+   * @default cycleUpdate
+   */
+  static get UPDATE() {
+    return 'cycleUpdate';
+  }
+  // ----------------------------------------
+  // METHOD
+  // ----------------------------------------
+  /**
+   * loop(requestAnimationFrame) を開始します
+   * @returns {boolean} start に成功すると true を返します
+   */
+  start() {
+    if (this.started) {
+      // already start
+      return false;
+    }
+    this.started = true;
+    this.update();
+
+    // @return
+    return true;
+  }
+  /**
+   * loop(cancelAnimationFrame) を止めます
+   * @param {number} [id] requestAnimationFrame id を使い cancelAnimationFrame をします
+   * @returns {boolean} stop に成功すると true を返します
+   */
+  stop(id = this.id) {
+    if (!this.started) {
+      // not start
+      return false;
+    }
+
+    cancelAnimationFrame(id);
+    this.started = false;
+
+    // @return
+    return true;
+  }
+  // ----------------------------------------
+  // PRIVATE METHOD
+  // ----------------------------------------
+  /**
+   * loop(requestAnimationFrame)コールバック関数<br>Cycle.UPDATE event を発火します
+   * @returns {number} requestAnimationFrame ID
+   */
+  update() {
+    // @type {number} - requestAnimationFrame id
+    const id = requestAnimationFrame(this.boundUpdate);
+    this.id = id;
+
+    // @type {Events} - events
+    const events = this.events;
+    events.id = id;
+    // event fire
+    this.dispatch(events);
+    return id;
+  }
+  // ----------------------------------------
+  // STATIC METHOD
+  // ----------------------------------------
+  /**
+   * Cycle instance を singleton を保証し作成します
+   * @returns {Cycle} Cycle instance を返します
+   */
+  static factory() {
+    if (instance === null) {
+      return new Cycle(singletonSymbol);
+    }
+    return instance;
+  }
+}
---
+++ _src/babels_carousel/src/moku/tick/Polling.js
@@ -0,0 +1,203 @@
+/**
+ * Copyright (c) 2011-2016 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2016/07/04 - 16:46
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ *
+ */
+
+// event
+import EventDispatcher from '../event/EventDispatcher';
+import Events from '../event/Events';
+
+// tick
+import Cycle from './Cycle';
+
+/**
+ * 一定間隔毎に UPDATE イベントを発生させます
+ *
+ * @example
+ * // 3sec. interval
+ * const polling = new Polling(1000 * 3);
+ * const update = () => {
+ *  // code here, something do
+ * };
+ * polling.on(Polling.UPDATE, update);
+ * polling.start();
+ */
+export default class Polling extends EventDispatcher {
+  /**
+   * 引数の polling に合わせ UPDATE イベントを発生させます
+   * @param {number} [interval=1000] イベント発生間隔 milliseconds
+   */
+  constructor(interval = 1000) {
+    super();
+    // @type {Cycle} - Cycle instance
+    const cycle = Cycle.factory();
+    const boundUpdate = this.update.bind(this);
+    const events = new Events(Polling.UPDATE, this, this);
+    /**
+     * Cycle instance を取得します
+     * @ty[e {Cycle}
+     */
+    this.cycle = cycle;
+    /**
+     * 間隔(ms)
+     * @type {number}
+     */
+    this.interval = interval;
+    /**
+     * bound update, Cycle.UPDATE event handler
+     * @returns {function}
+     */
+    this.boundUpdate = boundUpdate;
+    /**
+     * Events instance - Polling.UPDATE Events object
+     * @type {Events}
+     */
+    this.events = events;
+    /**
+     * polling event 発生時間, event を発火すると 0 にリセットされます
+     * @type {number}
+     */
+    this.begin = 0;
+    /**
+     * Cycle 監視開始フラッグ
+     * @type {boolean}
+     */
+    this.started = false;
+  }
+  // ----------------------------------------
+  // EVENT
+  // ----------------------------------------
+  /**
+   * 一定間隔(milliseconds)毎に発生するイベント type を取得します
+   * @returns {string} event, pollingUpdate を返します
+   */
+  static get UPDATE() {
+    return 'pollingUpdate';
+  }
+  // ----------------------------------------
+  // METHOD
+  // ----------------------------------------
+  /**
+   * polling 時間を変更します<br>
+   * 1. プロパティ polling 変更
+   * 1. update 実行
+   * @param {number} interval polling 時間
+   * @returns {boolean} `update` をコールし Polling.UPDATE event が発生すると true を返します
+   */
+  change(interval) {
+    this.interval = interval;
+    return this.update();
+  }
+  /**
+   * started flag を反転させ現在の started flag 状態を取得します
+   * @returns {boolean} 現在の started flag 状態を返します
+   */
+  turnOver() {
+    this.started = !this.started;
+    return this.started;
+  }
+  /**
+   * events object を発火前に作成します
+   * @param {number} begin 開始時間: 前回の発火時間
+   * @param {number} present 現在時間
+   * @returns {Events} アップデートした Events を返します
+   */
+  updateEvents(begin, present) {
+    this.begin = begin;
+    // @type {Events} - start event
+    const events = this.events;
+    events.begin = begin;
+    events.present = present;
+    events.interval = this.interval;
+    return events;
+  }
+  /**
+   * cycle ループを開始します<br>
+   * watch Cycle.UPDATE event
+   * @returns {Cycle} cycle ループを開始しインスタンスを返します
+   */
+  initCycle() {
+    // cycle
+    const cycle = this.cycle;
+    // bind Cycle.UPDATE
+    cycle.on(Cycle.UPDATE, this.boundUpdate);
+    // cycle 開始
+    cycle.start();
+    return cycle;
+  }
+  /**
+   * polling を開始します
+   * @returns {boolean} start に成功すると true を返します
+   */
+  start() {
+    if (this.started) {
+      // already start
+      return false;
+    }
+    // flag -> true
+    // this[startSymbol] = true;
+    this.turnOver();
+    // cycle
+    this.initCycle();
+    // @type {number} - 開始時間
+    const present = Date.now();
+    // 強制的に1回目を実行
+    this.fire(this.updateEvents(present, present));
+
+    return true;
+  }
+  /**
+   * polling を止めます
+   * @returns {boolean} stop に成功すると true を返します
+   */
+  stop() {
+    if (!this.started) {
+      // not start
+      return false;
+    }
+    this.cycle.off(Cycle.UPDATE, this.boundUpdate);
+    // this[startSymbol] = false;
+    this.turnOver();
+    return true;
+  }
+  /**
+   * Cycle.UPDATE event handler, polling を計測しイベントを発火するかを判断します
+   *
+   * @listens {Cycle.UPDATE} Cycle.UPDATE が発生すると実行されます
+   * @returns {boolean} Polling.UPDATE event が発生すると true を返します
+   */
+  update() {
+    // 現在時間
+    // @type {number}
+    const present = Date.now();
+    // @type {number} - interval 間隔
+    const interval = this.interval;
+    // @type {number} - 開始時間
+    const begin = this.begin;
+    // 現在時間 が interval より大きくなったか
+    if ((present - begin) >= interval) {
+      // event 発火
+      this.fire(this.updateEvents(begin, present));
+      // 開始時間を update
+      this.begin = present;
+      // event 発生
+      return true;
+    }
+    return false;
+  }
+  /**
+   * Polling.UPDATE event を発生します
+   * @param {Events} events Polling.UPDATE event object
+   * @returns {void}
+   */
+  fire(events) {
+    this.dispatch(events);
+  }
+}
