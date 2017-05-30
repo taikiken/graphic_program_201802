c99756b57bf4bb6bf9b4e768d1c95aca93ea9cf0
Yoshiaki Fujimori <info@2244.jp>
2017-05-30 23:12:51 +0900

    BEYONDX用ad_vmapアップデート #1936

--- public/ad_vmap/index.php
+++ public/ad_vmap/index.php
@@ -4,17 +4,30 @@
 # ad_vmap
 
 VASTタグをGoogle IMA準拠のVMAPフォーマットにコンバートする
-
 ref. https://docs.google.com/spreadsheets/d/1qbuCw6mWQOBzrUBk2lUnDPLbtWWxgHYZRvJjTQBj-Zg/edit#gid=1084766408
-
 
 # 想定リクエストURL
 
 https://sportsbull.jp/ad_vmap/?position={$position}&timeOffset=10&source={$ad_url}
-=> {$adtag url} にパラメータの値を踏襲してアクセス
+- {$position}   : pre-roll | mid-roll | post-roll
+- {$timeOffset} : mid-roll の場合の再生開始秒数 max 60秒
+- {$ad_url}     : 広告タグURL
+  - [timestamp] : 乱数に置き換わる部分
+  - [referer]   : https://sportsbull.jp/?ad_vmap='.$timestamp に置き換わる
 
-ex.
-https://sportsbull.jp/ad_vmap/?position=post-roll&source=https://web-jp.ad-v.jp/adam/inline?CE=0&cat=undo.SP.CRAZY_vol_8_4&format=post-roll&page=[timestamp]
+※ timestampに該当する部分は必ずURL末のパラメータに指定する
+NG : https://sportsbull.jp/ad_vmap/?〜rand=[timestamp]&REFURL=http://dev.sportsbull.jp/p/129693/
+OK : https://sportsbull.jp/ad_vmap/?〜&REFURL=http://dev.sportsbull.jp/p/129693/&rand=[timestamp]
+
+ex. Advantage - post-roll
+https://sportsbull.jp/ad_vmap/?position=post-roll&source=https://web-jp.ad-v.jp/adam/inline?CE=0&cat=undo.SP.CRAZY_vol_8_4&format=post-roll&page=[timestamp]&REFURL=[referer]
+
+ex. DFP - preroll
+https://sportsbull.jp/ad_vmap/?https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=[timestamp]
+
+ex. BEYOND X
+https://sportsbull.jp/ad_vmap/?source=https://req.tidaltv.com/vmm.ashx?mt=1&xf=35&pid=1&fmid=425759&logs=0&REFURL=https://sportsbull.jp/p/129693/&rand=[timestamp]
+
 
 */
 
@@ -23,6 +36,11 @@ header("Content-Type: application/xml; charset=UTF-8");
 // タイムスタンプ設定
 // ==============================
 $timestamp = time();
+
+
+// リファラデフォルト設定
+// ==============================
+$referer = 'https://sportsbull.jp/?ad_vmap='.$timestamp;
 
 
 // フォーマット設定
@@ -62,8 +80,11 @@ if ( isset($_GET['source']) ) :
 
   // [timestamp] : brightcove / cci 用の乱数
   $url    = str_replace('[timestamp]', $timestamp, $url);
-  // correlator : DFP用の乱数
-  $ad_url = "<![CDATA[{$url}&correlator={$timestamp}]]>";
+
+  // [referer] : リファラの置き換え
+  $url    = str_replace('[referer]', $referer, $url);
+
+  $ad_url = "<![CDATA[{$url}]]>";
 
 endif;
 
@@ -79,9 +100,7 @@ if ( $position == 'pre-roll' ) :
 
       <vmap:AdBreak timeOffset="start" breakType="linear" breakId="pre-roll">
         <vmap:AdSource id="pre-roll" allowMultipleAds="false" followRedirects="true">
-          <vmap:AdTagURI templateType="vast3">
-            {$ad_url}
-          </vmap:AdTagURI>
+          <vmap:AdTagURI templateType="vast3">{$ad_url}</vmap:AdTagURI>
         </vmap:AdSource>
       </vmap:AdBreak>
 
@@ -95,9 +114,7 @@ if ( $position == 'mid-roll' ) :
 
       <vmap:AdBreak timeOffset="00:00:{$timeOffset}" breakType="linear" breakId="mid-roll">
         <vmap:AdSource id="midr-oll" allowMultipleAds="false" followRedirects="true">
-          <vmap:AdTagURI templateType="vast3">
-            {$ad_url}
-          </vmap:AdTagURI>
+          <vmap:AdTagURI templateType="vast3">{$ad_url}</vmap:AdTagURI>
         </vmap:AdSource>
       </vmap:AdBreak>
 
@@ -110,9 +127,7 @@ if ( $position == 'post-roll' ) :
 
     <vmap:AdBreak timeOffset="end" breakType="linear" breakId="post-roll">
       <vmap:AdSource id="post-roll" allowMultipleAds="false" followRedirects="true">
-        <vmap:AdTagURI templateType="vast3">
-          {$ad_url}
-        </vmap:AdTagURI>
+        <vmap:AdTagURI templateType="vast3">{$ad_url}</vmap:AdTagURI>
       </vmap:AdSource>
     </vmap:AdBreak>
 
