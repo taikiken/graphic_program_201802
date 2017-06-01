451be4111116333401b5dc5fcd5d46f6c37171ab
Yoshiaki Fujimori <info@2244.jp>
2017-06-01 13:56:45 +0900

    google optimize コード導入 #1876

--- app/templates/_head.php
+++ app/templates/_head.php
@@ -69,3 +69,12 @@
   <link rel="apple-touch-icon-precomposed" href="/assets/sp/images/common/apple-touch-icon.png">
   <link rel="icon" sizes="192x192" href="/assets/sp/images/common/apple-touch-icon.png">
   <link rel="shortcut icon" href="/favicon.ico">
+
+  <?php // #1876 - Google Optimize ?>
+  <style>.async-hide { opacity: 0 !important} </style>
+  <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+  h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+  (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+  })(window,document.documentElement,'async-hide','dataLayer',4000,
+  {'GTM-KJ33JM9':true});</script>
+  <?php // Google Optimize ?>
