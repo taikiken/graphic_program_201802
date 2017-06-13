c4cf57a6961a197dca3c0baa30c7e49de40f175e
Yoshiaki Fujimori <info@2244.jp>
2017-06-13 17:36:29 +0900

    stats - テニスのデモ整形 #2016

--- app/templates/stats/tennis/index.php
+++ app/templates/stats/tennis/index.php
@@ -1,21 +1,29 @@
-<!-- #tennis -->
+<div style="text-align:center; margin: 0 auto; border:1px solid #ccc;">
+  <img src="https://user-images.githubusercontent.com/971124/27073330-6a8bb4ae-505e-11e7-9a89-2f3f38f02177.png" style="height: auto; max-width:80%;"/>
+</div>
 
-<?php if ( $app['ua'] === 'desktop' ) : ?>
+<?php
+/*
+
+# SJコードのJSエラー回避
+- jsでは、$('#SJ_Tennis_header') を呼ばれてるがSJコードが生成するHTMLは、 $('.SJ_Tennis_header')である
+
+*/
+?>
+<div id="SJ_Tennis_header"></div>
+
+<?php if ( $page['ua'] === 'desktop' ) : ?>
 <link rel="stylesheet" href="http://sports.stats-japan.jp/tennis/sjparts/css/common.css" />
-<link rel="stylesheet" href="http://sports.stats-japan.jp/tennis/sjparts/css/fonts/css/fontawesome.min.css"/>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 <link rel="stylesheet" href="http://sports.stats-japan.jp/tennis/sjparts/css/sj-tennis.css" />
 <?php else : ?>
 <link rel="stylesheet" href="http://sports.stats-japan.jp/tennis/sjparts/css/common-sp.css" />
-<link rel="stylesheet" href="http://sports.stats-japan.jp/tennis/sjparts/css/fonts/css/fontawesome.min.css"/>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 <link rel="stylesheet" href="http://sports.stats-japan.jp/tennis/sjparts/css/sj-tennis-sp.css" />
 <?php endif; ?>
 <script src="http://sports.stats-japan.jp/tennis/sjparts/js/main/tennis.js"></script>
 
-<script>
-sj_Tennistour.instance.render({
-  'hideBcl' : true
-});
-</script>
+
 <?php
 /*
 
@@ -39,4 +47,17 @@ sj_Tennistour.instance.callbackPage = function() {
 ```
 
 */
-?>
+?><script>
+sj_Tennistour.instance.render({
+  'hideBcl' : true
+});
+</script>
+
+<style>
+/*  SJ header 非表示 */
+.SJ_Tennis_header { display: none !important; }
+
+.stats {
+  padding-bottom: 100px;
+}
+</style>
