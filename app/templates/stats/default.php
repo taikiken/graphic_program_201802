41863aa6c24b6dcdc51f87b73bfd4f0715768179
Yoshiaki Fujimori <info@2244.jp>
2017-06-13 16:49:47 +0900

    stats - デモ用のルーターとベーステンプレ追加 #2016

---
+++ app/templates/stats/default.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo $page['og_type']; ?>: http://ogp.me/ns/<?php echo $page['og_type']; ?>#">
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+  <title><?php echo strip_tags($page['title']).' | '.$page['site_name']; ?></title>
+  <meta name="keywords" content="<?php echo $page['keywords']; ?>">
+  <meta name="description" content="<?php echo $page['og_description']; ?>">
+
+  <meta property="fb:app_id" content="<?php echo $page['app_id']; ?>">
+  <meta property="og:site_name" content="<?php echo $page['site_name']; ?>">
+  <meta property="og:type" content="<?php echo $page['og_type']; ?>">
+  <meta property="og:title" content="<?php echo $page['og_title']; ?>">
+  <meta property="og:image" content="<?php echo $page['og_image']; ?>">
+  <meta property="og:url" content="<?php echo $page['og_url']; ?>">
+  <meta property="og:description" content="<?php echo $page['og_description']; ?>">
+  <meta property="og:locale" content="ja_JP" />
+
+  <!-- twitter card -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@<?php echo $page['sns']['twitter']; ?>">
+  <meta name="twitter:title" content="<?php echo $page['og_title']; ?>">
+  <meta name="twitter:image" content="<?php echo $page['og_image']; ?>">
+  <meta name="twitter:url" content="<?php echo $page['og_url']; ?>">
+  <meta name="twitter:description" content="<?php echo $page['og_description']; ?>">
+  <link rel="canonical" href="<?php echo $page['og_url']; ?>">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="apple-touch-icon-precomposed" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="icon" sizes="192x192" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="shortcut icon" href="/favicon.ico">
+
+  <?php // #1876 - Google Optimize ?>
+  <style>.async-hide { opacity: 0 !important} </style>
+  <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+  h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+  (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+  })(window,document.documentElement,'async-hide','dataLayer',4000,
+  {'GTM-KJ33JM9':true});</script>
+  <?php // Google Optimize ?>
+
+
+  <link rel="stylesheet" href="/assets/sp/css/ui.css?v=<?php echo $page['version']; ?>">
+  <script type='text/javascript'>
+    var googletag = googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    (function() {
+      var gads = document.createElement('script');
+      gads.async = true;
+      gads.type = 'text/javascript';
+      var useSSL = 'https:' == document.location.protocol;
+      gads.src = (useSSL ? 'https:' : 'http:') +
+        '//www.googletagservices.com/tag/js/gpt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(gads, node);
+    })();
+  </script>
+
+  <script>
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+   ga('create', 'UA-74679267-1', 'auto');
+   ga('require', 'GTM-KJ33JM9');
+   ga('require', 'linkid');
+   ga('require', 'displayfeatures');
+   ga('send', 'pageview');
+
+  </script>
+
+<?php // #1860 記事詳細アンカー広告
+echo <<<__EOL__
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8613117509675807",
+    enable_page_level_ads: true
+  });
+</script>
+__EOL__;
+?>
+
+</head>
+<body>
+<div class="whole stats">
+
+
+<?php include_once __DIR__.'/'.$page['template']; ?>
+
+
+</div>
+</body>
+</html>
