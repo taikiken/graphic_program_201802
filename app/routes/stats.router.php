821707b8c0a8c2334caeb55c4d9c089dd21eb4a9
Yoshiaki Fujimori <info@2244.jp>
2017-06-13 17:36:17 +0900

    stats - 開発用に本番環境ではstatsページ表示しない #2016

--- app/routes/stats.router.php
+++ app/routes/stats.router.php
@@ -26,7 +26,10 @@ $app->group('/stats/{category:tennis|golf}', function () use($app) {
       'path'               => $args,
     ));
 
-    return $this->renderer->render($response, 'stats/default.php', $args);
+    if ( UT_ENV !== 'production' ) :
+      return $this->renderer->render($response, 'stats/default.php', $args);
+    endif;
+
   });
 
 });
