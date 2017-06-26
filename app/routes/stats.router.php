bf20846937055892e9c8a34d638fac1fd07db1f6
Yoshiaki Fujimori <info@2244.jp>
2017-06-26 19:43:23 +0900

    stats - ogp画像設定 #2016

--- app/routes/stats.router.php
+++ app/routes/stats.router.php
@@ -10,18 +10,21 @@ $app->group('/stats/{category:tennis}', function () use($app) {
       $category = array(
         'title' => 'テニス | 速報 &amp; データ',
       );
+      $og_image = 'https://sportsbull.jp/_/stats/tennis/ogp_tennis.jpg';
     endif;
 
     if ( $args['category'] === 'golf' ) :
       $category = array(
         'title' => 'ゴルフ | 速報 &amp; データ',
       );
+      $og_image = 'https://sportsbull.jp/_/stats/golf/ogp_golf.jpg';
     endif;
 
     $args['page'] = $app->model->set(array(
       'title'              => $category['title'],
       'og_title'           => $category['title'].' | '.$app->model->property('title_short'),
       'og_url'             => $app->model->property('site_url').'stats/'.$args['category'].'/',
+      'og_image'           => $og_image,
       'template'           => $args['category'].'/index.php',
       'path'               => $args,
     ));
