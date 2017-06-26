87a5e80e0758649f0f05d428a352f8da93d78cf9
taikiken <o@parachute-canopy.org>
2017-06-26 23:02:40 +0900

    mobile top stats banner #2080

--- app/templates/desktop/index.php
+++ app/templates/desktop/index.php
@@ -7,20 +7,7 @@
 // 一面タブからの導線を増やす #2080
 // @see https://github.com/undotsushin/undotsushin/issues/2080
 // @since 2017-06-26
-?>
-<aside class="stats_banner">
-  <div class="stats_banner-heading">
-    <h2 class="stats_banner-heading-title"><img src="/assets/images/index/stats_bnr-heading.png" alt="Breaking News &amp; Stats"></h2>
-    <span class="stats_banner-heading-ruby">速報 &amp; データ</span>
-  </div><!-- /.stats_banner-heading -->
-
-  <div id="js-stats_banner-list"></div>
-
-  <div class="stats_banner-btn">
-    <a class="stats_banner-btn-link" href="/stats/"><span>すべての速報&amp;データを見る</span></a>
-  </div><!-- /.stats_banner-btn -->
-</aside>
-<?php
+include_once __DIR__ . '/_stats_banner.php';
 // ------------------------------------------------------------
 ?>
 <?php
