f4367e05aab237562dce9e4cb6dbe91aad668af0
Yoshiaki Fujimori <info@2244.jp>
2017-06-26 17:58:42 +0900

    stats - 本番ならテンプレ渡さないオフる #2016

--- app/routes/stats.router.php
+++ app/routes/stats.router.php
@@ -26,9 +26,7 @@ $app->group('/stats/{category:tennis|golf}', function () use($app) {
       'path'               => $args,
     ));
 
-    if ( UT_ENV !== 'production' ) :
-      return $this->renderer->render($response, 'stats/default.php', $args);
-    endif;
+    return $this->renderer->render($response, 'stats/default.php', $args);
 
   });
 
