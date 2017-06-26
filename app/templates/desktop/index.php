043b17074e81a544fcaa4b196b5237644f26db44
taikiken <o@parachute-canopy.org>
2017-06-26 22:23:46 +0900

    top バナー一覧出力準備

--- app/templates/desktop/index.php
+++ app/templates/desktop/index.php
@@ -4,6 +4,26 @@
   <div class="body-sec-inner">
     <section class="main-sec">
 <?php
+// 一面タブからの導線を増やす #2080
+// @see https://github.com/undotsushin/undotsushin/issues/2080
+// @since 2017-06-26
+?>
+<aside class="stats_banner">
+  <div class="stats_banner-heading">
+    <h2 class="stats_banner-heading-title"><img src="/assets/images/index/stats_bnr-heading.png" alt="Breaking News &amp; Stats"></h2>
+    <span class="stats_banner-heading-ruby">速報 &amp; データ</span>
+  </div><!-- /.stats_banner-heading -->
+
+  <div id="js-stats_banner-list"></div>
+
+  <div class="stats_banner-btn">
+    <a class="stats_banner-btn-link" href="/stats/"><span>すべての速報&amp;データを見る</span></a>
+  </div><!-- /.stats_banner-btn -->
+</aside>
+<?php
+// ------------------------------------------------------------
+?>
+<?php
 // https://github.com/undotsushin/undotsushin/issues/1210
 // CMS から一面・すべてのバナーを設定できるようにする #1210
 // @since 2016-11-02
