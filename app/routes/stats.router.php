c9a19514293ccb2c4b89d1df713fb87f5e11bd15
Yoshiaki Fujimori <info@2244.jp>
2017-06-27 14:16:44 +0900

    mlb - テンプレルーター #2098

--- app/routes/stats.router.php
+++ app/routes/stats.router.php
@@ -35,4 +35,39 @@ $app->group('/stats/{category:tennis|golf}', function () use($app) {
 
 });
 
+
+
+
+// stats
+// ==============================
+$app->group('/stats/mlb', function () use($app) {
+
+  // トップ
+  $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+      'template' => 'mlb/schedule.php',
+      'path'     => $args,
+    ));
+
+    return $this->renderer->render($response, 'stats/default.php', $args);
+
+  });
+
+  // ヒットする文字列だけ
+  $this->get('/{category:schedule|standing|leaders|playerlist}[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+      'template' => 'mlb/'.$args['category'].'.php',
+      'path'     => $args,
+    ));
+
+    return $this->renderer->render($response, 'stats/default.php', $args);
+
+  });
+
+
+});
+
+
 ?>
