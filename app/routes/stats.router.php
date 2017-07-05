7afcd1debd11a2a3a1588879e15b7d687b1c0472
Kakoi <k@parachute-canopy.org>
2017-07-05 13:03:48 +0900

    MLBスタッツ : meta周り実装 #2098

--- app/routes/stats.router.php
+++ app/routes/stats.router.php
@@ -69,7 +69,6 @@ $app->group('/stats', function () use($app) {
   });
 
 
-  
   // MLB
   // ==============================
   $this->group('/mlb', function ($request, $response, $args) use ( $app ) {
@@ -77,9 +76,16 @@ $app->group('/stats', function () use($app) {
     // トップ
     $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app) {
 
+      $title = 'MLB | 速報 &amp; データ';
+
       $args['page'] = $app->model->set(array(
-        'template' => 'mlb/schedule.php',
-        'path'     => $args,
+        'title'              => $title,
+        'og_title'           => $title.' | '.$app->model->property('title_short'),
+        'og_description'     => 'MLB 速報 &amp; データ見るならスポーツブルで。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
+        'og_url'             => $app->model->property('site_url').'stats/mlb/',
+        'og_image'           => $app->model->property('site_url').'assets/images/stats/mlb/og_image.png',
+        'template'           => 'mlb/schedule.php',
+        'path'               => $args,
       ));
 
       return $this->renderer->render($response, 'stats/default.php', $args);
@@ -89,9 +95,18 @@ $app->group('/stats', function () use($app) {
     // ヒットする文字列だけ
     $this->get('/{category:schedule|standing|leaders|playerlist}[/]', function ($request, $response, $args) use ($app) {
 
+      $category = array(
+        'title' => 'MLB | 速報 &amp; データ',
+      );
+
       $args['page'] = $app->model->set(array(
-        'template' => 'mlb/'.$args['category'].'.php',
-        'path'     => $args,
+        'title'              => $category['title'],
+        'og_title'           => $category['title'].' | '.$app->model->property('title_short'),
+        'og_description'     => 'MLB 速報 &amp; データ見るならスポーツブルで。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
+        'og_url'             => $app->model->property('site_url').'stats/mlb/'.$args['category'].'/',
+        'og_image'           => $app->model->property('site_url').'assets/images/stats/mlb/og_image.png',
+        'template'           => 'mlb/'.$args['category'].'.php',
+        'path'               => $args,
       ));
 
       return $this->renderer->render($response, 'stats/default.php', $args);
