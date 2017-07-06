b693e00cb6d4f01f97e01ca5138f015f5bb9bc62
Kakoi <k@parachute-canopy.org>
2017-07-06 16:13:28 +0900

    F1のview進捗 #1914

--- public/motorsports/f1/view_desktop.php
+++ public/motorsports/f1/view_desktop.php
@@ -15,23 +15,45 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=1280">
   <script src="/assets/js/libs/sagen/sagen.min.js?v=<?php echo $page['version']; ?>" id="sagen" data-browser="true" data-orientation="true"></script>
-  <title>XXX | SPORTS BULL</title>
-  <meta name="description" content="XXX">
-  <meta name="keywords" content="XXX">
+  <title><?php echo strip_tags($page['title']).' | '.$page['site_name']; ?></title>
+  <meta name="keywords" content="<?php echo $page['keywords']; ?>">
+  <meta name="description" content="<?php echo $page['og_description']; ?>">
   <!-- sns ogp -->
-  <meta property="og:title" content="XXX | SPORTS BULL">
+  <meta property="fb:app_id" content="<?php echo $page['app_id']; ?>">
+  <meta property="og:site_name" content="<?php echo $page['site_name']; ?>">
   <meta property="og:type" content="<?php echo $page['og_type']; ?>">
-  <meta property="og:image" content="XXX">
-  <meta property="og:url" content="https://sportsbull.jp/motorsports/<?php echo $option_directory; ?>/">
-  <meta property="og:description" content="XXX">
+  <meta property="og:title" content="<?php echo $page['og_title']; ?>">
+  <meta property="og:image" content="<?php echo $page['og_image']; ?>">
+  <meta property="og:url" content="<?php echo $page['og_url']; ?>">
+  <meta property="og:description" content="<?php echo $page['og_description']; ?>">
+  <meta property="og:locale" content="ja_JP" />
   <!-- twitter card -->
   <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="@sportsbull_jp">
+  <meta name="twitter:site" content="@<?php echo $page['sns']['twitter']; ?>">
+  <meta name="twitter:title" content="<?php echo $page['og_title']; ?>">
+  <meta name="twitter:image" content="<?php echo $page['og_image']; ?>">
+  <meta name="twitter:url" content="<?php echo $page['og_url']; ?>">
+  <meta name="twitter:description" content="<?php echo $page['og_description']; ?>">
   <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="apple-touch-icon-precomposed" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="icon" sizes="192x192" href="/assets/sp/images/common/apple-touch-icon.png">
   <link rel="shortcut icon" href="/favicon.ico">
 
   <link rel="canonical" href="https://sportsbull.jp/motorsports/<?php echo $option_directory; ?>/">
   <link rel="stylesheet" href="/assets/css/ui.css?v=<?php echo $page['version']; ?>">
+
+  <script src="/assets/js/libs/vendor.react.js?v=<?php echo $page['version']; ?>"></script>
+  <script src="/assets/js/bundle/main.bundle.js?v=<?php echo $page['version']; ?>"></script>
+
+  <?php // #1876 - Google Optimize ?>
+  <style>.async-hide { opacity: 0 !important} </style>
+  <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+  h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+  (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+  })(window,document.documentElement,'async-hide','dataLayer',4000,
+  {'GTM-KJ33JM9':true});</script>
+  <?php // Google Optimize ?>
 
   <script type='text/javascript'>
     var googletag = googletag || {};
@@ -49,20 +71,22 @@
   </script>
 
   <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-74679267-1', 'auto');
-    ga('require', 'linkid');
-    ga('require', 'displayfeatures');
-    ga('send', 'pageview');
-
+   ga('create', 'UA-74679267-1', 'auto');
+   ga('require', 'GTM-KJ33JM9');
+   ga('require', 'linkid');
+   ga('require', 'displayfeatures');
+   ga('send', 'pageview');
   </script>
 </head>
 <body>
+
 <div id="whole" class="whole dark layout-list motorsports motorsports-<?php echo $option_directory; ?>">
+
   <header class="head-sec">
     <div class="head-sec-inner">
       <h1><a href="/">SPORTS BULL</a></h1>
@@ -95,7 +119,7 @@
 
       <section class="side-sec">
         <?php
-//        include_once __DIR__."/../../../app/templates/desktop/_sidebar_ad.php";
+       include_once __DIR__."/../../../app/templates/desktop/_sidebar_ad.php";
         ?>
       </section><!-- /.side-sec -->
 
@@ -105,6 +129,18 @@
 // -------------------------------------------------------------------------------
 ?>
 
-</div><!-- /.whole -->
-</body>
-</html>
+<?php
+
+// テンプレートの種類を挿入
+$page = array(
+  'template' => 'category',
+);
+
+include_once __DIR__."/../../../app/templates/desktop/_footer.php";
+include_once __DIR__."/../../../app/templates/_debug.php";
+
+?>
+
+<?php
+// var_dump($page);
+?>
