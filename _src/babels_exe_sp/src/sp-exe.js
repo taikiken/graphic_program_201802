f8ef69ae2b916d1698617bfdceeff80f77190c04
taikiken <o@parachute-canopy.org>
2017-07-10 18:49:49 +0900

    dector 追加 #2031

--- _src/babels_exe_sp/src/sp-exe.js
+++ _src/babels_exe_sp/src/sp-exe.js
@@ -7,9 +7,43 @@
  * http://www.opensource.org/licenses/mit-license.html
  *
  * This notice shall be included in all copies or substantial portions of the Software.
- *
+ * build @@buildTime
+ * version @@version
  */
 'use strict';
+
+
+// @see https://github.com/undotsushin/wiki/blob/master/%E4%BB%95%E6%A7%98/webview.md
+// - iOS : undotsushin-ios
+// - Android : undotsushin-android
+// = ではなく、含まれる、になります。match で判定してください
+
+const detector = () => {
+  // html 取得
+  const tags = document.getElementsByTagName('html');
+  console.log('tags', tags, tags.length);
+  if (!tags || !tags.length) {
+    return;
+  }
+  const html = tags[0];
+  // global object
+  const Sagen = self.Sagen;
+  const ua = Sagen.Browser.ua();
+
+  // android
+  const android = ua.indexOf('undotsushin-android') !== -1;
+  // ios
+  const ios = ua.indexOf('undotsushin-ios') !== -1;
+  // add class
+  if (android) {
+    Sagen.Dom.addClass(html, 'undotsushin-android');
+  } else if (ios) {
+    Sagen.Dom.addClass(html, 'undotsushin-ios');
+  }
+};
+
+detector();
+
 
 /**
  * UT library を使い実行します
