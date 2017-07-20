c73772a8e0eb392f5ade9788bc5292bf9271ab07
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-07-21 08:36:24 +0900

    インハイtemplate作成

---
+++ app/templates/inhigh/mobile/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+        <meta charset="UTF-8">
+        <meta http-equiv="pragma" content="no-cache">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <script src="/assets/js/libs/sagen/sagen.min.js" id="sagen" data-browser="true"></script>
+        <script src="/assets/js/app_divide.bundle.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+        <title>meta仮インターハイ　トップ　パーツ | SPORTS BULL</title>
+        <script src="/assets/js/libs/vendor.react.js"></script>
+        <script src="/assets/js/bundle/main.bundle.js"></script>
+        <!-- 表示確認用 / -->
+        <link rel="stylesheet" href="/assets/css/tmp/head_foot_demo.css" media="only screen and (min-width: 769px)">
+        <link rel="stylesheet" href="/assets/sp/css/tmp/head_foot_demo.css" media="only screen and (max-width: 768px)">
+        <!-- / 表示確認用 -->
+        <link rel="stylesheet" href="/assets/css/basic.css" media="only screen and (min-width: 769px)">
+        <link rel="stylesheet" href="/assets/sp/css/basic.css" media="only screen and (max-width: 768px)">
+
+        <!-- インターハイ css -->
+        <link rel="stylesheet" href="/assets/css/inhigh/parts_sp.css">
+        <!-- / インターハイ css -->
+        <script type='text/javascript'>
+            var googletag = googletag || {};
+            googletag.cmd = googletag.cmd || [];
+            (function() {
+                var gads = document.createElement('script');
+                gads.async = true;
+                gads.type = 'text/javascript';
+                var useSSL = 'https:' == document.location.protocol;
+                gads.src = (useSSL ? 'https:' : 'http:') +
+                    '//www.googletagservices.com/tag/js/gpt.js';
+                var node = document.getElementsByTagName('script')[0];
+                node.parentNode.insertBefore(gads, node);
+            })();
+        </script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-74679267-1', 'auto');
+            ga('require', 'linkid');
+            ga('require', 'displayfeatures');
+            ga('send', 'pageview');
+        </script>
+        <script>
+            googletag.cmd.push(function() {
+                googletag.defineSlot('/531683568/npb-pc-rectangle', [300, 250], 'div-gpt-ad-1492577512561-0').addService(googletag.pubads());
+                googletag.pubads().enableSingleRequest();
+                googletag.pubads().collapseEmptyDivs();
+                googletag.enableServices();
+            });
+        </script>
+        <!-- ad/npb-sp-anchor -->
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-8613117509675807",
+                enable_page_level_ads: true
+            });
+        </script>
+        <!-- // ad/npb-sp-anchor -->
+    </head>
+    <body>
+        <div id="apvad-9881b571b6869b1815c4f16e1a2adbd4"></div>
+        <script type="text/javascript" src="//cdn.apvdr.com/js/VastAdUnit.min.js"></script>
+        <script>;(function(){new APV.VASTAdUnit({s:"9881b571b6869b1815c4f16e1a2adbd4", format:APV.AD_FORMAT.OVERLAY}).load();})();</script>
+
+                       
+<!--  インターハイ トップ　パーツ ---------------------------------------------------------->
+                       
+                       
+
+                        <section class="section_interhigh_highlight">
+                            <div class="ttl-wrapper">
+                                <h2 class="ttl highlight"><i></i>動画特集</h2>
+                            </div>
+
+                            <div class="article_list">
+                                <article class="highlight_article">
+
+                                    <ul class="thumb_area">
+                                        <li><a href="">
+                                            <div class="img"><img src="/assets/images/inhigh/thumb_01.png" alt=""></div>
+                                            <div class="txt_area"><p>7/31 長文テキスト7/31 長文テキスト7/31 長文テキスト7/31</p></div>
+                                            </a></li>
+
+                                        <li><a href="">
+                                            <div class="img"><img src="/assets/images/inhigh/thumb_02.png" alt=""></div>
+                                            <div class="txt_area"><p>7/31 バスケットボール</p></div>
+                                            </a></li>
+
+                                        <li><a href="">
+                                            <div class="img"><img src="/assets/images/inhigh/thumb_03.png" alt=""></div>
+                                            <div class="txt_area"><p>7/31 ハイライト総集編</p></div>
+                                            </a></li>
+
+                                        <li><a href="">
+                                            <div class="img"><img src="/assets/images/inhigh/thumb_04.png" alt=""></div>
+                                            <div class="txt_area"><p>7/31 ハイライト</p></div>
+                                            </a></li>
+                                    </ul>
+                                </article>
+                            </div>
+
+                            <p class="section_btn"><a href=""><i></i>すべての動画を見る</a></p>
+                        </section>
+                       
+                       
+                       
+                       
+            <!--------------  section_photogallery  -------------->
+                        <section class="section_interhigh_photo">
+                            <div class="ttl-wrapper">
+                                <h2 class="ttl photo"><i></i>フォトギャラリー</h2>
+                            </div>
+                            
+                            <div id="js-current-post" class="current-post photo_gallery">
+                                <ul class="photo_list">
+                                    <li><a href="">
+                                        <div class="img"><img src="/assets/images/inhigh/thumb_01.png" alt=""></div>
+                                        <p class="txt">長文テキスト長文テキスト長文テキスト長文テキスト</p>
+                                        </a></li>
+                                    <li><a href="">
+                                        <div class="img"><img src="/assets/images/inhigh/photo_03.jpg" alt=""></div>
+                                        <p class="txt">2017.07.28</p>
+                                        </a></li>
+                                </ul>
+                            </div>
+                            <p class="section_btn"><a href=""><i></i>すべてのフォトギャラリーを見る</a></p>
+                            
+                        </section>
+
+                        <div class="interhigh_bnr"><a href=""><img src="/assets/images/inhigh/bnr_640x100.png" alt="" class="pc_hide"></a></div>
+
+<!-- / インターハイ トップ　パーツ ---------------------------------------------------------->
+
+
+        <!-- for facebook -->
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '842032129256034',
+                    xfbml      : true,
+                    version    : 'v2.5'
+                });
+            };
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/ja_JP/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <script src="/assets/js/global.bundle.js"></script>
+        <script type="text/javascript" src="//cdn.apvdr.com/js/apv-ifbstr.min.js"></script>
+    </body>
+</html>
