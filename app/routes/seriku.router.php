7e4c0eaf0b42a07f45d21f24a8530db7454d40be
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-07-26 15:25:35 +0900

    ルーター追加

---
+++ app/routes/seriku.router.php
@@ -0,0 +1,79 @@
+<?php
+
+// #inhigh.router.php seriku
+// ==============================
+$app->group('/{slug:seriku}',  function () use($app) {
+
+
+  // /seriku/
+  // ==============================
+  $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app) {
+    $args['page'] = $app->model->set(array(
+      'title'              => '世界陸上',
+      'og_title'           => '世界陸上 | '.$app->model->property('title'),
+      'og_url'             => $app->model->property('site_url').'seriku/',
+      'path'               => $args,
+      'template'           => 'category',
+      'template_classname' => '',
+    ));
+
+    return $this->renderer->render($response, 'seriku/index.php', $args);
+
+  });
+
+
+  // peking
+  // ==============================
+  $this->get('/peking[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+      'title'              => '世界陸上 - 北京大会ハイライト',
+      'og_title'           => '世界陸上 - 北京大会ハイライト |'.$app->model->property('title'),
+      'og_url'             => $app->model->property('site_url').'seriku/peking/',
+      'path'               => $args,
+      'template'           => 'category',
+      'template_classname' => '',
+    ));
+
+    return $this->renderer->render($response, 'seriku/peking.php', $args);
+
+  });
+
+
+  // world-record
+  // ==============================
+  $this->get('/world-record[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+      'title'              => '世界陸上 - 世界陸上で生まれた世界記録',
+      'og_title'           => '世界陸上 - 世界陸上で生まれた世界記録 | '.$app->model->property('title'),
+      'og_url'             => $app->model->property('site_url').'seriku/world-record/',
+      'path'               => $args,
+      'template'           => 'category',
+      'template_classname' => '',
+    ));
+
+    return $this->renderer->render($response, 'seriku/world-record.php', $args);
+
+  });
+
+
+  // webview
+  // ==============================
+  $this->get('/webview[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+      'title'              => '世界陸上',
+      'og_title'           => '世界陸上 | '.$app->model->property('title'),
+      'path'               => $args,
+      'template'           => 'webview',
+      'template_classname' => '',
+    ));
+
+    return $this->renderer->render($response, 'seriku/webview.php', $args);
+
+  });
+
+});
+
+?>
