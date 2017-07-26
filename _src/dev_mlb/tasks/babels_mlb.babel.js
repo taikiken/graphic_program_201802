646de04119abdfba877423dd9a75c57b43d89428
taikiken <o@parachute-canopy.org>
2017-07-26 23:24:06 +0900

    test 残す

--- _src/dev_mlb/tasks/babels_mlb.babel.js
+++ _src/dev_mlb/tasks/babels_mlb.babel.js
@@ -159,7 +159,7 @@ gulp.task('mlb:copy', () => (
     // .pipe(gulp.dest(dir.dist.root))
     // .pipe(gulp.dest(dir.app.js))
     .pipe(gulp.dest(`${dir.dist.assets.mlb}/js`))
-    // .pipe(gulp.dest(`${dir.root}/test/assets/js`))
+    .pipe(gulp.dest(`${dir.root}/test/assets/js`))
     // .pipe(gulp.dest(`${dir.root}/app/assets/js`))
     .pipe($.size({ title: '*** mlb:copy ***' }))
   ),
