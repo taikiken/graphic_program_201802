6d7a7f5741ccb67cc1a1797d28f8bf772b70dfff
saegusamakoto <saegusa@kiito.co.jp>
2017-07-28 22:32:00 +0900

    inhighユーザ切り替え ファイル変更し忘れ

--- include/glmenu.php
+++ include/glmenu.php
@@ -1,4 +1,5 @@
 <li class="pl0"><a href="/editdm/">TOP</a></li>
+<?php if(getSorC("usr")!="inhigh"){ ?>
 <li><a href="/editdm/ad/?cid=0&nid=0">広告設定</a></li>
 <li><a href="/editdm/repo_n/?cid=8&rid=7">TOPカルーセル</a></li>
 <li><a href="/editdm/repo_n/?cid=9&rid=7">TOPピックアップ</a></li>
@@ -10,6 +11,9 @@
 <li><a href="/editdm/repo_n/?cid=6">会員</a></li>
 <li><a href="/editdm/comment/">コメント</a></li>
 <li><a href="/editdm/link/">記事取得</a></li>
+<?php }else{ ?>
+<li><a href="/editdm/photo/">フォトアルバム</a></li>
+<?php } ?>
 <!--
 <li><a href="/editdm/repo_s/?rid=48">番組マスタ</a></li>
 <li class="pickup"><a href="/editdm/pickup/">ピックアップ</a></li>
