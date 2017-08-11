95c20abdb1d06de0c4186e296e36463a8bdeb34b
Yoshiaki Fujimori <info@2244.jp>
2017-08-11 12:27:08 +0900

    redbull - ルーティング追加 #2279

---
+++ app/templates/red-bull-holy-ride/desktop/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo $page['og_type']; ?>: http://ogp.me/ns/<?php echo $page['og_type']; ?>#">
+  <meta charset="UTF-8">
+
+  <?php include_once __DIR__.'/../../_head.php'; ?>
+
+  <script src="/assets/js/libs/vendor.react.js?v=<?php echo $page['version']; ?>"></script>
+  <script src="/assets/js/bundle/main.bundle.js?v=<?php echo $page['version']; ?>"></script>
+  <link rel="stylesheet" href="/assets/css/ui.css?v=<?php echo $page['version']; ?>">
+
+  <script type='text/javascript'>
+    var googletag = googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    (function() {
+      var gads = document.createElement('script');
+      gads.async = true;
+      gads.type = 'text/javascript';
+      var useSSL = 'https:' == document.location.protocol;
+      gads.src = (useSSL ? 'https:' : 'http:') +
+        '//www.googletagservices.com/tag/js/gpt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(gads, node);
+    })();
+  </script>
+
+  <script>
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+   ga('create', 'UA-74679267-1', 'auto');
+   ga('require', 'GTM-KJ33JM9');
+   ga('require', 'linkid');
+   ga('require', 'displayfeatures');
+   ga('send', 'pageview');
+
+  </script>
+</head>
+<body>
+
+<div id="whole" class="whole <?php echo $page['template_classname']; ?>">
+
+  <header class="head-sec">
+    <div class="head-sec-inner">
+      <h1><a href="/">SPORTS BULL</a></h1>
+    </div><!-- /.head-sec-inner -->
+  </header><!-- /.head-sec -->
+
+  <div class="body-sec">
+    <div class="body-sec-inner">
+      <section class="main-sec">
+
+        <?php include_once __DIR__."/../live.php"; ?>
+
+      </section><!-- /.main-sec -->
+
+      <section class="side-sec">
+        <?php include_once __DIR__."/../../desktop/_sidebar_ad.php"; ?>
+      </section><!-- /.side-sec -->
+
+    </div>
+  </div><!-- /.body-sec -->
+
+
+<?php
+
+include_once __DIR__."/../../desktop/_footer.php";
+include_once __DIR__."/../../_debug.php";
+
+?>
---
+++ app/templates/red-bull-holy-ride/mobile/index.php
@@ -0,0 +1,169 @@
+<?php
+// app webview かを `?app=(ios|android)` から判定します
+// ==============================
+$from_webview = false;
+if (isset($_GET['app'])) {
+  if ($_GET['app'] == 'ios' || $_GET['app'] == 'android') {
+    $from_webview = true;
+  }
+}
+?>
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo $page['og_type']; ?>: http://ogp.me/ns/<?php echo $page['og_type']; ?>#">
+  <meta charset="UTF-8">
+
+  <?php include_once __DIR__.'/../../_head.php'; ?>
+
+  <script src="/assets/js/libs/jquery2/jquery.min.js?v=<?php echo $page['version']; ?>"></script>
+  <link rel="stylesheet" href="/assets/sp/css/ui.css?v=<?php echo $page['version']; ?>">
+
+  <script type='text/javascript'>
+    var googletag = googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    (function() {
+      var gads = document.createElement('script');
+      gads.async = true;
+      gads.type = 'text/javascript';
+      var useSSL = 'https:' == document.location.protocol;
+      gads.src = (useSSL ? 'https:' : 'http:') +
+        '//www.googletagservices.com/tag/js/gpt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(gads, node);
+    })();
+  </script>
+
+  <script>
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+   ga('create', 'UA-74679267-1', 'auto');
+   ga('require', 'GTM-KJ33JM9');
+   ga('require', 'linkid');
+   ga('require', 'displayfeatures');
+   ga('send', 'pageview');
+
+  </script>
+</head>
+<body class="appbnr-disable">
+<div class="whole <?php echo $page['template_classname']; ?>">
+  <?php
+  // app in webview 時に .head-sec を非表示にする
+  if (!$from_webview) :
+  ?>
+  <header class="head-sec">
+    <div class="head-sec-inner">
+      <h1><a href="/">SPORTS BULL</a></h1>
+    </div><!-- /.head-sec-inner -->
+  </header><!-- /.head-sec -->
+  <?php
+  endif;
+  // -----------------------------------------
+  ?>
+
+<?php
+// ------------------------------------------------
+// SP カテゴリー
+// ------------------------------------------------
+?>
+<div id="body-section" class="body-sec">
+  <div class="body-sec-inner">
+
+    <section class="main-sec">
+
+      <?php include_once __DIR__."/../live.php"; ?>
+
+    </section><!-- /.main-sec -->
+
+  </div>
+</div><!-- /.body-sec -->
+
+  <?php
+  // app in webview 時に .foot-sec を非表示にする
+  if (!$from_webview) :
+  ?>
+  <footer class="foot-sec">
+    <div class="foot-sec-inner">
+
+      <div class="foot-pr">
+        <div class="foot-pr-inner">
+          <figure class="foot-pr-logo"><img src="/assets/sp/images/common/footer-overview-logo.png" alt="SPORTS BULL"></figure>
+          <div class="text-block">
+            <h3 class="foot-pr-heading">スポーツブルアプリをダウンロード</h3>
+            <ul class="foot-pr-list">
+              <li class="foot-pr-item"><a class="foot-pr-link" href="https://itunes.apple.com/jp/app/undotsushin/id1086719653?l=ja&ls=1&mt=8" target="_blank"><img src="/assets/sp/images/common/footer-overview-btn-applestore.png" alt="App Store" /></a></li>
+              <li class="foot-pr-item"><a class="foot-pr-link" href="https://play.google.com/store/apps/details?id=com.undotsushin" target="_blank"><img src="/assets/sp/images/common/footer-overview-btn-googleplay.png" alt="Google play"></a></li>
+            </ul>
+          </div>
+        </div><!-- /.foot-pr-inner -->
+
+        <div class="fb-page-plugin">
+          <div class="fb-page" data-href="https://www.facebook.com/sportsbull/" data-width="500" data-height="154" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/sportsbull/"><a href="https://www.facebook.com/sportsbull/">スポーツブル（SPORTS BULL）</a></blockquote></div></div>
+        </div>
+      </div><!-- /.foot-pr -->
+
+      <div id="js-page_top" class="pagetop"><a href="#"><span>このページの先頭へ</span></a></div>
+
+      <nav class="fnav">
+        <ul>
+          <li><a href="/about/">サービス紹介</a></li>
+          <li><a href="/about/privacy/">プライバシーポリシー</a></li>
+          <li><a href="/about/company/">会社概要</a></li>
+          <li><a href="/about/terms/">利用規約</a></li>
+        </ul>
+      </nav><!-- /.fnav -->
+
+      <div class="sns-block">
+        <ul>
+          <li class="sns-fb"><a href="https://www.facebook.com/sportsbull/" target="_blank">facebook</a></li>
+          <li class="sns-tw"><a href="https://twitter.com/sportsbull_jp" target="_blank">twitter</a></li>
+          <li class="sns-yt"><a href="https://www.youtube.com/channel/UCKwqba9IWuSKIk3DIpryOHw" target="_blank">youtube</a></li>
+        </ul>
+      </div><!-- /.sns-block -->
+
+      <p class="copyright">Copyright &copy; SPORTS BULL All rights reserved.</p>
+    </div><!-- /.foot-sec-inner -->
+  </footer><!-- /.foot-sec -->
+  <?php
+  // -----------------------------------------
+  endif;
+  ?>
+
+</div><!-- /.whole -->
+
+<?php
+// app in webview 時に .foot-sec を非表示にするので FB いらない
+if (!$from_webview) :
+?>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '842032129256034',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+<?php
+endif;
+// -----------------------------------------
+?>
+
+<script src="/assets/js/picks.bundle.js?v=<?php echo $page['version']; ?>"></script>
+
+</body>
+</html>
+
+<?php
+include_once __DIR__."/../../_debug.php";
+?>
