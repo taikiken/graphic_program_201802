6efd2a347cdaec8a6c0f2153988e9f44a198d7d8
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-08-23 23:11:50 +0900

    Webviewにも小見出し追加

--- app/templates/stats/ua_kansai/webview.php
+++ app/templates/stats/ua_kansai/webview.php
@@ -17,6 +17,10 @@
 </head>
 
 <body style="height: auto;">
+
+<div class="sub_head_wrapper" style="margin-top: 0;">
+  <h2 class="sub_head_title"><i></i>関西学生アメリカンフットボールリーグ</h2>  
+</div>
                        
 <!-- section_highlight_movie -->                      
 <section class="section_interhigh_highlight">
