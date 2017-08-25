c8153cb467353999c526f629ac1376c01cd41af7
YuFukuda <y_fukuda@cloudpack.jp>
2017-08-25 13:55:59 +0900

    jsonとってきた viewに渡したい

--- app/routes/big6tv.router.php
+++ app/routes/big6tv.router.php
@@ -1,5 +1,7 @@
 <?php
 
+use Aws\S3\S3Client;
+
 // big6tv
 // ==============================
 $app->group('/{slug:big6tv}',  function () use($app) {
@@ -47,11 +49,52 @@ $app->group('/{slug:big6tv}',  function () use($app) {
   });
 
 
-  // metatag
+  // game
   // ==============================
-  $this->get('/metatag[/]', function ($request, $response, $args) use ($app) {
+  $this->get('/game/{gameid:[A-Z][A-Z][0-9][0-9]}[/]', function ($request, $response, $args) use ($app) {
 
-      $json = '';
+      // パスからjson決めるソン
+      $url = explode('/', $_SERVER['REQUEST_URI']);
+      $league = $url[1];
+      $gameid = $args['gameid'];
+      $arr = [
+          'json',
+          $league,
+          '2017s', // 今はシーズン固定にしちゃってる
+          'game_info_' . $gameid . '.json',
+      ];
+      $key = implode('/', $arr);
+
+      // AWSのキー名
+      $keyId = 'AKIAJ7OMTZRU6PGV6GZA';
+      // シークレットキー
+      $secretKey = 'T5fLQ2MKuHLaC+5FSK2iCQWB7MDHqthnBlOMs6U5';
+      // region 東京region指定
+      $region = 'ap-northeast-1';
+      $version = 'latest';
+      $bucket = 'dev-ublive.sportsbull.jp';
+//
+      $s3Setting = [
+          'credentials' => [
+              'key' => $keyId,
+              'secret' => $secretKey,
+          ],
+          'region' => $region,
+          'version' => $version,
+      ];
+
+      $s3Object = S3Client::factory($s3Setting);
+      $json = $s3Object->getObjectUrl($bucket, $key);
+
+      // こっち使えないかなー
+//      $S3Module = new S3Module;
+//      $json = $S3Module->getUrl($bucket, $key);
+
+      if (!@file_get_contents($json, NULL, NULL, 0, 1)) {
+          // 404飛ばしたい
+          return $this->renderer->render($response, 'desktop/404.php', $args)->withStatus(404);
+      }
+      $json = json_decode(file_get_contents($json));
 
       $args['page'] = $app->model->set(array(
           'title'              => '東京六大学野球 BIG6.TV',
@@ -64,9 +107,9 @@ $app->group('/{slug:big6tv}',  function () use($app) {
       ));
 
       if ( $app->model->property('ua') === 'desktop' ) :
-          return $this->renderer->render($response, 'big6tv/desktop/metatag.php', $args);
+          return $this->renderer->render($response, 'big6tv/desktop/game.php', $args);
       else :
-          return $this->renderer->render($response, 'big6tv/mobile/metatag.php', $args);
+          return $this->renderer->render($response, 'big6tv/mobile/game.php', $args);
       endif;
 
   });
