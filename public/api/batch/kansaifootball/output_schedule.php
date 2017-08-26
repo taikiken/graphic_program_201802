c4610a42c5dd4b7c2185f410bafb55b3d5c45e9e
saegusamakoto <saegusa@kiito.co.jp>
2017-08-26 17:05:47 +0900

    JSONの提供が間に合わないので、それまでエクセルで更新するためのプログラム

---
+++ public/api/batch/kansaifootball/output_schedule.php
@@ -0,0 +1,65 @@
+<?php
+
+include "local.php";
+include "inc.php";
+
+$csv =sprintf("%s/csv/schedule.csv",$bucket);
+$json=sprintf("%s/schedule.json",$bucket);
+
+$lastupdate=filemtime($csv);
+
+if(!file_exists($json)||$lastupdate>filemtime($json)){
+
+	$data=get_contents($csv);
+	$data=mb_convert_encoding($data,"UTF-8","SJIS");
+	$data=preg_replace("/\r\n|\r|\n/","\n",$data);
+	$tmpfile=sprintf("%s/tmp/schedule.csv",$bucket);
+	file_put_contents($tmpfile,$data);
+	
+	$fp=fopen($tmpfile,"r");
+	while($l=fgetcsv($fp,1024)){
+		if(!preg_match("/^#/",$l[0])){
+			$sc[$l[0]][$l[1]][]=$l;
+		}
+	}
+	foreach($sc as $kk=>$vv){
+		foreach($vv as $k=>$v){
+			$league["date"]=$k;
+			$league["weekday"]=$v[0][2];
+			$game=array();
+			for($i=0;$i<count($v);$i++){
+				$game[]=array(
+					"gameid"=>$v[$i][5],
+					"time"=>$v[$i][3],
+					"team"=>array(
+						array(
+							"id"=>$univ[$v[$i][6]]["id"],
+							"name"=>$univ[$v[$i][6]]["name"],
+							"score"=>$v[$i][7]
+						),
+						array(
+							"id"=>$univ[$v[$i][8]]["id"],
+							"name"=>$univ[$v[$i][8]]["name"],
+							"score"=>$v[$i][9]
+						)
+					)
+				);
+				$league["games"]=$game;
+			}
+			$schedules[]=$league;
+		}
+		$schedule[]=array(
+			"leaguetype"=>$kk,
+			"league"=>$schedules,
+		);
+	}
+	
+	$y["response"]["lastupdate"]=date("Y-m-d H:i:s",$lastupdate);
+	$y["response"]["schedule"]=$schedule;
+	
+	put_json($json,$y);
+	unlink($tmpfile);
+	
+}
+
+?>
