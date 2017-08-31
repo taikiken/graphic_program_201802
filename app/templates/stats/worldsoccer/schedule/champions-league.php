cee74994b44ca93b42563a05a8cc61c271ead5ce
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-08-31 22:22:02 +0900

    テンプレートファイル追加

---
+++ app/templates/stats/worldsoccer/schedule/champions-league.php
@@ -0,0 +1,26 @@
+<div class="body-sec">
+  <div class="body-sec-inner">
+
+    <section class="main-sec">
+
+      <?php include __DIR__.'/_header.php'; ?>
+
+      <?php include __DIR__.'/_banner.php'; ?>
+
+      <?php include __DIR__.'/_nav.php'; ?>
+
+      <!-- Champions League -->
+      <div>
+        <script src="//widgets.sports.gracenote.com/gns.widget.loader.js?c=1323" data-widget_id="kyodo_sportsbull_eu_soccer2017-jp_schedule-cl"></script>
+      </div>
+      <!-- //Champions League -->
+
+    </section><!-- /.main-sec -->
+
+    <?php include __DIR__.'/_sidebar.php'; ?>
+
+  </div>
+</div><!-- /.body-sec -->
+
+
+<?php include __DIR__.'/_footer.php'; ?>
