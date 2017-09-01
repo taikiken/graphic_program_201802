e889c81316305d3deeb1371dc3732ee5e5efbbdd
Yoshiaki Fujimori <info@2244.jp>
2017-09-01 21:48:29 +0900

    big6tv - 秋版六大のコード最新版に #2319

--- public/big6tv/js/config.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var ApiFormat=exports.ApiFormat={lastupdate:"",ranking:[{rank:null,name:"",nameI:"",game:null,win:null,lose:null,draw:null,point:null,winningPercentage:null}],dataType:null};
-//# sourceMappingURL=config.js.map
