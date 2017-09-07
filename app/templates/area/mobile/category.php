c7639fd513ef59090cc08c3319c8997e67ba3673
taikiken <o@parachute-canopy.org>
2017-09-07 16:37:23 +0900

    イレギュラールールに合わせたテンプレートを用意する #2318

--- app/templates/area/mobile/category.php
+++ app/templates/area/mobile/category.php
@@ -52,68 +52,17 @@
       // eof: 記事一覧: sp banner
       // ---------------------------------------------------- ?>
 
-      <div class="guideboard">
-        <div class="guideboard-heading">
-          <h2 class="guideboard-heading-title">エリアを選択</h2>
-        </div><!-- /.guideboard-heading -->
-
-        <ul class="guideboard__list">
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--hokkaido" href="/area/北海道/">
-              <h3>北海道</h3>
-              <p>北海道</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--tohoku" href="/area/東北/">
-              <h3>東北</h3>
-              <p>青森県・岩手県・宮城県・秋田県・山形県・福島県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--kanto" href="/area/関東/">
-              <h3>関東</h3>
-              <p>茨城県・栃木県・群馬県・埼玉県・千葉県・東京都・神奈川県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--hokuriku" href="/area/北陸・甲信越/">
-              <h3>北陸・甲信越</h3>
-              <p>山梨県・長野県・新潟県・富山県・石川県・福井県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--tokai" href="/area/東海/">
-              <h3>東海</h3>
-              <p>愛知県・岐阜県・三重県・静岡県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--kansai" href="/area/関西/">
-              <h3>関西</h3>
-              <p>三重県・滋賀県・京都府・大阪府・兵庫県・奈良県・和歌山県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--chugoku" href="/area/中国/">
-              <h3>中国</h3>
-              <p>鳥取県・島根県・岡山県・広島県・山口県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--shikoku" href="/area/四国/">
-              <h3>四国</h3>
-              <p>徳島県・香川県・愛媛県・高知県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-          <li class="guideboard__list__item">
-            <a class="guideboard__list__link--kyushu" href="/area/九州・沖縄/">
-              <h3>九州</h3>
-              <p>福岡県・佐賀県・長崎県・熊本県・大分県・宮崎県・鹿児島県・沖縄県</p>
-            </a>
-          </li><!-- /.guideboard__list__item -->
-        </ul><!-- /.guideboard__list -->
-      </div><!-- /.guideboard -->
+      <div class="special-summary">
+        <h1 class="special-summary-heading"><?php echo $page['category']['label']; ?><?php
+          // イレギュラーリンクが追加されたので対応する
+          // `/area/地域名/`
+          // @see https://github.com/undotsushin/undotsushin/issues/2318
+          // @since 2017-09-05
+          if (isset($page['category']['label_area'])) {
+            echo $page['category']['label_area'];
+          }
+          ?></h1>
+      </div>
 
       <div id="js-headline"></div>
       <div id="category-container"></div>
