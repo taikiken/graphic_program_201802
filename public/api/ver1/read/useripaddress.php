4f8497e7822ae50cd2a85dc0774c55d6586dd7eb
misawa <misawa@iret.co.jp>
2017-09-11 21:05:42 +0900

    au回線かの判定処理を追加

--- public/api/ver1/read/useripaddress.php
+++ public/api/ver1/read/useripaddress.php
@@ -20,6 +20,32 @@ if($ipaddress){
 
 }
 $y["response"]["ip"]=$ipaddress;
+$y["response"]["is_au"] = false;
+
+$json = join('/', [$ImgPath, 'json', 'ip_list.json']);
+
+$data = json_decode(file_get_contents($json));
+foreach($data->include as $cidr)
+{
+    list($accept_ip, $mask) = explode('/', $cidr);
+    $accept_long = ip2long($accept_ip) >> (32 - $mask);
+    $remote_long = ip2long($ipaddress) >> (32 - $mask);
+    if($accept_long == $remote_long)
+    {
+        $y["response"]["is_au"] = true;
+    }
+}
+
+foreach($data->exclude as $cidr)
+{
+    list($accept_ip, $mask) = explode('/', $cidr);
+    $accept_long = ip2long($accept_ip) >> (32 - $mask);
+    $remote_long = ip2long($ipaddress) >> (32 - $mask);
+    if($accept_long == $remote_long)
+    {
+        $y["response"]["is_au"] = false;
+    }
+}
 
 print_json($y,$_SERVER['HTTP_REFERER']);
 
