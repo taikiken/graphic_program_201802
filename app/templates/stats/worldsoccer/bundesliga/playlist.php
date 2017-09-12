74c694a76f632dd09c09f277eef74420c765261b
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-09-12 23:15:14 +0900

    テンプレート構成変更

---
+++ app/templates/stats/worldsoccer/bundesliga/playlist.php
@@ -0,0 +1,27 @@
+<?php include __DIR__.'/../_common_header.php'; ?>
+
+<div class="body-sec">
+  <div class="body-sec-inner">
+
+    <section class="main-sec">
+
+      <?php include __DIR__.'/../_header.php'; ?>
+
+      <?php include __DIR__.'/../_banner.php'; ?>
+
+      <?php include __DIR__.'/../_nav.php'; ?>
+
+      <!-- Bundesliga -->
+      <div>
+        <script src="//widgets.sports.gracenote.com/gns.widget.loader.js?c=1323" data-widget_id="kyodo_sportsbull_eu_soccer2017-jp_toplist-1ger"></script>
+      </div>
+      <!-- //Bundesliga -->
+
+    </section><!-- /.main-sec -->
+
+    <?php include __DIR__.'/../_sidebar.php'; ?>
+
+  </div>
+</div><!-- /.body-sec -->
+
+<?php include __DIR__.'/../_footer.php'; ?>
