6fb2d4724eb55a658facce188aba30e2e80d6e9e
Taiki Kumagai <taiki.kumagai@gmail.com>
2017-09-12 23:16:45 +0900

    前の構成削除

--- app/templates/stats/worldsoccer/schedule/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="ja">
-<head>
-  <meta http-equiv="refresh" content="0;URL=/stats/worldsoccer/schedule/premier-league">
-  <title>日程・結果 - 海外サッカー | 速報 & データ | スポーツブル / SPORTS BULL</title>
-</head>
-<body>
-</body>
-</html>
--- app/templates/stats/worldsoccer/team/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="ja">
-<head>
-  <meta http-equiv="refresh" content="0;URL=/stats/worldsoccer/team/premier-league">
-  <title>チーム - 海外サッカー | 速報 & データ | スポーツブル / SPORTS BULL</title>
-</head>
-<body>
-</body>
-</html>
