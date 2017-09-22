edac6d740e9c1a1ca94866f263f17ccc4920d01a
taikiken <o@parachute-canopy.org>
2017-09-22 15:16:14 +0900

    パンくず area 対応

--- app/templates/mobile/p_breadcrumb.php
+++ app/templates/mobile/p_breadcrumb.php
@@ -16,6 +16,8 @@ $content_count = 1;
   <ol itemscope itemtype="http://schema.org/breadCrumbList">
     <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/"><span itemprop="name">TOP</span><meta itemprop="position" content="<?php echo $content_count; ?>" /></a></li>
     <?php
+    // ----------------------------------------------------------------
+    // category label
     // categories 値が空 or NULL の記事がある
     $post_categories = $page['post']['categories'];
     if (is_array($post_categories)) :
@@ -26,13 +28,58 @@ $content_count = 1;
         $post_categories_first_label = $post_categories_first['label'];
 
         if (isset($post_categories_first_slug) && isset($post_categories_first_label)) :
-          $content_count += 1;
+          // area 除外
+          if ($post_categories_first_slug != 'area') :
+            $content_count += 1;
     ?>
       <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/category/<?php echo $post_categories_first_slug; ?>"><span itemprop="name"><?php echo $post_categories_first_label; ?></span><meta itemprop="position" content="<?php echo $content_count; ?>" /></a></li>
     <?php
-        endif;
+          endif;// not area
+        endif;// isset $post_categories_first_slug
+      endif;// isset $post_categories_first
+    endif;// is_array($post_categories)
+    ?>
+    <?php
+    // ----------------------------------------------------------------
+    // another_categories - 地域
+    $post_another_categories_area = $page['post']['another_categories']['area'];
+    // another_categories.area check
+    if (is_array($post_another_categories_area)) :
+      // found another_categories.area
+      // region を集める
+      $area_regions = array();
+      foreach ($post_another_categories_area as $area) :
+        if (isset($area['region'])) {
+          $area_regions[] = $area['region'];
+        }
+      endforeach;
+      if (count($area_regions) >= 1) :
+        // found region
+    ?>
+        <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+          <?php
+          $area_region_first = true;
+          foreach ($area_regions as $area_region) :
+            if (!$area_region_first) {
+              echo ', ';
+            }
+            $content_count += 1;
+          ?>
+          <a itemprop="item" href="<?php echo '/area/' . $area_region . '/'; ?>">
+            <span itemprop="name"><?php echo $area_region; ?></span><meta itemprop="position" content="<?php echo $content_count; ?>" />
+          </a>
+          <?php
+            $area_region_first = false;
+          endforeach;
+          ?>
+        </li>
+    <?php
       endif;
     endif;
+    ?>
+    <?php
+    // ----------------------------------------------------------------
+    // last - this article
     // count up
     $content_count += 1;
     ?>
