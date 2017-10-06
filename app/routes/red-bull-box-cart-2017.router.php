ffc8c84dcd15dbaeaf9d9b8ea08d554a6c0a9229
Kakoi <k@parachute-canopy.org>
2017-10-06 14:16:16 +0900

    ルーティングたたき #2415

---
+++ app/routes/red-bull-box-cart-2017.router.php
@@ -0,0 +1,38 @@
+<?php
+
+// red-bull-holy-ride
+// ==============================
+$app->group('/red-bull-box-cart-2017',  function () use($app) {
+
+  $page = array(
+    'title'              => 'RED BULL BOX CART RACE 2017 ライブ配信',
+    'site_name'          => 'スポーツブル / SPORTS BULL',
+    'og_type'            => 'article',
+    'og_title'           => 'RED BULL BOX CART RACE 2017 ライブ配信 | '.$app->model->property('title'),
+    'og_url'             => $app->model->property('site_url').'red-bull-box-cart-2017/',
+    'og_image'           => $app->model->property('site_url').'assets/images/red-bull-box-cart-2017/ogp.png',
+    'og_description'     => '説明文',
+    'keywords'           => 'キーワード',
+    'template'           => 'index',
+    'template_classname' => 'red-bull-box-cart-2017',
+    'ua'                 => $app->model->property('ua'),
+  );
+
+  // index
+  // ==============================
+  $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app, $page) {
+
+    $args['path'] = $args;
+    $args['page'] = $page;
+
+    if ( $app->model->property('ua') === 'desktop' ) :
+      return $this->renderer->render($response, 'red-bull-box-cart-2017/desktop/index.php', $args);
+    else :
+      return $this->renderer->render($response, 'red-bull-box-cart-2017/mobile/index.php', $args);
+    endif;
+
+  });
+
+});
+
+?>
