bfea61437a4b0db01f6d204bfec9439acbee1e8c
taikiken <o@parachute-canopy.org>
2017-10-21 13:57:30 +0900

    候補選手 template desktop - TODO sp #2574

---
+++ app/templates/draft/players.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+  <meta charset="UTF-8">
+  <meta http-equiv="pragma" content="no-cache">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <script src="/assets/js/libs/sagen/sagen.min.js?v=<?php echo $page['version']; ?>" id="sagen" data-browser="true"></script>
+  <script src="/assets/js/app_ua_detector.bundle.js?v=<?php echo $page['version']; ?>"></script>
+  <?php if ( $page['ua'] == 'mobile' ) : ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+  <?php else : ?>
+    <meta name="viewport" content="width=1280">
+  <?php endif; ?>
+  <title>プロ野球2017 ドラフト候補選手 | スポーツブル (スポブル)</title>
+  <meta name="apple-itunes-app" content="app-id=1086719653">
+  <meta name="description" content="ドラフト候補選手見るならスポーツブル（スポブル）で。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。">
+  <meta name="keywords" content="ドラフト, 野球, 候補選手, スポーツ, メディア, ニュース, 動画, sports, media, スポブル">
+  <!-- sns ogp -->
+  <meta property="og:title" content="プロ野球2017 ドラフト候補選手 | スポーツブル (スポブル)">
+  <meta property="og:type" content="article">
+  <meta property="og:image" content="https://sportsbull.jp/assets/images/common/og_image.png">
+  <meta property="og:url" content="https://sportsbull.jp/stats/npb-draft2017/">
+  <meta property="og:description" content="ドラフト候補選手見るならスポーツブル（スポブル）で。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。">
+  <!-- twitter card -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@sportsbull_jp">
+  <meta name="twitter:title" content="<?php echo $page['og_title']; ?>">
+  <meta name="twitter:image" content="<?php echo $page['og_image']; ?>">
+  <meta name="twitter:url" content="<?php echo $page['og_url']; ?>">
+  <meta name="twitter:description" content="<?php echo $page['og_description']; ?>">
+  <!-- favicon -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="apple-touch-icon-precomposed" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="shortcut icon" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="icon" sizes="192x192" href="/assets/sp/images/common/apple-touch-icon.png">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="canonical" href="https://sportsbull.jp/stats/npb-draft2017/">
+
+<!--  <script src="/assets/js/libs/vendor.react.js?v=<?php echo $page['version']; ?>"></script>-->
+<!--  <script src="/assets/js/bundle/main.bundle.js?v=<?php echo $page['version']; ?>"></script>-->
+
+
+  <script type='text/javascript'>
+    var googletag = googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    (function() {
+      var gads = document.createElement('script');
+      gads.async = true;
+      gads.type = 'text/javascript';
+      var useSSL = 'https:' == document.location.protocol;
+      gads.src = (useSSL ? 'https:' : 'http:') +
+        '//www.googletagservices.com/tag/js/gpt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(gads, node);
+    })();
+  </script>
+
+  <script>
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+   ga('create', 'UA-74679267-1', 'auto');
+   ga('require', 'linkid');
+   ga('require', 'displayfeatures');
+   ga('send', 'pageview');
+
+  </script>
+
+  <script>
+    googletag.cmd.push(function() {
+      googletag.defineSlot('/531683568/npb-pc-rectangle', [300, 250], 'div-gpt-ad-1492577512561-0').addService(googletag.pubads());
+      googletag.pubads().enableSingleRequest();
+      googletag.pubads().collapseEmptyDivs();
+      googletag.enableServices();
+    });
+  </script>
+
+</head>
+<body>
+
+<div id="apvad-9881b571b6869b1815c4f16e1a2adbd4"></div>
+<script type="text/javascript" src="//cdn.apvdr.com/js/VastAdUnit.min.js"></script>
+<script>;(function(){new APV.VASTAdUnit({s:"9881b571b6869b1815c4f16e1a2adbd4", format:APV.AD_FORMAT.OVERLAY}).load();})();</script>
+<script type="text/javascript" src="//cdn.apvdr.com/js/apv-ifbstr.min.js"></script>
+
+<div class="whole dark">
+
+  <header class="head-sec">
+    <div class="head-sec-inner">
+      <h1><a href="/">SPORTS BULL</a></h1>
+    </div><!-- /.head-sec-inner -->
+  </header><!-- /.head-sec -->
+
+  <div class="body-sec">
+    <div class="body-sec-inner">
+      <section class="main-sec">
+        <!-- draft-header -->
+        <header class="draft-header">
+          <aside class="draft-header-bnr-top">
+            <ul>
+              <li><a href="#"><img src="/assets/images/stats/npb-draft2017/bnr-dummy-728x90.png" alt="" width="728" height="90"></a></li>
+            </ul>
+          </aside>
+
+          <div class="draft-header-main">
+            <div class="draft-logo"><img src="/assets/images/stats/npb-draft2017/draft-logo.png" alt="DRAFT 2017! NPB supported by リポビタンD" width="225" height="207"></div>
+            <h1><img src="/assets/images/stats/npb-draft2017/player-title.png" alt="プロ野球2017 ドラフト候補選手一覧" width="584" height="163"></h1>
+            <p class="lead">高校生、大学生そして社会人・独立リーグと、今年のドラフト候補選手を幅広くリストアップ！</p>
+          </div>
+
+          <aside class="draft-header-bnr-bottom">
+            <ul>
+              <li><a href="#"><img src="/assets/images/stats/npb-draft2017/bnr-dummy-728x90.png" alt="" width="728" height="90"></a></li>
+            </ul>
+          </aside>
+
+          <!-- draft-nav -->
+          <nav class="draft-nav">
+            <ul>
+              <li>ドラフト候補選手名鑑</li>
+              <li><a href="result/">ドラフト超速報</a></li>
+            </ul>
+          </nav>
+          <!-- draft-nav -->
+        </header>
+        <!-- /draft-header -->
+
+        <div id="js-players"></div>
+
+        <div class="note-block">
+          <ul>
+            <li>※ 表示地域は高校生と社会人はチームが所属している各地区の連盟、大学生は野球部合宿所（合宿所がない場合はグラウンド）の所在地に基づきます。<br>独立リーグはチームの本拠地となる都道府県を記載しています。</li>
+          </ul>
+        </div>
+
+        <!-- players-info -->
+        <div id="js-modal"></div>
+      </section><!-- /.main-sec -->
+
+    </div>
+  </div><!-- /.body-sec -->
+
+  <footer id="footer-container" class="foot-sec show-for-large">
+    <div class="foot-sec-inner">
+      <nav class="foot-breadCrumb">
+        <ol itemscope itemtype="http://schema.org/breadCrumbList">
+          <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/"><span itemprop="name">TOP</span><meta itemprop="position" content="1" /></a></li>
+          <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="./"><span itemprop="name">プロ野球2017 ドラフト候補選手</span><meta itemprop="position" content="2" /></a></li>
+        </ol>
+      </nav><!-- /.foot-breadCrumb -->
+
+      <div class="foot-pr">
+        <div class="foot-pr-inner">
+          <figure><img src="/assets/images/common/footer-overview-figure.png" alt=""></figure>
+
+          <div class="text-block">
+            <h3 class="foot-pr-logo"><img src="/assets/images/common/footer-overview-logo.png" alt="SPORTS BULL アプリ版(iPhone/Android)"></h3>
+            <ul class="foot-pr-btn">
+              <li><a href="https://itunes.apple.com/jp/app/undotsushin/id1086719653?l=ja&ls=1&mt=8" target="_blank"><img src="/assets/images/common/footer-overview-btn-applestore.png" alt="App Store" /></a></li>
+              <li><a href="https://play.google.com/store/apps/details?id=com.undotsushin" target="_blank"><img src="/assets/images/common/footer-overview-btn-googleplay.png" alt="Google play"></a></li>
+            </ul>
+            <p class="foot-pr-text">話題のスポーツニュースがサクサク読める、無料のニュースまとめアプリ「スポーツ・ブル」。高品質なスポーツのニュース、動画をいつでもお楽しみ頂けます。スマートフォンアプリをダウンロードして今日のニュースをチェックしましょう。</p>
+          </div><!-- /.text-block -->
+
+          <div class="fb-page-plugin">
+            <div class="fb-page" data-href="https://www.facebook.com/sportsbull/" data-width="400" data-height="154" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/sportsbull/"><a href="https://www.facebook.com/sportsbull/">スポーツブル（SPORTS BULL）</a></blockquote></div></div>
+          </div>
+        </div><!-- /.foot-pr-inner -->
+      </div><!-- /.foot-pr -->
+
+      <div id="pageTop" class="pagetop"><a href="#"><span>このページの先頭へ</span></a></div>
+
+      <div class="fnav-block">
+        <nav class="fnav">
+          <h3 class="fnav-logo"><img src="/assets/images/common/footer-fnav-logo.png" alt="SPORTS BULL"></h3>
+          <ul>
+            <li><a href="/about/">サービス紹介</a></li>
+            <li><a href="/about/company/">会社概要</a></li>
+            <li><a href="/about/privacy/">プライバシーポリシー</a></li>
+            <li><a href="/about/terms/">利用規約</a></li>
+          </ul>
+        </nav><!-- /.fnav -->
+
+        <div class="sns-block">
+          <ul>
+            <li class="sns-fb"><a href="https://www.facebook.com/sportsbull/" target="_blank">facebook</a></li>
+            <li class="sns-tw"><a href="https://twitter.com/sportsbull_jp" target="_blank">twitter</a></li>
+            <li class="sns-yt"><a href="https://www.youtube.com/channel/UCKwqba9IWuSKIk3DIpryOHw" target="_blank">youtube</a></li>
+          </ul>
+        </div><!-- /.sns-block -->
+
+        <p class="copyright">Copyright &copy; SPORTS BULL All rights reserved.</p>
+      </div><!-- /.fnav-block -->
+    </div><!-- /.foot-sec-inner -->
+  </footer><!-- /.foot-sec -->
+
+  <footer class="foot-sec show-for-small">
+    <div class="foot-sec-inner">
+
+      <div class="foot-pr">
+        <div class="foot-pr-inner">
+          <figure class="foot-pr-logo"><img src="/assets/sp/images/common/footer-overview-logo.png" alt="SPORTS BULL"></figure>
+          <div class="text-block">
+            <h3 class="foot-pr-heading">スポーツブルアプリをダウンロード</h3>
+            <ul class="foot-pr-list">
+              <li class="foot-pr-item"><a class="foot-pr-link" href="https://itunes.apple.com/jp/app/undotsushin/id1086719653?l=ja&ls=1&mt=8" target="_blank"><img src="/assets/sp/images/common/footer-overview-btn-applestore.png" alt="App Store" /></a></li>
+              <li class="foot-pr-item"><a class="foot-pr-link" href="https://play.google.com/store/apps/details?id=com.undotsushin" target="_blank"><img src="/assets/sp/images/common/footer-overview-btn-googleplay.png" alt="Google play"></a></li>
+            </ul>
+          </div>
+        </div><!-- /.foot-pr-inner -->
+
+        <div class="fb-page-plugin">
+          <div class="fb-page" data-href="https://www.facebook.com/sportsbull/" data-width="500" data-height="154" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/sportsbull/"><a href="https://www.facebook.com/sportsbull/">スポーツブル（SPORTS BULL）</a></blockquote></div></div>
+        </div>
+      </div><!-- /.foot-pr -->
+
+      <div id="js-page_top" class="pagetop"><a href="#"><span>このページの先頭へ</span></a></div>
+
+      <nav class="fnav">
+        <ul>
+          <li><a href="/about/">サービス紹介</a></li>
+          <li><a href="/about/privacy/">プライバシーポリシー</a></li>
+          <li><a href="/about/company/">会社概要</a></li>
+          <li><a href="/about/terms/">利用規約</a></li>
+        </ul>
+      </nav><!-- /.fnav -->
+
+      <div class="sns-block">
+        <ul>
+          <li class="sns-fb"><a href="https://www.facebook.com/sportsbull/" target="_blank">facebook</a></li>
+          <li class="sns-tw"><a href="https://twitter.com/sportsbull_jp" target="_blank">twitter</a></li>
+          <li class="sns-yt"><a href="https://www.youtube.com/channel/UCKwqba9IWuSKIk3DIpryOHw" target="_blank">youtube</a></li>
+        </ul>
+      </div><!-- /.sns-block -->
+
+      <p class="copyright">Copyright &copy; SPORTS BULL All rights reserved.</p>
+    </div><!-- /.foot-sec-inner -->
+  </footer><!-- /.foot-sec -->
+</div><!-- /.whole -->
+
+<link rel="stylesheet" href="/assets/css/stats/npb-draft2017/ui.css?v=<?php echo $page['version']; ?>" media="only screen">
+<?php
+// 一面タブからの導線を増やす #2080
+// @see https://github.com/undotsushin/undotsushin/issues/2080
+// @since 2017-06-26
+?>
+<script src="/assets/js/bundle/banners_with_json.bundle.js?v=<?php echo $page['version']; ?>"></script>
+<script src="/assets/popup/js/banner_popup_app.bundle.js?v=<?php echo $page['version']; ?>"></script>
+<script src="/assets/draft/js/bull_draft_2017_player.bundle.js?v=<?php echo $page['version']; ?>"></script>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '842032129256034',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+
+</body>
+</html>
