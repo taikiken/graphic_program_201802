cb34d08dec38f21aff168c650d70d8820e57a954
Yu Fukuda <y_fukuda@cloudpack.jp>
2017-10-25 19:58:43 +0900

    フォーム幅おおきくした

--- include/lib/company_news/controller.php
+++ include/lib/company_news/controller.php
@@ -5,15 +5,13 @@
 
 if($q->get_dir()===0){ // 新規
 	if($q->get_file()===0){
+    include $INCLUDEPATH."formback.php";
 
-		include $INCLUDEPATH."formback.php";
 	}elseif($q->get_file()===1){
-
 		data_conf();
+
 	}elseif($q->get_file()===2){
-
 		include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/ex.php";
-
 		data_sql();
 
     $sv[$sn[]="created_at"]="now()";
@@ -24,24 +22,19 @@ if($q->get_dir()===0){ // 新規
 }elseif($q->get_dir()===1){ // 編集
 	if($q->get_file()===0){
 
-		$sql=sprintf("select * from company_news where id=%s",$g->f("id"));
-
+		$sql=sprintf("select * from %s where id=%s",$TABLE,$g->f("id"));
 		$o->query($sql);
 		$p=$o->fetch_array();
-
 		include $INCLUDEPATH."formback.php";
+
 	}elseif($q->get_file()===1){
-
 		data_conf();
+
 	}elseif($q->get_file()===2){
-
 		include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/ex.php";
-
 		data_sql();
 
-
     $sv[$sn[]="id"]=$g->f("id");
-
 		$o=new dbutl($TABLE,$sn,$sv);
 		$e=$o->update($g->f("id"));
 
@@ -50,17 +43,16 @@ if($q->get_dir()===0){ // 新規
 	if($q->get_file()===0){
 
 		$sql=sprintf("select * from %s where id=%s",$TABLE,$g->f("id"));
-
 		$o->query($sql);
-
+    $p=$o->fetch_array();
+    include $INCLUDEPATH."formback.php";
 
 	}elseif($q->get_file()===1){
-
 		data_conf();
+
 	}elseif($q->get_file()===2){
 
 		include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/ex.php";
-
 		$o=new dbutl($TABLE);
 		$e=$o->remove($g->f("id"));
 
--- include/lib/company_news/ex.php
+++ include/lib/company_news/ex.php
@@ -3,7 +3,7 @@
 $sql="SELECT * FROM company_news ORDER BY publoshed_at";
 $o->query($sql);
 
-  $a[]=array("textfield","タイトル","title","50");
+  $a[]=array("textfield","タイトル","title","120");
   $a[]=array("textfield","URL","url","50");
   $a[]=array("textfield","公開日","published_at","50");
   include $INCLUDEPATH."print_write.php";
