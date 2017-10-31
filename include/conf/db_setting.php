f6ff15859c88972d5aeaa00b04f9662a4f1fb779
saegusamakoto <saegusa@kiito.co.jp>
2017-10-31 23:21:34 +0900

    Revert "コメント追加"
    This reverts commit 63862dcf84133b588bda1c0645af448244969f88.

--- include/conf/db_setting.php
+++ include/conf/db_setting.php
@@ -1,8 +1,5 @@
 <?php
 
-//このファイルはcmsサーバからWeb{n}サーバにrsyncされません
-//変更した際はcmsサーバだけではなく各Web{n}サーバに手動でアップロードしてください。
-
 if(!preg_match("/dev/",$servername)){
 	$DBNAME="ut0";
 	if(preg_match("/editdm|write|batch|password|image/",$filename)){
