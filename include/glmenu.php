1220d86f0f3e80692deada6e1486064300fa4d48
Yu Fukuda <y_fukuda@cloudpack.jp>
2017-10-31 19:06:08 +0900

    メニュー位置変更 about/company 静的ファイルを削除 #2730

--- include/glmenu.php
+++ include/glmenu.php
@@ -5,13 +5,13 @@
         <li><a href="/editdm/repo_n/?cid=8&rid=7">TOPカルーセル</a></li>
         <li><a href="/editdm/repo_n/?cid=9&rid=7">TOPピックアップ</a></li>
         <li><a href="/editdm/repo_s/?rid=7">TOP以外ピックアップ</a></li>
-        <li><a href="/editdm/company_news/">プレスリリース</a></li>
     <?php } ?>
     <li><a href="/editdm/repo_n/?cid=1">記事</a></li>
     <?php if(getSorC("is_external")!=1){ ?>
         <li><a href="/editdm/photo/">フォトアルバム</a></li>
         <li><a href="/editdm/repo_n/?cid=10">カテゴリー</a></li>
         <li><a href="/editdm/repo_s/?rid=2">メディア</a></li>
+        <li><a href="/editdm/company_news/">プレスリリース</a></li>
         <li><a href="/editdm/repo_n/?cid=6">会員</a></li>
         <li><a href="/editdm/comment/">コメント</a></li>
         <li><a href="/editdm/link/">記事取得</a></li>
