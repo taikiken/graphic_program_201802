3d92f79f01ea868796c0cb9dd9cd2eb080cb15b9
RL_tomiyasu <tomiyasu@realink-group.co.jp>
2017-11-15 15:54:59 +0900

    - 20171119Live配信対応 -  プレイヤー下バナーの差し込み -  サイドバー広告の変更 -  ad/appvador の削除

--- public/assets/stats/ua_kansai/js/userAgent.js
+++ public/assets/stats/ua_kansai/js/userAgent.js
@@ -6,7 +6,9 @@
 		$("#ua-app-big-banner").show();
 	}else if(agent.indexOf('iPhone') > 0 || agent.indexOf('iPod') > 0 || agent.indexOf('Android') > 0 && agent.indexOf('Mobile') > 0 || agent.indexOf('iPad') > 0 || agent.indexOf('Android') > 0){
 		$("#ua-sp-big-banner").show();
+		$("#athletech_sp").show();
 	}else{
 		$("#ua-pc-big-banner").show();
+		$("#athletech_pc").show();
 	}
 }());
