1ee3e201c604443f692118bc534b4bb67d796423
oracle <inazuma15@inazumatv-with-oracle.local>
2017-11-15 16:14:36 +0900

    comment 二重になってた修正 UNDO_SPBL-155

--- _src/app/assets/_babel/wowma-campaign-171116.js
+++ _src/app/assets/_babel/wowma-campaign-171116.js
@@ -10,18 +10,7 @@
  *
  * @build @@buildTime
  */
-/*!
- * Copyright (c) 2011-2017 inazumatv.com, inc.
- * @author (at)taikiken / http://inazumatv.com
- * @date 2017/02/09 - 19:05
- *
- * Distributed under the terms of the MIT license.
- * http://www.opensource.org/licenses/mit-license.html
- *
- * This notice shall be included in all copies or substantial portions of the Software.
- *
- * @build @@buildTime
- */
+
 'use strict';
 
 // ushi
