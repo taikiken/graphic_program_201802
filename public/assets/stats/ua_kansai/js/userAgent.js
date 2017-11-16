1dab17c0b65e383a1a554abbf29e50e070a604c3
RL_tomiyasu <tomiyasu@realink-group.co.jp>
2017-11-16 19:31:05 +0900

    アプリにスポーツアスリートバナー表示

--- public/assets/stats/ua_kansai/js/userAgent.js
+++ public/assets/stats/ua_kansai/js/userAgent.js
@@ -4,6 +4,7 @@
 	if ( agent.match(/undotsushin-ios/) || agent.match(/undotsushin-android/)) {
 		$("header.head-sec").hide();
 		$("#ua-app-big-banner").show();
+		$("#athletech_sp").show();
 	}else if(agent.indexOf('iPhone') > 0 || agent.indexOf('iPod') > 0 || agent.indexOf('Android') > 0 && agent.indexOf('Mobile') > 0 || agent.indexOf('iPad') > 0 || agent.indexOf('Android') > 0){
 		$("#ua-sp-big-banner").show();
 		$("#athletech_sp").show();
