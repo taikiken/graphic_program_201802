cb9bc4a42f355a489279b87694f1960d21014469
KenOnodera <onodera_stg@iret.co.jp>
2017-11-20 19:22:13 +0900

    dews特別対応     　カルーセルの一覧画面で操作権限がないと表示されないように修正

--- include/__layout_datatable.php
+++ include/__layout_datatable.php
@@ -1,7 +1,5 @@
 <?php if($CURRENTDIRECTORY!="log"){ ?>
 <?php if($q->get_dir()===3){ ?>
-
-<?php // 一覧ヘッダー定義 ?>
 <tr>
 <?php if($NUMBERINGOFF!=1){ ?>
 <th scope="col" width="45" class="t_numbering<?php if(getSorC("order")!=1){ ?>_disabled<?php } ?>">順番</th>
@@ -21,26 +19,11 @@
         <?php }?>
 <th scope="col" width="55" align="center" class="t_delete<?php if(getSorC("delete")!=1){ ?>_disabled<?php } ?>">削除</th>
 </tr>
-
-<?php // 一覧詳細定義 ?>
 <?php for($i=0;$i<count($p);$i++){ ?>
-<?php
-	$is_my_media = true;
-	if($TABLE==='u_headline' && empty(getSorC('is_carousel_headline')) === false){
-		//権限チェック
-		if(!preg_match("/^[0-9]+$/",$p[$i]["d2"])){
-			preg_match("/^([0-9]+):/",$p[$i]["d2"],$r);
-			$sid=$r[1];
-		}else{
-			$sid=$p[$i]["d2"];
-		}
-		$is_my_media = isMyMedia($sid, explode(',', getSorC('u_media')));
-	}
-?>
 
 <tr class="blockds" style="border-top:1px dotted #ccc;">
 <?php if($NUMBERINGOFF!=1){ ?>
-<td class="numbering"><?php if($p[$i]["n"]!=1 && $is_my_media){ ?><a<?php if(getSorC("order")==1){ ?> href="javascript://" onclick="javascript:cn(<?=$p[$i]["n"]?>,1)"<?php } ?>><img src="/shared/cms/img/cmd_up<?php if(getSorC("order")!=1 || $is_my_media === false){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ上へ入れ替える" ></a><?php }else{ ?><img src="/shared/cms/img/cmd_ups<?php if(getSorC("order")!=1 || $is_my_media === false){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ上へ入れ替える" ><?php } ?><?php if($p[$i]["n"]!=$N && $is_my_media){ ?><a<?php if(getSorC("order")==1){ ?> href="javascript://" onclick="javascript:cn(<?=$p[$i]["n"]?>,0)"<?php } ?>><img src="/shared/cms/img/cmd_down<?php if(getSorC("order")!=1 || $is_my_media === false){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ下へ入れ替える" ></a><?php }else{ ?><img src="/shared/cms/img/cmd_downs<?php if(getSorC("order")!=1 || $is_my_media === false){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ下へ入れ替える" ><?php } ?>
+<td class="numbering"><?php if($p[$i]["n"]!=1){ ?><a<?php if(getSorC("order")==1){ ?> href="javascript://" onclick="javascript:cn(<?=$p[$i]["n"]?>,1)"<?php } ?>><img src="/shared/cms/img/cmd_up<?php if(getSorC("order")!=1){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ上へ入れ替える" ></a><?php }else{ ?><img src="/shared/cms/img/cmd_ups<?php if(getSorC("order")!=1){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ上へ入れ替える" ><?php } ?><?php if($p[$i]["n"]!=$N){ ?><a<?php if(getSorC("order")==1){ ?> href="javascript://" onclick="javascript:cn(<?=$p[$i]["n"]?>,0)"<?php } ?>><img src="/shared/cms/img/cmd_down<?php if(getSorC("order")!=1){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ下へ入れ替える" ></a><?php }else{ ?><img src="/shared/cms/img/cmd_downs<?php if(getSorC("order")!=1){ ?>_disabled<?php } ?>.gif" width="13" height="13" alt="一つ下へ入れ替える" ><?php } ?>
 <?php } ?>
 <!--
 <img src="/shared/cms/img/cmd_ups_disabled.gif" width="13" height="13" alt="一つ上へ入れ替える" ><img src="/shared/cms/img/cmd_downs_disabled.gif" width="13" height="13" alt="一つ下へ入れ替える" >
@@ -52,7 +35,7 @@
         echo sprintf("<div class=\"dp\"><img src=\"/shared/cms/img/cmd_%sactive.gif\" width=\"13\" height=\"13\" class=\"flagswitch lang_%s\" id=\"e%s\"></div>",$p[$i]["flag".$LANG[$EI]]!=1?"dis":"",$LANG[$EI],$p[$i]["id"]);
 
       } else {
-        if(getSorC("draft")==1 && $is_my_media){
+        if(getSorC("draft")==1){
           echo sprintf("<div class=\"dp\"><img src=\"/shared/cms/img/cmd_%sactive.gif\" width=\"13\" height=\"13\" class=\"flagswitch lang_%s\" id=\"e%s\"></div>",$p[$i]["flag".$LANG[$EI]]!=1?"dis":"",$LANG[$EI],$p[$i]["id"]);
         }else{
           echo "<div class=\"dp2\"><img src=\"/shared/cms/img/cmd_active_disabled.gif\" width=\"13\" height=\"13\"></div>";
@@ -65,7 +48,7 @@
     <?php if($CURRENTDIRECTORY == "photo"){?>
         <td><a href="./list/?<?=$EDITDELETEINITIAL?>id=<?=$p[$i]["id"]?>&<?=$g->g_url()?>" class="menu"><span class="edit">画像一覧</span></a></td>
     <?php }else{?>
-        <td><a<?php if(getSorC("edit")==1 && $is_my_media){ ?> href="./edit/?<?=$EDITDELETEINITIAL?>id=<?=$p[$i]["id"]?>&<?=$g->g_url()?><?php if($CURRENTDIRECTORY=="repo_e"){ ?>&types=<?=$p[$i]["types"]?><?php } ?>"<?php } ?> class="menu"><span class="edit<?php if(getSorC("edit")!=1 || $is_my_media === false){ ?>_disabled<?php } ?>">編集</span></a></td>
+        <td><a<?php if(getSorC("edit")==1){ ?> href="./edit/?<?=$EDITDELETEINITIAL?>id=<?=$p[$i]["id"]?>&<?=$g->g_url()?><?php if($CURRENTDIRECTORY=="repo_e"){ ?>&types=<?=$p[$i]["types"]?><?php } ?>"<?php } ?> class="menu"><span class="edit<?php if(getSorC("edit")!=1){ ?>_disabled<?php } ?>">編集</span></a></td>
     <?php }?>
 
     <?php if($CURRENTDIRECTORY == "photo"){?>
@@ -75,7 +58,7 @@
                 <td><a href="/editdm/repo_n/delete/?<?=$EDITDELETEINITIAL?>id=<?=$p[$i]["id"]?>&cid=1" class="menu"><span class="delete">削除</span></a></td>
 
     <?php }else{?>
-                <td><a<?php if(getSorC("delete")==1 && $is_my_media){ ?> href="./delete/?<?=$EDITDELETEINITIAL?>id=<?=$p[$i]["id"]?>&<?=$g->g_url()?><?php if($CURRENTDIRECTORY=="repo_e"){ ?>&types=<?=$p[$i]["types"]?><?php } ?>"<?php } ?> class="menu"><span class="delete<?php if(getSorC("delete")!=1 || $is_my_media === false){ ?>_disabled<?php } ?>">削除</span></a></td>
+                <td><a<?php if(getSorC("delete")==1){ ?> href="./delete/?<?=$EDITDELETEINITIAL?>id=<?=$p[$i]["id"]?>&<?=$g->g_url()?><?php if($CURRENTDIRECTORY=="repo_e"){ ?>&types=<?=$p[$i]["types"]?><?php } ?>"<?php } ?> class="menu"><span class="delete<?php if(getSorC("delete")!=1){ ?>_disabled<?php } ?>">削除</span></a></td>
     <?php }?>
 </tr>
 <?php } ?>
