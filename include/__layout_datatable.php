7c4b61e74a0cad1ceb5ea3a226531adce06a9a64
Yu Fukuda <y_fukuda@cloudpack.jp>
2017-11-21 12:02:07 +0900

    UPDATE repo SET t2 = 'タイトル' WHERE id=95; 必須 UNDO_SPBL-27

--- include/__layout_datatable.php
+++ include/__layout_datatable.php
@@ -9,7 +9,7 @@
   <?php if ($CURRENTDIRECTORY != "company_news") { ?>
     <th scope="col" width="35" class="t_display<?php if(getSorC("draft")!=1){ ?>_disabled<?php } ?>">公開</th>
   <?php } ?>
-<th scope="col" class="t_title"><?=$THIS?>タイトル</th>
+<th scope="col" class="t_title"><?=$THIS?><?php echo $g->f("rid") == 95 ? '' : 'タイトル' ?></th>
         <?php if($CURRENTDIRECTORY == "photo"){?>
             <th scope="col" width="55" align="center" class="t_edit<?php if(getSorC("edit")!=1){ ?>_disabled<?php } ?>">画像一覧</th>
         <?php }else{?>
