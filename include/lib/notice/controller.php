df4de93133cb26cf0eac68776cc04b5a8ec82090
Yu Fukuda <y_fukuda@cloudpack.jp>
2017-11-28 10:37:15 +0900

    テキストフォーム UNDO_SPBL-150

--- include/lib/notice/controller.php
+++ include/lib/notice/controller.php
@@ -14,15 +14,30 @@ if($q->get_dir()===0){ // 新規
 		include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/ex.php";
 		data_sql();
 
-    $sv[$sn[]="created_at"]="now()";
-    $o=new dbutl($TABLE,$sn,$sv);
-    $e=$o->insert();
+    $sv[$sn[] = "created_at"] = "now()";
+    $o = new dbutl($TABLE, $sn, $sv);
+    $columns = implode(",", $sn);
+    $values = implode(",", $sv);
 
-	}
+    $sql = sprintf("INSERT INTO notices(%s) VALUES(%s) RETURNING id;", $columns, $values);
+    $o->query($sql);
+    $f = $o->fetch_array();
+    $notice_id = $f['id'];
+
+    // 選択されたカテゴリたち
+    $p_u_categories = empty($_POST['p_u_categories']) ? '0' : $_POST['p_u_categories'];
+    $categoryid_list = explode(',', $p_u_categories);
+
+    foreach ($categoryid_list as $category_id) {
+      $s[] = sprintf("INSERT INTO categories_notices(category_id,notice_id,created_at) VALUES(%s,%s,NOW());", $category_id, $notice_id);
+    }
+    $s = implode("\n", $s);
+    $e = $o->query($s);
+  }
 }elseif($q->get_dir()===1){ // 編集
 	if($q->get_file()===0){
 
-		$sql=sprintf("select * from %s where id=%s",$TABLE,$g->f("id"));
+		$sql=sprintf("select category_id from %s where id=%s",$TABLE,$g->f("id"));
 		$o->query($sql);
 		$p=$o->fetch_array();
 		include $INCLUDEPATH."formback.php";
@@ -34,10 +49,23 @@ if($q->get_dir()===0){ // 新規
 		include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/ex.php";
 		data_sql();
 
-    $sv[$sn[]="id"]=$g->f("id");
+		$notice_id = $g->f("id");
+    $sv[$sn[]="id"]=$notice_id;
 		$o=new dbutl($TABLE,$sn,$sv);
-		$e=$o->update($g->f("id"));
+		$e=$o->update($notice_id);
 
+    // 中間テーブル削除
+    $sql = sprintf("DELETE FROM categories_notices WHERE notice_id=%s;", $notice_id);
+    $o->query($sql);
+    // 選択されたカテゴリたち
+    $p_u_categories = empty($_POST['p_u_categories']) ? '0' : $_POST['p_u_categories'];
+    $categoryid_list = explode(',', $p_u_categories);
+
+    foreach ($categoryid_list as $category_id) {
+      $s[] = sprintf("INSERT INTO categories_notices(category_id,notice_id,created_at) VALUES(%s,%s,NOW());", $category_id, $notice_id);
+    }
+    $s = implode("\n", $s);
+    $e = $o->query($s);
 	}
 }elseif($q->get_dir()===2){ // 削除
 	if($q->get_file()===0){
@@ -53,8 +81,13 @@ if($q->get_dir()===0){ // 新規
 	}elseif($q->get_file()===2){
 
 		include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/ex.php";
-		$o=new dbutl($TABLE);
-		$e=$o->remove($g->f("id"));
+    $notice_id = $g->f("id");
+
+    $o=new dbutl($TABLE);
+		$e=$o->remove($notice_id);
+		// 中間テーブル削除
+    $sql = sprintf("DELETE FROM categories_notices WHERE notice_id=%s;", $notice_id);
+    $o->query($sql);
 
 	}
 }elseif($q->get_dir()===3){ // 一覧画面
