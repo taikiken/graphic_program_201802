4dabb3f419713336812472d51c391499b7d80af7
Yu Fukuda <y_fukuda@cloudpack.jp>
2017-12-04 12:48:12 +0900

    一覧に広告設定リンク足す feature/UNDO_SPBL-180

--- include/lib/repo_s/fetchdata.php
+++ include/lib/repo_s/fetchdata.php
@@ -1,6 +1,12 @@
 <?php if($SC==1){ ?>
 <span class="j2"><?php if(strlen($DIRECTORY1)>0){ ?><a href="<?=rewrite($DIRECTORY1,$p[$i])?>" target="_blank"><img src="/shared/cms/img/icon_browser<?=($p[$i]["expire"]==1)?"":"_"?>.gif" alt="<?=mod_HTML($p[$i]["name"])?>ページをブラウザでプレビューする" width="16" height="16" ></a><?php } ?>
-<a href="<?=($p[$i]["c_flag"]!=1) ? sprintf("%srepo_n/?cid=%s&rid=%s",$ADPATH,$p[$i]["id"],$PARAM["rid"]) : sprintf("?cid=%s&rid=%s",$PARAM["rid"],$p[$i]["id"])?>" class="folder"><?=mod_HTML($p[$i]["name"])?></a></span>
+<a href="<?=($p[$i]["c_flag"]!=1) ? sprintf("%srepo_n/?cid=%s&rid=%s",$ADPATH,$p[$i]["id"],$PARAM["rid"]) : sprintf("?cid=%s&rid=%s",$PARAM["rid"],$p[$i]["id"])?>" class="folder"><?=mod_HTML($p[$i]["name"])?></a>
+<div align="right">
+    <a href="../ad/?nid=<?= $p[$i]["id"] ?>&<?= $g->g_url() ?>" class=" toe">
+  <img src="/shared/cms/img/file.png" width="17" height="16">広告設定
+  </a>
+</div>
+</span>
 <?php if($BILLINGUAL){ ?><span class="e2"><?php if(strlen($DIRECTORY1_E)>0){ ?><a href="<?=rewrite($DIRECTORY1_E,$p[$i])?>" target="_blank"><img src="/shared/cms/img/icon_browser<?=($p[$i]["expire"]==1)?"":"_"?>.gif" alt="<?=mod_HTML($p[$i]["name_e"])?>ページをブラウザでプレビューする" width="16" height="16" ></a><?php } ?>
 <?=checkEnglishTitle($p[$i]["name_e"])?></span><?php } ?>
 <?php }else{ ?>
