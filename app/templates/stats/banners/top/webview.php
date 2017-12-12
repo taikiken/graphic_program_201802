2ba4c8f0a23314e9dad25a1b08ff56efb14ac746
Yoshiaki Fujimori <info@2244.jp>
2017-12-12 22:31:00 +0900

    renewal - アプリプレビュー用にstats部分を用意 - tweaks
    overflow: none; -> overflow: hidden;

--- app/templates/stats/banners/top/webview.php
+++ app/templates/stats/banners/top/webview.php
@@ -14,7 +14,7 @@
   <style>
 
   html,body {
-    overflow: none;
+    overflow: hidden;
   }
 
   * {
@@ -25,9 +25,8 @@
   }
 
   .wrapper {
-    overflow: none;
-    overflow-x: auto;
-    -webkit-overflow-scrolling: touch;
+    overflow:hidden;
+    overflow-x:scroll;
     display: flex;
     align-items: center;
     padding: 0.5vw 0;
@@ -93,7 +92,7 @@
   .wrapper ul a span {
     display: block;
     line-height: 1.2em;
-    font-size: 2.3vw;
+    font-size: 2.25vw;
   }
 
   .wrapper p {
@@ -159,27 +158,27 @@
       </a>
     </li>
     <li>
-      <a href="https://sportsbull.jp/stats/bleague/">
-        <i>
-          <svg class="icon">
-            <use xlink:href="#icon-basketball" />
-          </svg>
-        </i>
-        <span>
-          Bリーグ
-        </span>
-      </a>
-    </li>
-    <li>
       <a href="https://sportsbull.jp/stats/u_rugby/kantou/">
         <i>
           <svg class="icon">
-            <use xlink:href="#icon-football" />
+            <use xlink:href="#icon-baseball" />
           </svg>
         </i>
         <span>
           関西学生<br />
           アメフト
+        </span>
+      </a>
+    </li>
+    <li>
+      <a href="https://sportsbull.jp/stats/bleague/">
+        <i>
+          <svg class="icon">
+            <use xlink:href="#icon-soccer" />
+          </svg>
+        </i>
+        <span>
+          Bリーグ
         </span>
       </a>
     </li>
@@ -200,27 +199,8 @@
 </div>
 -->
 
-<svg style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg style="position: absolute; width: 0; height: 0; overflow: hidden; left: 0; top: 0;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <defs>
-<symbol id="icon-basketball" viewBox="0 0 32 32">
-<title>basketball</title>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M30.946 15.972c0 8.263-6.711 14.974-14.974 14.974s-14.974-6.711-14.974-14.974c0-8.263 6.711-14.974 14.974-14.974 8.319 0 14.974 6.711 14.974 14.974z"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M5.435 26.565c-0.832-0.832 3.217-6.267 9.040-12.146 3.549-3.549 6.988-6.433 9.262-7.986"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M26.565 26.565c-1.719 1.719-7.82-1.664-13.698-7.487-3.438-3.438-6.045-6.988-7.21-9.65"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M28.007 15.972c-3.993 0.111-10.981 0.887-13.976 4.215-2.107 2.329-1.22 5.435-0.055 7.709"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M13.865 4.27c-0.665 3.272-2.052 8.263-4.88 10.704-1.775 1.553-3.716 1.83-5.213 1.719"></path>
-</symbol>
-<symbol id="icon-football" viewBox="0 0 32 32">
-<title>football</title>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M20.631 1.276c3.938 2.496 7.653 6.156 10.094 10.094-0.666 4.714-2.828 9.262-6.433 12.922-5.047 5.047-11.813 7.21-18.412 6.544-1.22-0.111-2.773-0.499-3.494-1.22-0.776-0.776-1.109-2.274-1.22-3.494-0.055-0.444-0.055-0.832-0.111-1.276"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M30.946 7.043c0-0.388-0.055-0.776-0.111-1.165-0.111-1.22-0.499-2.773-1.22-3.494-0.776-0.776-2.329-1.109-3.549-1.276-6.544-0.61-13.366 1.553-18.357 6.6-3.66 3.66-5.823 8.208-6.489 12.922 2.496 3.938 6.156 7.653 10.149 10.094"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M21.518 10.426l-11.092 11.147"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M17.414 10.759l3.771 3.771"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M10.759 17.414l3.771 3.771"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M14.087 14.087l3.771 3.771"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M15.695 2.496c5.601 2.773 10.981 8.152 13.754 13.754"></path>
-<path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M2.496 15.75c2.773 5.601 8.152 10.981 13.754 13.754"></path>
-</symbol>
 <symbol id="icon-baseball" viewBox="0 0 32 32">
 <title>baseball</title>
 <path stroke-width="2.0477" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" d="M30.946 15.972c0 8.27-6.704 14.974-14.974 14.974s-14.974-6.704-14.974-14.974c0-8.27 6.704-14.974 14.974-14.974s14.974 6.704 14.974 14.974z"></path>
@@ -248,7 +228,6 @@
 </symbol>
 </defs>
 </svg>
-
 
 </body>
 </html>
