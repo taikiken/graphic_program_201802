2d0a338eb7d430a1bd49361eb690d9c481330305
taikiken <denkosekka@gmail.com>
2017-12-14 22:48:40 +0900

    fowtype やめる UNDO_SPBL-212

--- _src/babels/src/util/Elements.js
+++ _src/babels/src/util/Elements.js
@@ -15,20 +15,23 @@ import { Offset } from './Offset';
 import { Style } from './Style';
 
 // @since 2016-09-16
+/**
+ * [library] - Sagen
+ */
 const Sagen = self.Sagen;
 
-/**
- * Sagen.Dom instance を保持するための private Symbol
- * @private
- * @type {Symbol}
- */
-const domSymbol = Symbol('Sagen.Dom instance');
-/**
- * Style instance を保持するための private Symbol
- * @private
- * @type {Symbol}
- */
-const styleSymbol = Symbol('Style instance');
+// /**
+//  * Sagen.Dom instance を保持するための private Symbol
+//  * @private
+//  * @type {Symbol}
+//  */
+// const domSymbol = Symbol('Sagen.Dom instance');
+// /**
+//  * Style instance を保持するための private Symbol
+//  * @private
+//  * @type {Symbol}
+//  */
+// const styleSymbol = Symbol('Style instance');
 
 /**
  * 要素(HTMLElement)の ClientReact, style を管理します
@@ -38,7 +41,7 @@ export class Elements extends Offset {
    * element 要素処理ヘルパー
    * @param {Element} element 処理対象 Element
    */
-  constructor(element:Element) {
+  constructor(element) {
     if (!element) {
       throw new Error(`element is not correct: ${element}`);
     }
@@ -48,28 +51,30 @@ export class Elements extends Offset {
      * 引数 `element` を Sagen.Dom instance にし addClass, removeClass を行います
      * @type {Sagen.Dom}
      */
-    this[domSymbol] = new Sagen.Dom(element);
+    this.dom = new Sagen.Dom(element);
+    // this[domSymbol] = new Sagen.Dom(element);
     /**
      * 引数 `element` の style 関連管理を行います
      * @type {Style}
      */
-    this[styleSymbol] = new Style(element);
+    this.style = new Style(element);
+    // this[styleSymbol] = new Style(element);
   }
   // ---------------------------------------------------
   //  GETTER / SETTER
   // ---------------------------------------------------
-  /**
-   * 引数 `element` を Sagen.Dom instance 変換
-   * @return {Sagen.Dom} 引数 `element` を Sagen.Dom instance にしたものを返します
-   */
-  get dom() {
-    return this[domSymbol];
-  }
-  /**
-   * 引数 `element` を Style instance 変換
-   * @return {Style} 引数 `element` を Style instance にしたものを返します
-   */
-  get style() {
-    return this[styleSymbol];
-  }
+  // /**
+  //  * 引数 `element` を Sagen.Dom instance 変換
+  //  * @return {Sagen.Dom} 引数 `element` を Sagen.Dom instance にしたものを返します
+  //  */
+  // get dom() {
+  //   return this[domSymbol];
+  // }
+  // /**
+  //  * 引数 `element` を Style instance 変換
+  //  * @return {Style} 引数 `element` を Style instance にしたものを返します
+  //  */
+  // get style() {
+  //   return this[styleSymbol];
+  // }
 }
