e17a2c43ba6090427e23b849e12720b7ad3d66a3
taniguchi <taniguchi@cloudpack.jp>
2017-12-21 19:21:18 +0900

    deskとmobileに分けた

--- app/routes/pyeongchang2018.router.php
+++ app/routes/pyeongchang2018.router.php
@@ -8,22 +8,28 @@ $app->group('/pyeongchang2018', function () use ($app) {
   $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app) {
     $args['partners'] = $app->model->get_partners();
 
-    return 'pyeongchang2018';
+    return;
   });
 
   // フォト一覧
   // ==============================
   $this->get('/photo[/]', function ($request, $response, $args) use ($app) {
 
-    return $this->renderer->render($response, '/pyeongchang2018/photo.php', $args);
-
+    if ( $app->model->property('ua') === 'desktop' ) :
+      return $this->renderer->render($response, '/pyeongchang2018/desktop/photo.php', $args);
+    else :
+      return $this->renderer->render($response, '/pyeongchang2018/mobile/photo.php', $args);
+    endif;
   });
   // ハイライト一覧
   // ==============================
   $this->get('/movie[/]', function ($request, $response, $args) use ($app) {
 
-    return $this->renderer->render($response, '/pyeongchang2018/movie.php', $args);
-
+    if ( $app->model->property('ua') === 'desktop' ) :
+      return $this->renderer->render($response, '/pyeongchang2018/desktop/movie.php', $args);
+    else :
+      return $this->renderer->render($response, '/pyeongchang2018/mobile/movie.php', $args);
+    endif;
   });
   // WebView
   // ==============================
