90fa7ed791ce991a82f82ef418661145e0fcec16
Yoshiaki Fujimori <info@2244.jp>
2018-01-08 16:40:34 +0900

    renewal - stats - ua_kansai 調整

---
+++ app/templates/stats/ua_kansai/_head.php
@@ -0,0 +1,52 @@
+<link rel="canonical" href="">
+<link rel="apple-touch-icon" sizes="180x180" href="https://sportsbull.jp/assets/sp/images/common/apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" href="https://sportsbull.jp/assets/sp/images/common/apple-touch-icon.png">
+<link rel="icon" sizes="192x192" href="https://sportsbull.jp/assets/sp/images/common/apple-touch-icon.png">
+<link rel="shortcut icon" href="/favicon.ico">
+
+<?php
+
+// for dfp
+$page['template']         = 'stats';
+$page['category']['slug'] = 'ua_kansai';
+
+?>
+<?php include_once __DIR__.'/../../_env.php'; ?>
+
+<!-- 表示確認用 / -->
+<link rel="stylesheet" href="/assets/css/tmp/head_foot_demo.css" media="only screen and (min-width: 769px)">
+<link rel="stylesheet" href="/assets/sp/css/tmp/head_foot_demo.css" media="only screen and (max-width: 768px)">
+<!-- / 表示確認用 -->
+
+<link rel="stylesheet" href="/assets/css/basic.css" media="only screen and (min-width: 769px)">
+<link rel="stylesheet" href="/assets/sp/css/basic.css" media="only screen and (max-width: 768px)">
+
+<link rel="stylesheet" href="/assets/stats/ua_kansai/css/ui.css">
+<link rel="stylesheet" href="/assets/stats/ua_kansai/css/style.css">
+
+<!-- css & js -->
+<link rel="stylesheet" href="/assets/css/photo/photo.css" media="only screen and (min-width: 769px)">
+<link rel="stylesheet" href="/assets/css/photo/sp_photo.css" media="only screen and (max-width: 768px)">
+
+<?php include_once __DIR__.'/../../_head_bottom.php'; ?>
+
+<!-- ad/dfp - pc/rectangle -->
+<script>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/531683568/npb-pc-rectangle', [300, 250], 'div-gpt-ad-1492577512561-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().collapseEmptyDivs();
+    googletag.enableServices();
+  });
+</script>
+<!-- //ad/dfp - pc/rectangle -->
+
+<!-- ad/npb-sp-anchor -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8613117509675807",
+    enable_page_level_ads: true
+  });
+</script>
+<!-- // ad/npb-sp-anchor -->
