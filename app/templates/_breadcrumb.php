2ad71bf898cff670b27360270638b20b4e78684c
Yoshiaki Fujimori <info@2244.jp>
2018-01-08 15:01:26 +0900

    renewal - パンくず出力整理

--- app/templates/_breadcrumb.php
+++ app/templates/_breadcrumb.php
@@ -4,24 +4,53 @@
  * Time: 18:23
  * SEO対策 / パンくずリストを設置する #776
  */
+
+if ($page['template'] == 'category') :
+  $BREADCRUMB = array(
+    array(
+      'label' => $page['category']['label'],
+      'path'  => '/category/'.$page['category']['slug'].'/',
+    ),
+  );
+
+elseif ($page['template'] == 'p') :
+  $BREADCRUMB = array(
+    array(
+      'label' => $page['post']['categories'][0]['label'],
+      'path'  => '/category/'.$page['post']['categories'][0]['slug'].'/',
+    ),
+    array(
+      'label' => $page['post']['title'],
+      'path'  => $page['post']['url'],
+    ),
+  );
+
+elseif ($page['template'] == 'motorsports') :
+  $BREADCRUMB = array(
+    array(
+      'label' => $page['motorsports']['label'],
+      'path'  => '/category/motorsports/',
+    ),
+    array(
+      'label' => strtoupper($page['motorsports']['url']),
+      'path'  => '/motorsports/'.$page['motorsports']['url'],
+    ),
+  );
+
+endif;
 ?>
 <nav class="foot-breadCrumb">
   <ol itemscope itemtype="http://schema.org/breadCrumbList">
     <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/"><span itemprop="name">TOP</span><meta itemprop="position" content="1" /></a></li>
-    <?php if ($page['template'] == 'category') :
-    // category ?>
-    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/category/<?php echo $page['category']['slug']; ?>"><span itemprop="name"><?php echo $page['category']['label']; ?></span><meta itemprop="position" content="2" /></a></li>
-
-    <?php elseif ($page['template'] == 'p') :
-    // single ?>
-    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/category/<?php echo $page['post']['categories'][0]['slug']; ?>"><span itemprop="name"><?php echo $page['post']['categories'][0]['label']; ?></span><meta itemprop="position" content="2" /></a></li>
-    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="<?php echo $page['post']['url'] ?>"><span itemprop="name"><?php echo $page['post']['title'] ?></span><meta itemprop="position" content="3" /></a></li>
-
-    <?php elseif ($page['template'] == 'motorsports') :
-    // motorsports - @since 2017-07-06 ?>
-      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/category/motorsports/"><span itemprop="name"><?php echo $page['motorsports']['label'] ?></span><meta itemprop="position" content="2" /></a></li>
-      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="/motorsports/<?php echo $page['motorsports']['url']; ?>"><span itemprop="name"><?php echo strtoupper($page['motorsports']['url']) ?></span><meta itemprop="position" content="3" /></a></li>
-
-    <?php endif; ?>
+    <?php
+    if ( isset($BREADCRUMB) ) :
+      foreach( $BREADCRUMB as $key => $value ) :
+    ?>
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="<?php echo $value['path']; ?>"><span itemprop="name"><?php echo $value['label']; ?></span>
+      <meta itemprop="position" content="<?php echo $key + 2; ?>" /></a></li>
+    <?php
+      endforeach;
+    endif;
+    ?>
   </ol>
 </nav>
