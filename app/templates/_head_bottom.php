0c4900fcdf64c4ce9db71f313031b442ec9f0047
Yoshiaki Fujimori <info@2244.jp>
2018-01-08 12:57:01 +0900

    UNDO_SPBL-321 - Google Optimizeコードを外部ファイル化 - tweak

--- app/templates/_head_bottom.php
+++ app/templates/_head_bottom.php
@@ -1,12 +1,12 @@
+<!-- google -->
 <?php // # Google css ?>
 <link rel="stylesheet" href="/assets/google/style.css?v=<?php echo $page['version']; ?>">
 <?php // Google css ?>
-
 <?php // #2987 - Google DFP ?>
 <script src='https://www.googletagservices.com/tag/js/gpt.js'></script>
 <script src="/assets/google/dfp.js?v=<?php echo $page['version']; ?>"></script>
 <?php // Google DFP ?>
-
 <?php // # Google Analytics & Optimize?>
 <script src="/assets/google/ga.js?v=<?php echo $page['version']; ?>"></script>
 <?php // Google Analytics & Optimize ?>
+<!-- // google -->
