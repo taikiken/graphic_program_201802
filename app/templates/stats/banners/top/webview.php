6bf2682f5ffbab0f79c7dad60dd56193d3c771d0
Yoshiaki Fujimori <info@2244.jp>
2018-01-10 22:18:54 +0900

    renewal - stats / webview用のスタイルちょい調整

--- app/templates/stats/banners/top/webview.php
+++ app/templates/stats/banners/top/webview.php
@@ -16,7 +16,6 @@
 <?php else : ?>
   <link rel="stylesheet" href="/assets/sp/css/ui.css?v=<?php echo $page['version']; ?>">
 <?php endif; ?>
-
   <style>
   html,body {
     overflow: hidden;
@@ -29,115 +28,74 @@
     font-family: sans-serif;
   }
 
-  .wrapper {
-    overflow:hidden;
-    overflow-x:scroll;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  .stats_banner {
     padding: 0 2vw;
+    border: none;
   }
 
-  a {
+  .stats_banner a {
     font-weight: bold;
     text-decoration: none;
     color: #0f3580;
-  }
-
-  .wrapper header {
-    width: 10.5vw;
-    text-align: left;
-    border-right: 1px solid #ddd;
-    padding: 2.5vw 0;
-  }
-
-  .wrapper header h1 {
-    font-size: 2.7vw;
-    line-height: 1.2em;
-    color: #0f3580;
-  }
-
-  .wrapper a {
     padding: 3.575vw 0;
   }
 
-  .wrapper ul {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    list-style: none;
+  .stats_banner__heading {
+    width: 10.5vw;
+    padding: 2.5vw 0;
+    text-align: left;
+  }
+
+  .stats_banner__heading__title {
+    font-size: 2.7vw;
+    line-height: 1.2em;
+  }
+
+  .stats_banner__list {
     width: 77vw; /*  100 - header(速報&データ) - p(すべて) */
     padding-left: 2vw;
     padding-right: 2vw;
   }
 
-
-  .wrapper ul a {
-    display: flex;
-    align-items: center;
+  .stats_banner__item {
+    width: auto;
   }
 
-  .wrapper ul a i {
+  .stats_banner__item a {
+    padding: 3.575vw 0;
+  }
+
+  .stats_banner__item a i {
     display: flex;
-    background: #0f3580;
-    align-items: center;
-    justify-content: center;
     width: 6.45vw;
     height: 6.45vw;
-    border-radius: 50%;
     margin-right: .8vw;
-    line-height: 0;
   }
 
-  .wrapper ul a i svg {
-    margin: 0;
-    padding: 0;
+  .stats_banner__item a svg {
     width: 3.9vw;
     height: 3.9vw;
-    fill: transparent;
-    stroke-width: 0;
-    stroke: #fff;
-    line-height: 0;
   }
 
-  .wrapper ul a i svg.icon--fill  {
-    fill: #fff;
-  }
-
-  .wrapper ul a span {
+  .stats_banner__item a span {
     display: block;
-    line-height: 1.2em;
     font-size: 2.2vw;
   }
 
-  .wrapper p {
+  .stats_banner__btn {
     margin-left: auto;
     width: 12.5vw;
   }
 
-  .wrapper p a {
-    display: block;
-    margin-left: auto;
-  }
-
-  .wrapper p a span {
-    display: flex;
+  .stats_banner__btn__link {
     border: .3vw solid #0f3580;
-    align-items: center;
-    justify-content: center;
-    height: 6vw;
-    border-radius: 1vw;
   }
 
-  .wrapper p a span em {
+  .stats_banner__btn__link span {
     font-size: 2vw;
     line-height: 1;
     font-weight: bold;
     font-style: normal;
-  }
-
-  .stats_banner__item svg.icon--fill {
-    fill : #fff;
   }
   </style>
 </head>
