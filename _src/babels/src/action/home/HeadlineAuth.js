a549b35bafcc786baf940f249f686cbf3b6e6a28
taikiken <denkosekka@gmail.com>
2018-01-11 17:18:35 +0900

    sp 条件式追加する UNDO_SPBL-333

--- _src/babels/src/action/home/HeadlineAuth.js
+++ _src/babels/src/action/home/HeadlineAuth.js
@@ -17,33 +17,48 @@ import {User} from '../../app/User';
 import {Length} from '../../app/const/Length';
 
 /**
- * <p>token つき 認証（ログイン）<br>
- * Home headline（注目ニュース）</p>
+ * Home headline（注目ニュース）
+ * - token つき 認証（ログイン）
  */
 export class HeadlineAuth extends ActionAuth {
   /**
-   * <p>Home headline（注目ニュース） データを取得します<br>
-   * ** types: Api.home() ** を使用します</p>
+   * Home headline（注目ニュース） データを取得します - **認証**（ログイン） token 付き
+   * - types: Api.home() ** を使用します
    *
-   * **認証**（ログイン）
-   *
-   * @param {Function} [resolve=null] Ajax 成功時の callback
-   * @param {Function} [reject=null] Ajax 失敗時の callback
+   * [memo]
+   * - 2018-01-11 sp / pc 取得件数変更になる
+   * - UNDO_SPBL-333 【WEB:SP:headline】取得件数を変える - https://aws-plus.backlog.jp/view/UNDO_SPBL-333
+   * ```
+   * PC : 5件固定 ( 右下に広告が配置されるため / 記事が奇数でないとレイアウト崩れる?という都合があったはず)
+   * SP : 6件固定ではなくCMSから設定している分表示 ( APIのレスポンス分 )
+   * -- ex. https://sportsbull.jp/api/v1/articles/self/headline ( length 指定なし )
+   * ```
+   * @param {function} [resolve=null] Ajax 成功時の callback
+   * @param {function} [reject=null] Ajax 失敗時の callback
+   * @param {boolean} [sp=false] sp flag - 取得件数が異なる為に使用します
    */
-  constructor( resolve:Function = null, reject:Function = null ) {
-    super( User.token, Api.home(), resolve, reject );
+  constructor(resolve = null, reject = null, sp = false) {
+    super(User.token, Api.home(), resolve, reject);
+    /**
+     * sp flag - 取得件数が異なる為に使用します
+     * @type {boolean}
+     */
+    this.sp = sp;
   }
   // ---------------------------------------------------
   //  GETTER / SETTER
   // ---------------------------------------------------
   /**
    * Ajax API url を作成します<br>
-   * <code>Api.home().url/headline?offset=0&length=6</code>
+   * ```
+   * Api.home().url/headline?offset=0&length=6
+   * ```
    * @return {string} headline API url を返します
    */
-  get url():string {
-
-    return `${this._url}/headline?offset=0&length=${Length.headline}`;
-
+  get url() {
+    // return `${this._url}/headline?offset=0&length=${Length.headline}`;
+    return this.sp ?
+      `${this.path}/headline` :
+      `${this.path}/headline?offset=0&length=${Length.headline}`;
   }
 }
