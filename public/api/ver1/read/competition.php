31e7ebfe44f05c62b909294ad24d56661077b7f3
YuudaiSuzuki <yuudai.suzuki@iret.co.jp>
2018-01-16 17:56:33 +0900

    パラボード　大会概要ページ用API作成

--- public/api/ver1/read/competition.php
+++ public/api/ver1/read/competition.php
@@ -12,95 +12,148 @@ $week_list = array('日', '月', '火', '水', '木', '金', '土');
 $o = new db;
 $o->connect();
 
+//大会概要取得処理------------------------------
 $sql = <<<SQL
 SELECT 
     cmp.* ,
     sports.name as sport_name
 FROM
     competitions cmp
-INNER JOIN
-    u_categories uc ON cmp.category_id = uc.id
 LEFT JOIN
     sports ON cmp.sport_id = sports.id
 WHERE
     cmp.id = {$id}
 AND
-    cmp.is_public IS TRUE 
+    cmp.is_public IS TRUE
+AND
+    cmp.is_delete IS FALSE
 ORDER BY
     start_date_time DESC;
 SQL;
 
 $o->query($sql);
 $f = $o->fetch_array();
-$f['sport_name'] = !empty($f['sport_name']) ? $f['sport_name'] : '';
-$f['sport_id'] = !empty($f['sport_id']) ? $f['sport_id'] . '.png' : '';
 
-$DateTime = new DateTime($f['start_date_time']);
-$start_date = $DateTime->format('Y年m月d日');
-$week_list = array('日', '月', '火', '水', '木', '金', '土');
-$day_of_week = $week_list[(int)$DateTime->format('w')];
-$start_date = $start_date . '（' . $day_of_week . '）';
+if(!empty($f)){
+    $f['sport_name'] = !empty($f['sport_name']) ? $f['sport_name'] : '';
+    $f['sport_id'] = !empty($f['sport_id']) ? $f['sport_id'] . '.png' : '';
 
-$DateTime = new DateTime($f['end_date_time']);
-$end_date = $DateTime->format('d日');
-$week_list = array('日', '月', '火', '水', '木', '金', '土');
-$day_of_week = $week_list[(int)$DateTime->format('w')];
-$end_date = $end_date . '（' . $day_of_week . '）';
+    //開始日
+    $DateTime = new DateTime($f['start_date_time']);
+    $start_date = $DateTime->format('Y年m月d日');
+    $day_of_week = $week_list[(int)$DateTime->format('w')];
+    $start_date = $start_date . '（' . $day_of_week . '）';
 
-$articles = [];
-$article_id_list = [
-  284543,
-  284559,
-  284088,
-];
-$article_ids_sql = implode(' OR id = ', $article_id_list);
-$sql = sprintf("select * from %s", sprintf($articletable, set_isbookmark($uid), sprintf(" and (id = %s)", $article_ids_sql)));
-$o->query($sql);
+    //終了日
+    $DateTime = new DateTime($f['end_date_time']);
+    $end_date = $DateTime->format('d日');
+    $day_of_week = $week_list[(int)$DateTime->format('w')];
+    $end_date = $end_date . '（' . $day_of_week . '）';
 
-while ($f = $o->fetch_array()){
-  $articles[] = $f;
-}
+    $response = [
+        'competition_name'  => $f['name'],
+        'sport_name'        => $f['sport_name'],
+        'icon'              => $f['sport_id'],
+        'period'            => $start_date . ' 〜 ' . $end_date,
+        'organizer'         => $f['organizer'],
+        'collaborator'      => $f['collaborator'],
+        'sponser'           => $f['sponser'],
+        'venue'             => $f['venue'],
+        'qualification'     => $f['qualification'],
+        'regulation'        => $f['regulation'],
+        'file'              => $f['file'],
+        'provider_name'     => $f['provider_name'],
+        'provider_url'      => $f['provider_url'],
+    ];
 
-$response = [
-  'competition_name'  => $f['name'],
-  'sport_name'        => $f['sport_name'],
-  'icon'              => $f['sport_id'],
-  'period'            => $start_date . ' 〜 ' . $end_date,
-  'organizer'         => $f['organizer'],
-  'collaborator'      => $f['collaborator'],
-  'sponser'           => $f['sponser'],
-  'venue'             => $f['venue'],
-  'qualification'     => $f['qualification'],
-  'regulation'        => $f['regulation'],
-  'file'              => $f['file'],
-  'highlight_movie'   =>[
-    'foo' => 'foo',
-    'bar' => 'bar',
-  ],
-  'news'              =>[
-    'foo' => 'foo',
-    'bar' => 'bar',
-  ],
-  'column'            =>[
-    'foo' => 'foo',
-    'bar' => 'bar',
-  ],
-  'photo_gallery'      =>[
-    'foo' => 'foo',
-    'bar' => 'bar',
-  ],
-];
-if (true) {
 
-  $y["status"]["code"] = 200;
-  $y["status"]["user_message"] = "";
-  $y["status"]["developer_message"] = "";
-  $y["response"] = $response;
+    //関連記事取得処理------------------------------
+    $articles_list = [];
+    $highlight_movie = [];
+    $news = [];
+    $photo_gallery = [];
+    $max_count = 3;
+    $max_count_sp = 2;
+    $sql = <<<SQL
+SELECT
+    acmp.article_id,
+    acmp.type
+FROM
+    articles_competitions acmp
+WHERE
+    acmp.competition_id = {$id}
+AND
+    acmp.is_public IS TRUE
+ORDER BY
+    acmp.created_at ASC;
+SQL;
+    $o->query($sql);
+
+    //取得したidをtype毎に分類する
+    while($f=$o->fetch_array()){
+        switch($f['type']){
+            case 1:
+                if(count($highlight_movie) < $max_count){
+                    $articles_list['highlight_movie'][] = $f['article_id'];
+                }
+                break;
+            case 2:
+                if(count($news) < $max_count){
+                    $articles_list['news'][] = $f['article_id'];
+                }
+                break;
+            case 3:
+                if(count($photo_gallery) < $max_count){
+                    $articles_list['photo_gallery'][] = $f['article_id'];
+                }
+                break;
+        }
+    }
+
+    //取得したidから記事データ取得
+    foreach ($articles_list as $key => $value) {
+        $articles = [];
+        $article_ids_sql = implode(' OR id = ', $value);
+        $sql = sprintf("select * from %s", sprintf($articletable, set_isbookmark($uid), sprintf(" and (id = %s)", $article_ids_sql)));
+        $o->query($sql);
+        while ($f = $o->fetch_array()){
+            $p[] = $f;
+          }
+        include "public/articlecomments.php";
+        $articles = $s;
+
+        if($key === 'highlight_movie'){
+            $response['pc']['photo_gallery']      = $articles;
+            for($i=count($articles);$i>$max_count_sp;$i--){
+                unset($articles[i]);
+            }
+            $response['sp']['photo_gallery']      = $articles;
+
+        }
+        if($key === 'news'){
+            $response['pc']['news']      = $articles;
+            $response['sp']['news']      = $articles;
+
+        }
+        if($key === 'photo_gallery'){
+            $response['pc']['photo_gallery']      = $articles;
+            for($i=count($articles);$i>$max_count_sp;$i--){
+                unset($articles[i]);
+            }
+            $response['sp']['photo_gallery']      = $articles;
+
+        }
+    }
+
+    $y["status"]["code"] = 200;
+    $y["status"]["user_message"] = "";
+    $y["status"]["developer_message"] = "";
+    $y["response"] = $response;
+
 } else {
-
-  $y["status"]["code"] = 404;
-  $y["status"]["user_message"] = "指定された大会は存在しません。";
-  $y["status"]["developer_message"] = "指定された大会は存在しません。";
+    $y["status"]["code"] = 404;
+    $y["status"]["user_message"] = "指定された大会は存在しません。";
+    $y["status"]["developer_message"] = "指定された大会は存在しません。";
 }
 
 print_json($y, $_SERVER['HTTP_REFERER']);
