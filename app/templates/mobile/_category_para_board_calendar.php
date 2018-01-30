8bf517439b278ef7d0c056b026bd04e659565be0
taikiken <denkosekka@gmail.com>
2018-01-30 20:55:05 +0900

    category top sp UNDO_SPBL-307

--- app/templates/mobile/_category_para_board_calendar.php
+++ app/templates/mobile/_category_para_board_calendar.php
@@ -5,3 +5,19 @@
  * @since 2018-01-30
  */
 ?>
+
+<?php
+include_once __DIR__ . '/../para-board/module/_functions.php';
+
+//$api_result = get_recent();
+// TODO 404 になるのでテスト用にデータが存在する API を使う
+$api_result = get_recent_id_year(0, 2018);
+$response = $api_result['response'];
+if (!empty($response)) :
+?>
+
+
+<?php
+endif;
+// パラボード 日程一覧 - 直近
+?>
