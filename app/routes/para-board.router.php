dfbfd14d6bd6ff54fb806f12242781024fb1aae3
taikiken <denkosekka@gmail.com>
2018-01-30 18:44:46 +0900

    routing 増えた cherry-pick

--- app/routes/para-board.router.php
+++ app/routes/para-board.router.php
@@ -21,6 +21,26 @@ $app->group('/para-board', function () use($app) {
 
   });
 
+  // 日程一覧 抽出時
+  // ==============================
+  $app->get('/{sports_id:all|[0-9]+}/{year:[0-9]+}[/]', function ($request, $response, $args) use ($app) {
+
+    $args['page'] = $app->model->set(array(
+
+    ));
+
+
+    if($app->model->property('ua') === 'desktop')
+    {
+      return $this->renderer->render($response, 'para-board/desktop/index.php', $args);
+    }
+    else
+    {
+      return $this->renderer->render($response, 'para-board/mobile/index.php', $args);
+    }
+
+  });
+
   // 試合詳細
   // ==============================
   $app->get('/{competition_id:[0-9]+}[/]', function ($request, $response, $args) use ($app) {
