46156cf0b0d5c16072ef47ce9d28be3f074d0555
taikiken <denkosekka@gmail.com>
2018-01-30 21:55:51 +0900

    router 準備しておく UNDO_SPBL-307

--- app/routes/para-board.router.php
+++ app/routes/para-board.router.php
@@ -5,8 +5,20 @@ $app->group('/para-board', function () use($app) {
   // ==============================
   $app->get('[/]', function ($request, $response, $args) use ($app) {
 
-    $args['page'] = $app->model->set(array(
+    $category = $app->model->get_category_by_slug('parasposts');
+    $template_classname = ( isset($category['theme']['base']) ) ? $category['theme']['base'] : '';
+    $template_classname .= ' parasposts';
 
+    $args['page'] = $app->model->set(array(
+      'template_classname' => $template_classname,
+      'category'       => $category,
+      'template'       => 'category',
+      'title'          => 'XXXXX',
+      'keywords'       => 'XXXXX',
+      'og_description' => 'XXXXX',
+      'og_title'       => 'XXXXX',
+      'og_url'         => $app->model->property('site_url') . 'para-board/',
+      'og_image'       => 'XXXXX',
     ));
 
 
@@ -25,8 +37,22 @@ $app->group('/para-board', function () use($app) {
   // ==============================
   $app->get('/{sports_id:all|[0-9]+}/{year:[0-9]+}[/]', function ($request, $response, $args) use ($app) {
 
-    $args['page'] = $app->model->set(array(
+    $category = $app->model->get_category_by_slug('parasposts');
+    $template_classname = ( isset($category['theme']['base']) ) ? $category['theme']['base'] : '';
+    $template_classname .= ' parasposts';
 
+    $args['page'] = $app->model->set(array(
+      'template_classname' => $template_classname,
+      'category'       => $category,
+      'template'       => 'category',
+      'title'          => 'XXXXX',
+      'keywords'       => 'XXXXX',
+      'og_description' => 'XXXXX',
+      'og_title'       => 'XXXXX',
+      'og_url'         => $app->model->property('site_url') . 'para-board/',
+      'og_image'       => 'XXXXX',
+      'sports_id'      => $args['sports_id'],
+      'year'           => $args['year'],
     ));
 
 
@@ -45,8 +71,22 @@ $app->group('/para-board', function () use($app) {
   // ==============================
   $app->get('/{competition_id:[0-9]+}[/]', function ($request, $response, $args) use ($app) {
 
-    $args['page'] = $app->model->set(array(
 
+    $category = $app->model->get_category_by_slug('parasposts');
+    $template_classname = ( isset($category['theme']['base']) ) ? $category['theme']['base'] : '';
+    $template_classname .= ' parasposts';
+
+    $args['page'] = $app->model->set(array(
+      'template_classname' => $template_classname,
+      'category'       => $category,
+      'template'       => 'category',
+      'title'          => 'XXXXX',
+      'keywords'       => 'XXXXX',
+      'og_description' => 'XXXXX',
+      'og_title'       => 'XXXXX',
+      'og_url'         => $app->model->property('site_url') . 'para-board/',
+      'og_image'       => 'XXXXX',
+      'competition_id' => $args['competition_id'],
     ));
 
 
@@ -65,9 +105,9 @@ $app->group('/para-board', function () use($app) {
   // ==============================
   $app->get('/webview[/]', function ($request, $response, $args) use ($app) {
 
-    $args['page'] = $app->model->set(array(
-
-    ));
+//    $args['page'] = $app->model->set(array(
+//
+//    ));
 
 
     return $this->renderer->render($response, 'para-board/webview.php', $args);
