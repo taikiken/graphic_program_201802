b9223c5ebaf15e2ec66a872a82ddc8c2c14fd372
YuudaiSuzuki <yuudai.suzuki@iret.co.jp>
2018-01-31 15:38:11 +0900

    パラスポーツ プルダウンAPIにsports_idを追加

--- public/api/ver1/read/competition_pulldown.php
+++ public/api/ver1/read/competition_pulldown.php
@@ -21,10 +21,10 @@ while($f=$o->fetch_array()){
 }
 if($response_flag){
 
-    $sports_list[] = 'すべて';
-    $sql=sprintf("select name from sports");
+    $sports_list['0'] = 'すべて';
+    $sql=sprintf("select id, name from sports");
     $o->query($sql);
-    while($f=$o->fetch_array()){$sports_list[]=$f['name'];}
+    while($f=$o->fetch_array()){$sports_list[$f['id']]=$f['name'];}
 
     $response['year'] = array_values(array_unique($year_list));
     $response['sports'] = $sports_list;
