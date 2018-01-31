dc0755d348942365a415fc82103d408829f4a2f5
Yu Fukuda <y_fukuda@cloudpack.jp>
2018-01-31 16:53:33 +0900

    fix: jpgのみ圧縮する feature/UNDO_SPBL-378
    (cherry picked from commit ceb4e26)

--- include/tool.php
+++ include/tool.php
@@ -761,7 +761,8 @@ function outputs($imgSubstance,$filename,$type,$size,$copy){
 	global $domain;
 
 	// 画像圧縮
-  if (exif_imagetype($filename) == IMAGETYPE_JPEG) {
+  if (exif_imagetype($RAWIMG . $filename) == IMAGETYPE_JPEG)
+  {
     $filepath_array = explode('/', $RAWIMG . $filename);
     $img_name = $filepath_array[count($filepath_array) - 1];
     exec(sprintf('mv %s /tmp/%s', $RAWIMG . $filename, $img_name));
