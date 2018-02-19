fe8c74f65cbd91cd27cdd04793a1c1d61a075fe0
Yoshiaki Fujimori <info@2244.jp>
2018-02-19 20:28:58 +0900

    UNDO_SPBL-473 | 旧アプリ向けに関連リンク表示を追加

--- app/templates/app.p.php
+++ app/templates/app.p.php
@@ -3,7 +3,6 @@
 include_once __DIR__."/mobile/_header.php";
 
 include_once __DIR__."/mobile/p.php";
-// include_once __DIR__."/_app_body.php";
 
 include_once __DIR__."/mobile/_footer-bottom.php";
 
