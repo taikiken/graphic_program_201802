3f4c880e5a14b797e59648029fbc51342247c459
Yu Fukuda <y_fukuda@cloudpack.jp>
2018-02-20 16:19:52 +0900

    feat: 2018s UNDO_SPBL-375

--- public/api/big6tv/live.php
+++ public/api/big6tv/live.php
@@ -8,11 +8,26 @@
 include "local.php";
 include_once __DIR__."/../../../app/helpers/env.helper.php";
 
-if ( UT_ENV !== 'PRODUCTION' ) :
-  $json = 'https://dev-img.sportsbull.jp/static/big6/2017/autumn/live.json';
-else:
-  $json = 'https://img.sportsbull.jp/static/big6/2017/autumn/live.json';
-endif;
+$season = $_GET["season"];
+
+
+if ($season == '2017a') //legacy
+{
+  if ( UT_ENV !== 'PRODUCTION' ) :
+    $json = 'https://dev-img.sportsbull.jp/static/big6/2017/autumn/live.json';
+  else:
+    $json = 'https://img.sportsbull.jp/static/big6/2017/autumn/live.json';
+  endif;
+}
+else
+{
+  if ( UT_ENV !== 'PRODUCTION' ) :
+    $json = 'https://dev-img.sportsbull.jp/static/big6/' . $season . '/live.json';
+  else:
+    $json = 'https://img.sportsbull.jp/static/big6/' . $season . '/live.json';
+  endif;
+}
+
 
 if ( !empty(file_get_contents($json, false, null, 0, 1)) ) :
 
