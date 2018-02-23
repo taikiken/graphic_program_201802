1c509be3990395c281fed0539c7d6edb7df42cec
大島 慎平 <s-ohshima@cloudpack.jp>
2018-02-23 17:01:55 +0900

    fix: プリロードの挙動修正

--- public/big6tv/js/videoPlayer.js
+++ public/big6tv/js/videoPlayer.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const result = document.querySelector('#placeHolder');
-const accountId = '5704890303001';
-const playerId  = 'r1Zn0fWf4f';
-let playerHTML;
-let scriptFlg = false;
-let json = [];
-let data = {
+var result = document.querySelector('#placeHolder');
+var accountId = '5704890303001';
+var playerId = 'r1Zn0fWf4f';
+var playerHTML = void 0;
+var flg = true;
+var initLoad = false;
+var json = [];
+var data = {
 	lastupdate: '',
 	live: {
 		alt: {
@@ -24,14 +25,13 @@ let data = {
 		}
 	}
 };
-let getJson = () => {
-  $.ajax({
-	type:"GET",
-    url: 'https://dev.sportsbull.jp/api/big6tv/live/2018s',
-    cache: false,
-    timeout: 10000
-  }).then(
-    (json) => {
+var getJson = function getJson() {
+	$.ajax({
+		type: "GET",
+		url: 'https://dev.sportsbull.jp/api/big6tv/live/2018s',
+		cache: false,
+		timeout: 10000
+	}).then(function (json) {
 		data.lastupdate = json.response.lastupdat;
 		data.live.alt.large = json.response.live.alt.large;
 		data.live.alt.medium = json.response.live.alt.medium;
@@ -40,35 +40,39 @@ let getJson = () => {
 		data.live.interval = json.response.live.interval;
 		data.live.isPlaying = json.response.live.isPlaying;
 		data.live.video.id = json.response.live.video.id;
+		if (!initLoad) {
+			initLoad = true;
+			setInterval(getJson, data.live.interval * 1000);
+		}
 		if (data.live.isPlaying) {
-			let playerHTML = `<video id=\"myPlayerID\" class=\"video-js\" data-video-id=\"${data.live.video.id}\" data-account=\"${accountId}\" data-player=\"${playerId}\" data-embed=\"default\" controls width=\"728\" height=\"410\" >`;
-			result.innerHTML = playerHTML;
-			let scriptTag = document.createElement('script');
-			scriptTag.src = `\/\/players.brightcove.net\/${accountId}\/${playerId}_default\/index.min.js`;
-			if (!scriptFlg) {
+			if (!flg) {
+				location.reload();
+			} else {
+				var scriptTag = document.createElement('script');
+				scriptTag.src = '//players.brightcove.net/' + accountId + '/' + playerId + '_default/index.min.js';
 				document.body.appendChild(scriptTag);
-				scriptFlg= true
+				scriptTag.onload = function () {
+					callback();
+				};
 			}
-			scriptTag.onload = callback;
-			console.log(data.live.interval * 1000);
 		} else {
-			let liveBeforeImg = `<img src=\"${data.live.alt.large}\">`
+
+			var liveBeforeImg = '<img src="' + data.live.alt.large + '">';
+			flg = data.live.isPlaying;
 			result.innerHTML = liveBeforeImg;
 		}
-    },
-    () => {
-      console.log('failed load')
-	  let loadErrorImg = `<img src=\"${data.live.error.large}\">`;
-	  result.innerHTML = loadErrorImg;
-    }
-  )
-};
-
-let callback = () => {
-	videojs('myPlayerID').on('ads-ad-ended', function( evt ){
-		let myPlayer = this;
-		myPlayer.play();
+	}, function () {
+		console.log('failed load');
+		var loadErrorImg = '<img src="' + data.live.error.large + '">';
+		result.innerHTML = loadErrorImg;
 	});
 };
 
+var callback = function callback() {
+	videojs('myPlayerID').on('ads-ad-ended', function (evt) {
+		var myPlayer = this;
+		myPlayer.play();
+	});
+};
 getJson();
+//# sourceMappingURL=videoPlayer.js.map
