8b28a5119ea668f3b7d7b74b8603b680d6dae5a4
Yu Fukuda <y_fukuda@cloudpack.jp>
2018-03-01 21:17:22 +0900

    feat: 一旦desktopのみね 平昌と地域 UNDO_SPBL-180

--- app/templates/desktop/category_pyeongchang2018.php
+++ app/templates/desktop/category_pyeongchang2018.php
@@ -30,6 +30,12 @@ include_once __DIR__.'/_category-heading.php';
     <section class="main-sec">
 
       <?php
+      // 注目のアスリート
+      // ----------------------------------------------------
+      if(empty($page['list']) === false) :
+        include_once __DIR__.'/_category-pickup_player.php';
+      endif;
+
       // medals
       include_once __DIR__ . '/../pyeongchang2018/module/_pyeongchang2018_medals.php';
       ?>
