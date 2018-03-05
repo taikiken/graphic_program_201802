826511a7b26fbca65eb65f85f0eff81a2d6d3a87
Kakoi <k@parachute-canopy.org>
2018-03-05 10:02:50 +0900

    UNDO_SPBL-471 【WEB】レッドブル・クラッシュドアイスライブ配信
    PC/SP一旦完了

---
+++ app/templates/red-bull-crashed-ice-2018/_include/_static_contents.php
@@ -0,0 +1,260 @@
+<?php
+/**
+ * Author: kakoi
+ * Date: 2018/03/05
+ * Time: 00:00
+ * UNDO_SPBL-471 【WEB】レッドブル・クラッシュドアイスライブ配信
+ * https://aws-plus.backlog.jp/view/UNDO_SPBL-471
+ */
+?>
+<div class="crashedice--intro crashedice__container">
+  <p class="intro__catch">アイスクロス・ダウンヒルの先駆けとして、2001年の初開催以来、世界トップレベルのスリルを誇るウィンタースポーツイベントへと成長を重ねてきたRed Bull Crashed Ice!<br />
+  2017－18シーズンの締めくくり、カナダ・アルバータでの熱狂を見逃すな！</p>
+
+  <div class="intro__banner">
+    <a href="hoge" target="_blank"><img class="show-for-pc" src="/assets/images/red-bull-crashed-ice-2018/banner-crashedice.jpg" alt="Red Bull TV"><img class="show-for-sp" src="/assets/sp/images/red-bull-crashed-ice-2018/banner-crashedice.jpg" alt="Red Bull TV"></a>
+  </div>
+
+  <div class="intro__link__app"><a href="https://app.adjust.com/paf496?deep_link=sportsbull%3A%2F%2F" target="_blank">アプリでライブ配信開始の通知を受け取る</a></div>
+
+  <div class="intro__timetable">
+    <table>
+      <tbody>
+        <tr>
+          <th>12:00 －</th>
+          <td>コース観戦エリア 開場</td>
+        </tr>
+        <tr>
+          <th>12:30 －</th>
+          <td>選手紹介</td>
+        </tr>
+        <tr>
+          <th>13:00 －</th>
+          <td>オープニングアクト</td>
+        </tr>
+        <tr>
+          <th>13:30 －</th>
+          <td>一次予選 開始</td>
+        </tr>
+        <tr>
+          <th>14:00 －</th>
+          <td>二次予選 開始</td>
+        </tr>
+        <tr>
+          <th>14:30 －</th>
+          <td>決勝戦</td>
+        </tr>
+        <tr>
+          <th>15:00 －</th>
+          <td>レース終了予定</td>
+        </tr>
+        <tr>
+          <th>16:00 －</th>
+          <td>表彰式 / 閉会</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p class="intro__timetable__note">※ スケジュールは、当日の進行により変更になる可能性があります。</p>
+  </div><!-- /.intro__schedule -->
+
+  <div class="intro__widjet_twitter">
+    <a class="twitter-timeline"  href="https://twitter.com/hashtag/%E3%83%AC%E3%83%83%E3%83%89%E3%83%96%E3%83%AB%E3%82%AF%E3%83%A9%E3%83%83%E3%82%B7%E3%83%A5%E3%83%89%E3%82%A2%E3%82%A4%E3%82%B9" data-widget-id="970345530147524608">#レッドブルクラッシュドアイス のツイート</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+  </div><!-- /.widjet_twitter -->
+</div><!-- /.crashedice--intro -->
+
+<div class="crashedice--about crashedice__container">
+  <h2 class="about__heading"><img class="show-for-pc" src="/assets/images/red-bull-crashed-ice-2018/about-heading.png" alt="Red Bull Crashed Iceとは"><img class="show-for-sp" src="/assets/sp/images/red-bull-crashed-ice-2018/about-heading.png" alt="Red Bull Crashed Iceとは"></h2>
+
+  <div class="about__text">
+    <p>アイスクロス・ダウンヒル「Red Bull Crashed Ice」は2001年の初開催以来、世界トップレベルのスリルを誇るウィンタースポーツイベントへと成長を重ねてきた。最長600mのコースを4人のライダーがひしめき合いながら勝利を目指して滑り下りていくこのレースは、シケイン、ジャンプ、ローラーなどが配置された急峻なダウンヒルトラックが舞台になる。トラック上ではプッシングやスライディング、全力疾走など、ありとあらゆるアクションが展開されるが、ルールは至ってシンプルで、最初にゴールに到達したライダーが勝利を手にする。</p>
+    <p>雪山のスロープや市街地などに設置される極めて先進的なトラックで展開されるRed Bull Crashed Iceは、これまで延べ数十万人もの観客を集めてきた。初期はワンオフイベントとして開催されていたが、2010年以降は正式なアイスクロス・ダウンヒル・ワールドチャンピオンシップとして開催されている。</p>
+  </div><!-- /.about__text -->
+
+  <div class="about__movie">
+    <a href="hoge"><img src="/assets/images/red-bull-crashed-ice-2018/about-movie-poster.jpg" alt=""></a>
+  </div><!-- /.about__movie -->
+
+  <div class="about__featured">
+    <h3 class="about__featured__heading"><img class="show-for-pc" src="/assets/images/red-bull-crashed-ice-2018/about-featured-heading.png" alt="注目選手"><img class="show-for-sp" src="/assets/sp/images/red-bull-crashed-ice-2018/about-featured-heading.png" alt="注目選手"></h3>
+
+    <ul class="about__featured__list">
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb1.jpg" alt=""></figure>
+        <h4>キャメロン・ナーズ</h4>
+        <p class="ruby">Cameron Naasz</p>
+        <p class="country">USA</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb2.jpg" alt=""></figure>
+        <h4>スコット・クロクソール</h4>
+        <p class="ruby">Scott Croxall</p>
+        <p class="country">Canada</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb3.jpg" alt=""></figure>
+        <h4>カイル・クロクソール</h4>
+        <p class="ruby">Kyle Croxall</p>
+        <p class="country">Canada</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb4.jpg" alt=""></figure>
+        <h4>マルコ・ダラーゴ</h4>
+        <p class="ruby">Marco Dallago</p>
+        <p class="country">Austria</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb5.jpg" alt=""></figure>
+        <h4>ルカ・ダラーゴ</h4>
+        <p class="ruby">Luca Dallago</p>
+        <p class="country">Austria</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb6.jpg" alt=""></figure>
+        <h4>ジャクリーン・レジェール</h4>
+        <p class="ruby">Jacqueline Legere</p>
+        <p class="country">Canada</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb7.jpg" alt=""></figure>
+        <h4>アマンダ・トルンゾ</h4>
+        <p class="ruby">Amanda Trunzo</p>
+        <p class="country">USA</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-thumb8.jpg" alt=""></figure>
+        <h4>山本 純子</h4>
+        <p class="ruby">Junko Yamamoto</p>
+        <p class="country">Japan</p>
+      </li><!-- /.about__featured__item -->
+      <li class="about__featured__item">
+        <figure><img src="/assets/images/red-bull-crashed-ice-2018/about-featured-andmore.png" alt="and MORE."></figure>
+      </li><!-- /.about__featured__item -->
+    </ul><!-- /.about__featured__list -->
+  </div><!-- /.about__featured -->
+</div><!-- /.crashedice--course -->
+
+<div class="crashedice--overview crashedice__container">
+  <h2 class="overview__heading"><img class="show-for-pc" src="/assets/images/red-bull-crashed-ice-2018/overview-heading.png" alt="イベント概要"><img class="show-for-sp" src="/assets/sp/images/red-bull-crashed-ice-2018/overview-heading.png" alt="イベント概要"></h2>
+
+  <table class="overview__data">
+    <tbody>
+      <tr>
+        <th>大会名</th>
+        <td>Red Bull Crashed Ice</td>
+      </tr>
+      <tr>
+        <th>開催日</th>
+        <td>2018年 3月11日</td>
+      </tr>
+      <tr>
+        <th>開催場所</th>
+        <td>カナダ・アルバータ州 エドモントン</td>
+      </tr>
+      <tr>
+        <th>主 催</th>
+        <td>レッドブル・ジャパン株式会社</td>
+      </tr>
+      <tr>
+        <th>主 管</th>
+        <td>レッドブル・ジャパン株式会社</td>
+      </tr>
+      <tr>
+        <th>WEB</th>
+        <td>http://crashedice.redbull.com/ja_INT</td>
+      </tr>
+    </tbody>
+  </table><!-- /.overview__data -->
+</div><!-- /.crashedice--overview -->
+
+<div class="crashedice--related crashedice__container">
+  <h2 class="related__heading"><img class="show-for-pc" src="/assets/images/red-bull-crashed-ice-2018/related-heading.png" alt="関連記事"><img class="show-for-sp" src="/assets/sp/images/red-bull-crashed-ice-2018/related-heading.png" alt="関連記事"></h2>
+
+  <div class="related__post">
+    <a href="hoge" class="movie">
+      <figure class="related__post__thumb"><span><img src="/assets/images/red-bull-crashed-ice-2018/related-thumb1.jpg" alt=""></span></figure>
+      <div class="related__post__text">
+        <h3 class="related__post__heading">Red Bull Crashed Ice, Marseille</h3>
+        <p class="related__post__text">アイスクロス・ダウンヒルの先駆けとして、2001年の初開催以来、世界トップレベルのスリルを誇るウィンタースポーツイベントへと成長を重ねてきたRed Bull Crashed Ice! 2017－18シーズンの締めくくり、カナダ・アルバータでの熱狂を見逃すな！</p>
+        <p class="related__post__date">3月3日（木） 22:04</p>
+      </div><!-- /.related__post__text -->
+    </a>
+  </div><!-- /.related__post -->
+
+  <div class="related__post">
+    <a href="hoge" class="movie">
+      <figure class="related__post__thumb"><span><img src="/assets/images/red-bull-crashed-ice-2018/related-thumb2.jpg" alt=""></span></figure>
+      <div class="related__post__text">
+        <h3 class="related__post__heading">Red Bull Crashed Ice, Jyväskylä</h3>
+        <p class="related__post__text">アイスクロス・ダウンヒルの先駆けとして、2001年の初開催以来、世界トップレベルのスリルを誇るウィンタースポーツイベントへと成長を重ねてきたRed Bull Crashed Ice! 2017－18シーズンの締めくくり、カナダ・アルバータでの熱狂を見逃すな！</p>
+        <p class="related__post__date">3月3日（木） 22:04</p>
+      </div><!-- /.related__post__text -->
+    </a>
+  </div><!-- /.related__post -->
+
+  <div class="related__post">
+    <a href="hoge" class="movie">
+      <figure class="related__post__thumb"><span><img src="/assets/images/red-bull-crashed-ice-2018/related-thumb3.jpg" alt=""></span></figure>
+      <div class="related__post__text">
+        <h3 class="related__post__heading">Red Bull Crashed Ice, Saint Paul</h3>
+        <p class="related__post__text">アイスクロス・ダウンヒルの先駆けとして、2001年の初開催以来、世界トップレベルのスリルを誇るウィンタースポーツイベントへと成長を重ねてきたRed Bull Crashed Ice! 2017－18シーズンの締めくくり、カナダ・アルバータでの熱狂を見逃すな！</p>
+        <p class="related__post__date">3月3日（木） 22:04</p>
+      </div><!-- /.related__post__text -->
+    </a>
+  </div><!-- /.related__post -->
+
+  <div class="related__post">
+    <a href="hoge" class="movie">
+      <figure class="related__post__thumb"><span><img src="/assets/images/red-bull-crashed-ice-2018/related-thumb4.jpg" alt=""></span></figure>
+      <div class="related__post__text">
+        <h3 class="related__post__heading">Red Bull Crashed Ice, Saint Paul</h3>
+        <p class="related__post__text">アイスクロス・ダウンヒルの先駆けとして、2001年の初開催以来、世界トップレベルのスリルを誇るウィンタースポーツイベントへと成長を重ねてきたRed Bull Crashed Ice! 2017－18シーズンの締めくくり、カナダ・アルバータでの熱狂を見逃すな！</p>
+        <p class="related__post__date">3月3日（木） 22:04</p>
+      </div><!-- /.related__post__text -->
+    </a>
+  </div><!-- /.related__post -->
+
+  <div class="related__topics">
+    <h4 class="related__topics__heading"><img class="show-for-pc" src="/assets/images/red-bull-crashed-ice-2018/related-topics-heading.png" alt="外部記事"><img class="show-for-sp" src="/assets/sp/images/red-bull-crashed-ice-2018/related-topics-heading.png" alt="外部記事"></h4>
+
+    <div class="related__topics__post">
+      <a href="hoge" target="_blank">
+        <figure class="related__topics__post__thumb"><img src="/assets/images/red-bull-crashed-ice-2018/related-topics-thumb1.jpg" alt=""></figure>
+        <div class="related__topics__post__text">
+          <h5><span>【５分で学べる】</span><br />
+          Red Bull Crashed Ice、５つの基礎知識</h5>
+        </div><!-- /.related__topics__post__text -->
+      </a>
+    </div><!-- /.related__topics__post -->
+
+    <div class="related__topics__post">
+      <a href="hoge" target="_blank">
+        <figure class="related__topics__post__thumb"><img src="/assets/images/red-bull-crashed-ice-2018/related-topics-thumb2.jpg" alt=""></figure>
+        <div class="related__topics__post__text">
+          <h5><span>【Red Bull Crashed Ice】 タイヘンなのは氷上だけじゃない？</span><br />
+          ライダー山本純子の意外な５つの事実</h5>
+        </div><!-- /.related__topics__post__text -->
+      </a>
+    </div><!-- /.related__topics__post -->
+
+    <div class="related__topics__post">
+      <a href="hoge" target="_blank">
+        <figure class="related__topics__post__thumb"><img src="/assets/images/red-bull-crashed-ice-2018/related-topics-thumb3.jpg" alt=""></figure>
+        <div class="related__topics__post__text">
+          <h5><span>【ならず者アスリート登場？】</span><br />
+          Red Bull Crashed Iceライダーがオモシロすぎる件</h5>
+        </div><!-- /.related__topics__post__text -->
+      </a>
+    </div><!-- /.related__topics__post -->
+
+    <div class="related__topics__post">
+      <a href="hoge">
+        <figure class="related__topics__post__thumb"><img src="/assets/images/red-bull-crashed-ice-2018/related-topics-thumb4.jpg" alt=""></figure>
+        <div class="related__topics__post__text">
+          <h5>Red Bull Crashed Iceを試してみた</h5>
+        </div><!-- /.related__topics__post__text -->
+      </a>
+    </div><!-- /.related__topics__post -->
+  </div><!-- /.related__topics -->
+</div><!-- /.crashedice--related -->
