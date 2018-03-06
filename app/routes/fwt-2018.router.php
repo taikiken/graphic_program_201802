c128530a82f576defba8c2cf85f7fbd4fdd61cfe
adachi <yuta.adachi@sportsbull.jp>
2018-03-06 13:09:06 +0900

    FWTアンドラ大会リリース

--- app/routes/fwt-2018.router.php
+++ app/routes/fwt-2018.router.php
@@ -15,11 +15,11 @@ $app->group('/fwt',  function () use($app) {
     'og_image'           => $app->model->property('site_url').'assets/images/fwt/ogp.png',
     'og_description'     => 'Freeride World Tour 2018をスポーツブルで無料ライブ配信。スポーツブル(スポブル)は、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
     'keywords'           => 'Freeride World Tour 2018,スポブル,ライブ配信,スポーツ,メディア,クレイジー,アスリート,ニュース,動画,sports,media,crazy',
-    'template'           => 'webview',
+    'category'           => $category,
+    'template'           => 'category',
     'template_classname' => 'fwt',
     'ua'                 => $app->model->property('ua'),
     'app_id'             => '842032129256034',
-    'category'           => $category,
     'sns'                => array(
       'twitter'  => 'sportsbull_jp',
       'facebook' => 'sportsbull',
