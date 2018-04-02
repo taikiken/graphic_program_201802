ffef1f980dc60915b4cb41eaa1111a4d533bf66c
Yoshiaki Fujimori <info@2244.jp>
2018-04-02 23:12:43 +0900

    part - head_video の内容ファイル分割

--- app/templates/desktop/_header.php
+++ app/templates/desktop/_header.php
@@ -1,106 +1,44 @@
+<?php if ( $page['conditional']['html_start'] ) : ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="ja">
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# <?php echo $page['og_type']; ?>: http://ogp.me/ns/<?php echo $page['og_type']; ?>#">
   <meta charset="UTF-8">
+<?php endif; ?>
 
 
-<?php include_once __DIR__."/../_head.php"; ?>
+<?php
+if ( $page['conditional']['head'] ) :
+  include_once __DIR__."/../_head.php";
+endif;
+?>
 
-<?php if(count($page['photo']) > 0):?>
-  <link rel="stylesheet" href="/assets/css/style_pc.css?v=<?php echo $page['version']; ?>">
-  <script src="/assets/js/libs.js?v=<?php echo $page['version']; ?>"></script>
-<?php endif;?>
+
+<?php if ( $page['conditional']['head_assets'] ) : ?>
+  <?php if(count($page['photo']) > 0):?>
+    <link rel="stylesheet" href="/assets/css/style_pc.css?v=<?php echo $page['version']; ?>">
+    <script src="/assets/js/libs.js?v=<?php echo $page['version']; ?>"></script>
+  <?php endif;?>
+
   <link rel="stylesheet" href="/assets/css/ui.css?v=<?php echo $page['version']; ?>">
   <script src="/assets/js/libs/vendor.react.js?v=<?php echo $page['version']; ?>"></script>
   <script src="/assets/js/bundle/main.bundle.js?v=<?php echo $page['version']; ?>"></script>
+<?php endif; ?>
 
-<?php include_once __DIR__.'/../_head_bottom.php'; ?>
 
 <?php
-// ---------------------------------------------------------------------------
-// @since 2016-11-13
-// 記事詳細・次の記事一覧のメインビジュアルを動画に変更
-// 常に videojs 関連を読込む
-//if ( $page['template'] == 'p' ) :
-
-// @since 2016-01-13
-// hotfix @see https://github.com/undotsushin/undotsushin/issues/1468
-//if ( $page['template'] == 'p' && $page['post']['media']['video']['player'] == 'brightcove' ) :
-if ( $page['template'] == 'p'
-  && $page['post']['media']['video']['player'] == 'brightcove'
-  && $page['post']['media_vk_refid'] == ''
-) :
-  // brightcove code をここに
-  // JS で非同期で読み込むと付随コードの読み込みが行われない様子
+if ( $page['conditional']['head_bottom'] ) :
+  include_once __DIR__."/../_head_bottom.php";
+endif;
 ?>
-  <style>
-    body.vjs-full-window {
-      padding: 0;
-      margin: 0;
-      height: 100%;
-    }
-    .video-js.vjs-fullscreen {
-      position: fixed;
-      overflow: hidden;
-      z-index: 1000;
-      left: 0;
-      top: 0;
-      bottom: 0;
-      right: 0;
-      width: 100% !important;
-      height: 100% !important;
-    }
-    .video-js:-webkit-full-screen {
-      width: 100% !important;
-      height: 100% !important;
-    }
-    .video-js.vjs-fullscreen.vjs-user-inactive {
-      cursor: none;
-    }
-  </style>
 
-  <link href="//vjs.zencdn.net/5.3/video-js.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="/assets/ima_plugin/css/videojs.ads.css" />
-  <link rel="stylesheet" href="/assets/ima_plugin/css/videojs.ima.css" />
-  <link rel="stylesheet" href="/assets/ima_plugin/css/ima-style.css" />
 
-  <script src="//vjs.zencdn.net/5.3/video.min.js"></script>
-  <script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>
+<?php if ( $page['conditional']['head_video'] ) :
+  include_once __DIR__."/_head_video.php";
+endif;
+?>
 
-  <script src="/assets/js/libs/hls/videojs-contrib-hls.min.js"></script>
-  <script src="/assets/ima_plugin/js/videojs.hls.js"></script>
-  <script src="/assets/ima_plugin/js/videojs.ads.js"></script>
-  <script src="/assets/ima_plugin/js/videojs.ima.js"></script>
 
-  <style type="text/css">
-    #mainContainer{
-      border: 1px #fff solid;
-      box-sizing: border-box;
-      overflow: hidden;
-      padding: 0;
-      margin: 0;
-      padding-top: 30px;
-      position: relative;
-      width:100%;
-    }
-    #single-visual-container{
-      box-sizing: border-box;
-    }
-    .vjs-poster{
-      display: none !important;
-    }
-    .video-js{
-      background-color: #fff !important;
-    }
-    .vjs-big-play-button{
-      z-index: 9999;
-    }
-  </style>
 <?php
- endif;
-// eof brightcove
-// ---------------------------------------------------------------------------
-
 // header 表示条件 & template_classname に使用する template 名称を $template_name へ
 $template_name = $page['template'];
 
@@ -171,9 +109,11 @@ if ( $template_name == 'category' ) {
     }
 }
 ?>
+
 </head>
 <body>
 <div id="whole" class="whole <?php echo join( ' ', $whole_classes);?>">
+
 <?php
 // header 表示条件
 if (
