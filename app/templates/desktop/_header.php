b4530adba0652a1d5c74076344a0a88c5968baba
Yoshiaki Fujimori <info@2244.jp>
2018-04-03 09:06:41 +0900

    parts - wholeの判定わける

--- app/templates/desktop/_header.php
+++ app/templates/desktop/_header.php
@@ -20,8 +20,6 @@ endif;
   <?php endif;?>
 
   <link rel="stylesheet" href="/assets/css/ui.css?v=<?php echo $page['version']; ?>">
-  <script src="/assets/js/libs/vendor.react.js?v=<?php echo $page['version']; ?>"></script>
-  <script src="/assets/js/bundle/main.bundle.js?v=<?php echo $page['version']; ?>"></script>
 <?php endif; ?>
 
 
@@ -38,85 +36,19 @@ endif;
 ?>
 
 
-<?php
-// header 表示条件 & template_classname に使用する template 名称を $template_name へ
-$template_name = $page['template'];
+<?php if ( $page['conditional']['body_start'] ) : ?>
+  </head>
+  <body>
+<?php endif; ?>
 
-// .whole へ追加する CSS class を設定します
-$whole_classes = array();
 
-// $page['template_classname'] に設定されている CSS class を追加します
-if ( !empty( $page['template_classname'] ) ) {
-  $whole_classes[] = $page['template_classname'];
-}
-// 記事詳細
-if ( $template_name == 'p' ) {
-
-  // 記事詳細へ識別 CSS class 追加
-  $whole_classes[] = 'post-single';
-
-  // theme 設定 class を追加
-  // JSON レスポンスの theme.base を CSS class へ追加します
-  if ( $page['theme']['base'] ) {
-    $whole_classes[] = $page['theme']['base'];
-  }
-}
-
-// in category
-if ( $template_name == 'category' ) {
-
-    //crazy athletes除外
-    if($page['category']['slug'] != 'crazy') {
-      // @since 2016-09-01
-      // https://github.com/undotsushin/undotsushin/issues/1053
-      $whole_classes[] = 'layout-list';
-      // ---[end 2016-09-01]---
-
-      // template_classname があれば
-      if ( !empty($page['template_classname']) && !in_array($page['template_classname'], $whole_classes) ) {
-        $whole_classes[] = $page['template_classname'];
-      }
-
-  // @see https://github.com/undotsushin/undotsushin/issues/1891#issuecomment-298291706
-  // @since 2017-05-08 - category slug を追加する
-        $whole_classes[] = $page['category']['slug'];
-    }
-} elseif ( $template_name == 'search' ) {
-  // @since 2016-09-01
-  // https://github.com/undotsushin/undotsushin/issues/1053
-  $whole_classes[] = 'layout-list';
-  // ---[end 2016-09-01]---
-} elseif ( $template_name == 'index' ) {
-  // @since 2016-09-01
-  // https://github.com/undotsushin/undotsushin/issues/1053
-  $whole_classes[] = 'home';
-  // ---[end 2016-09-01]---
-} elseif ( $template_name == 'p' ) {
-  // @since 2016-09-30
-  $whole_classes[] = 'layout-detail';
-  // 記事詳細 `big6tv` の時に `theme_big6` を whole へ追加する
-  // @since 2017-03-24
-  $page_category = $page['category'];
-  if (isset($page_category) && $page_category['slug'] == 'big6tv' && !in_array('theme_big6', $whole_classes)) {
-    $whole_classes[] = 'theme_big6';
-  }
-
-  // @see https://github.com/undotsushin/undotsushin/issues/1891#issuecomment-298291706
-  // @since 2017-05-08 - category slug を追加する
-    if($page_category['slug'] != 'crazy')
-    {
-        $whole_classes[] = $page_category['slug'];
-    }
-}
+<?php if ( $page['conditional']['whole'] ) :
+  include_once __DIR__."/_whole.php";
+endif;
 ?>
 
-</head>
-<body>
-<div id="whole" class="whole <?php echo join( ' ', $whole_classes);?>">
-
 
 <?php
-
 // header & gnav 表示条件
 // TODO - model側でやりたい
 $conditional_header = array(
@@ -134,7 +66,8 @@ $conditional_header = array(
   'mypage.activities',
   'notifications',
   'logout',
-  'crazy'
+  'crazy',
+  'inc'
 );
 
 if ( !in_array($page['template'], $conditional_header, true) ) :
