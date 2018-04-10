7d043b742111e13545529cc5c72b51c7ca4f019a
kinjo_stg <kinjo_stg@iret.co.jp>
2018-04-10 19:31:51 +0900

    親タブ一覧のページタイトル変更 子タブ一覧ページタイトルを"「親タブ名」の子タブに変更"

--- include/lib/bottom_tabs_category/dx.php
+++ include/lib/bottom_tabs_category/dx.php
@@ -1,7 +1,7 @@
 <?php
 
-$TITLE=$PARENT="競技 親タブ設定";
-$THIS="タイトル";
+$TITLE=$PARENT="競技タブ設定";
+$THIS="親タブ";
 
 $l[]=sprintf("<li class=\"root\"><a href=\"%s\">%s</a></li>",$ADPATH,"メインメニュー");
 $l[]=sprintf("<li class=\"root\"><a href=\"%s\">%s</a></li>",$ADPATH."tabs",$PARENT);
--- include/lib/bottom_tabs_nodes/dx.php
+++ include/lib/bottom_tabs_nodes/dx.php
@@ -1,7 +1,10 @@
 <?php
+$sql = "SELECT name FROM bottom_tab_categories WHERE id = ".$_GET['parent_tab_id'];
+$o->query($sql);
+$parent_name=$o->fetch_array();
 
-$TITLE=$PARENT="競技 子タブ設定";
-$THIS="タイトル";
+$TITLE=$PARENT='競技タブ設定';
+$THIS='「'.$parent_name['name'].'」の子タブ';
 
 $l[]=sprintf("<li class=\"root\"><a href=\"%s\">%s</a></li>",$ADPATH,"メインメニュー");
 $l[]=sprintf("<li class=\"root\"><a href=\"%s\">%s</a></li>",$ADPATH."tabs",$PARENT);
