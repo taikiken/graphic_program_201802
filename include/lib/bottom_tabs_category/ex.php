3a88e917a02a04376dd3282d9e5d481a9e178669
kinjo_stg <kinjo_stg@iret.co.jp>
2018-04-11 14:27:50 +0900

    確認画面までの処理

--- include/lib/bottom_tabs_category/ex.php
+++ include/lib/bottom_tabs_category/ex.php
@@ -1,9 +1,39 @@
 <?php
 
-$a[]=array("textfield",$THIS,"title","30","","","");
-$a[]=array("menu","20","rb",6,"","","","","","","","category_id");
+if($q->get_dir()!=2){
 
-include $INCLUDEPATH."print_write.php";
+  ?>
 
+  <?php if($q->get_file()==0){ ?>
+  <?php }elseif($q->get_file()==1){ ?>
+    <?php
+  }
+
+  ?>
+
+  <?php if($q->get_file()==0){ ?>
+
+  <?php }elseif($q->get_file()==1){ ?>
+    <?php
+  }
+
+
+}
+
+$a[] = array("textfield", "名称", "name", "120");
+
+$a[] = array("textfield", "リンク", "link", "120");
+
+$a[] = array("textfield", "アイコン(PC)", "icon", "120");
+
+$a[] = array("textfield", "アイコン(SP)", "icon_sp", "120");
+
+$a[] = array("textfield", "アイコン(iOS)", "icon_ios", "120");
+
+$a[] = array("textfield", "アイコン(Android)", "icon_android", "120");
+
+include $INCLUDEPATH . "print_write.php";
+include $INCLUDEPATH."lib/".$CURRENTDIRECTORY."/categories.php";
 include $INCLUDEPATH."_pageaddition.php";
+
 ?>
