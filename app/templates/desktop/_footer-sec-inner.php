7da32676be7b8d2272223bfe6dcf7b506ff78bde
Yoshiaki Fujimori <info@2244.jp>
2018-04-13 11:44:11 +0900

    parts  - footerのclass指定にprefixつける

--- app/templates/desktop/_footer-sec-inner.php
+++ app/templates/desktop/_footer-sec-inner.php
@@ -1,31 +1,31 @@
-<div class="foot-sec-inner">
+<div class="<?php echo $page['html_prefix']; ?>foot-sec-inner">
   <?php
   // SEO対策 / パンくずリストを設置する #776
   include_once __DIR__."/../_breadcrumb.php"; ?>
-  <div class="foot-pr">
-    <div class="foot-pr-inner">
+  <div class="<?php echo $page['html_prefix']; ?>foot-pr">
+    <div class="<?php echo $page['html_prefix']; ?>foot-pr-inner">
       <figure><img src="/assets/images/common/footer-overview-figure.png" alt=""></figure>
 
-      <div class="text-block">
-        <h3 class="foot-pr-logo"><img src="/assets/images/common/footer-overview-logo.png" alt="SPORTS BULL アプリ版(iPhone/Android)"></h3>
-        <ul class="foot-pr-btn">
+      <div class="<?php echo $page['html_prefix']; ?>text-block">
+        <h3 class="<?php echo $page['html_prefix']; ?>foot-pr-logo"><img src="/assets/images/common/footer-overview-logo.png" alt="SPORTS BULL アプリ版(iPhone/Android)"></h3>
+        <ul class="<?php echo $page['html_prefix']; ?>foot-pr-btn">
           <li><a href="https://itunes.apple.com/jp/app/undotsushin/id1086719653?l=ja&ls=1&mt=8" target="_blank"><img src="/assets/images/common/footer-overview-btn-applestore.png" alt="App Store" /></a></li>
           <li><a href="https://play.google.com/store/apps/details?id=com.undotsushin" target="_blank"><img src="/assets/images/common/footer-overview-btn-googleplay.png" alt="Google play"></a></li>
         </ul>
-        <p class="foot-pr-text">話題のスポーツニュースがサクサク読める、無料のニュースまとめアプリ「スポーツ・ブル」。高品質なスポーツのニュース、動画をいつでもお楽しみ頂けます。スマートフォンアプリをダウンロードして今日のニュースをチェックしましょう。</p>
+        <p class="<?php echo $page['html_prefix']; ?>foot-pr-text">話題のスポーツニュースがサクサク読める、無料のニュースまとめアプリ「スポーツ・ブル」。高品質なスポーツのニュース、動画をいつでもお楽しみ頂けます。スマートフォンアプリをダウンロードして今日のニュースをチェックしましょう。</p>
       </div><!-- /.text-block -->
 
-      <div class="fb-page-plugin">
+      <div class="<?php echo $page['html_prefix']; ?>fb-page-plugin">
         <div class="fb-page" data-href="https://www.facebook.com/sportsbull/" data-width="400" data-height="154" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/sportsbull/"><a href="https://www.facebook.com/sportsbull/">スポーツブル（SPORTS BULL）</a></blockquote></div></div>
       </div>
     </div><!-- /.foot-pr-inner -->
   </div><!-- /.foot-pr -->
 
-  <div id="pageTop" class="pagetop fixed blind"><a href="#"><span>このページの先頭へ<img src="/assets/images/sprite/sprite-footer.png" role="presentation"></span></a></div>
+  <div id="pageTop" class="<?php echo $page['html_prefix']; ?>pagetop fixed blind"><a href="#"><span>このページの先頭へ<img src="/assets/images/sprite/sprite-footer.png" role="presentation"></span></a></div>
 
-  <div class="fnav-block">
-    <nav class="fnav">
-      <h3 class="fnav-logo"><img src="/assets/images/common/footer-fnav-logo.png" alt="SPORTS BULL"></h3>
+  <div class="<?php echo $page['html_prefix']; ?>fnav-block">
+    <nav class="<?php echo $page['html_prefix']; ?>fnav">
+      <h3 class="<?php echo $page['html_prefix']; ?>fnav-logo"><img src="/assets/images/common/footer-fnav-logo.png" alt="SPORTS BULL"></h3>
       <ul>
         <?php // <li><a href="/about/ads/">広告掲載について</a></li> ?>
         <li><a href="/about/">サービス紹介</a></li>
@@ -37,11 +37,11 @@
       </ul>
     </nav><!-- /.fnav -->
 
-    <div class="sns-block">
+    <div class="<?php echo $page['html_prefix']; ?>sns-block">
       <ul>
-        <li class="sns-fb"><a href="https://www.facebook.com/sportsbull/" target="_blank">facebook</a></li>
-        <li class="sns-tw"><a href="https://twitter.com/sportsbull_jp" target="_blank">twitter</a></li>
-        <li class="sns-yt"><a href="https://www.youtube.com/channel/UCKwqba9IWuSKIk3DIpryOHw" target="_blank">youtube</a></li>
+        <li class="<?php echo $page['html_prefix']; ?>sns-fb"><a href="https://www.facebook.com/sportsbull/" target="_blank">facebook</a></li>
+        <li class="<?php echo $page['html_prefix']; ?>sns-tw"><a href="https://twitter.com/sportsbull_jp" target="_blank">twitter</a></li>
+        <li class="<?php echo $page['html_prefix']; ?>sns-yt"><a href="https://www.youtube.com/channel/UCKwqba9IWuSKIk3DIpryOHw" target="_blank">youtube</a></li>
       </ul>
     </div><!-- /.sns-block -->
 
