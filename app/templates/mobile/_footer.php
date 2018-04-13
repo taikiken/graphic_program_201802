123379b2a9fcb469275d5195f2e0a507f4dadb96
Yoshiaki Fujimori <info@2244.jp>
2018-04-13 12:45:47 +0900

    parts - spのサイドメニューも調整
    react生成のhtmlのclassもcss先に納品用にprefixつけちゃう

--- app/templates/mobile/_footer.php
+++ app/templates/mobile/_footer.php
@@ -51,11 +51,11 @@ endif;
       <li id="side-menu-service">
         <ul>
           <!-- Service Specific Menu -->
-          <li class="side-menu-ut-nav"><a class="side-menu-ut-nav-link side-menu-ut-nav-home" href="#"><i></i>スポーツブルトップへ</a></li>
-          <li class="side-menu-ut-nav"><a class="side-menu-ut-nav-link side-menu-ut-nav-mypage" href="#"><i></i>マイページ</a></li>
-          <li class="side-menu-ut-nav"><a class="side-menu-ut-nav-link side-menu-ut-nav-config" href="#"><i></i>設定</a></li>
-          <li class="side-menu-ut-nav"><a class="side-menu-ut-nav-link side-menu-ut-nav-logout" href="#"><i></i>ログアウト</a></li>
-          <li class="side-menu-ut-nav"><a class="side-menu-ut-nav-link side-menu-ut-nav-about" href="#"><i></i>スポーツブルとは</a></li>
+          <li class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav"><a class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav-link <?php echo $page['html_prefix']; ?>side-menu-ut-nav-home" href="#"><i></i>スポーツブルトップへ</a></li>
+          <li class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav"><a class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav-link <?php echo $page['html_prefix']; ?>side-menu-ut-nav-mypage" href="#"><i></i>マイページ</a></li>
+          <li class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav"><a class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav-link <?php echo $page['html_prefix']; ?>side-menu-ut-nav-config" href="#"><i></i>設定</a></li>
+          <li class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav"><a class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav-link <?php echo $page['html_prefix']; ?>side-menu-ut-nav-logout" href="#"><i></i>ログアウト</a></li>
+          <li class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav"><a class="<?php echo $page['html_prefix']; ?>side-menu-ut-nav-link <?php echo $page['html_prefix']; ?>side-menu-ut-nav-about" href="#"><i></i>スポーツブルとは</a></li>
         </ul>
       </li>
 
@@ -63,26 +63,26 @@ endif;
       <li>
           <ul>
               <?php foreach ($page['side-menu'] as $list) : ?>
-                  <li class="sidemenu-list-title"><?php echo $list['header'];?></li>
+                  <li class="<?php echo $page['html_prefix']; ?>sidemenu-list-title"><?php echo $list['header'];?></li>
                   <?php foreach($list['items'] as $side_item) : ?>
                       <li>
                           <?php if ($side_item['type'] === 1 && false === empty($side_item['caption'])):?>
-                              <div class="sidemenu_container">
+                              <div class="<?php echo $page['html_prefix']; ?>sidemenu_container">
                                   <a href="<?php echo $side_item['link']; ?>">
-                                      <img src="<?php echo $side_item['image']['sp']; ?>" class="sidemenu_icon">
-                                      <div class="sidemenu_title">
+                                      <img src="<?php echo $side_item['image']['sp']; ?>" class="<?php echo $page['html_prefix']; ?>sidemenu_icon">
+                                      <div class="<?php echo $page['html_prefix']; ?>sidemenu_title">
                                           <?php echo $side_item['title']; ?>
                                       </div>
-                                      <div class="sidemenu_caption">
+                                      <div class="<?php echo $page['html_prefix']; ?>sidemenu_caption">
                                           <?php echo $side_item['caption'];?>
                                       </div>
                                   </a>
                               </div>
                           <?php else: ?>
-                              <div class="sidemenu_container_single">
+                              <div class="<?php echo $page['html_prefix']; ?>sidemenu_container_single">
                                   <a href="<?php echo $side_item['link']; ?>">
-                                      <img src="<?php echo $side_item['image']['sp']; ?>" class="sidemenu_icon">
-                                      <div class="sidemenu_title_single">
+                                      <img src="<?php echo $side_item['image']['sp']; ?>" class="<?php echo $page['html_prefix']; ?>sidemenu_icon">
+                                      <div class="<?php echo $page['html_prefix']; ?>sidemenu_title_single">
                                           <?php echo $side_item['title']; ?>
                                       </div>
                                   </a>
