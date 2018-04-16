da1ba4d97744fe6a6c94680b890567e308bd4fd2
taniguchi <taniguchi@cloudpack.jp>
2018-04-16 18:24:48 +0900

    jsonプレビューの見た目整理
    (cherry picked from commit ff642fd)

--- public/api/editdm/tour_of_japan/get_toj_json.php
+++ public/api/editdm/tour_of_japan/get_toj_json.php
@@ -30,7 +30,24 @@ if (preg_match("/cms/",$servername) ||
         $res = 'ファイルが存在しません';
         echo $res;
       } else {
+        $json =<<<_EOD
+{
+    lastupdate: {$res->lastupdate}
+    live: 
+        alt: 
+            large: {$res->live->alt->large}
+            medium: {$res->live->alt->medium}
+        error:
+            large: {$res->live->error->large}
+            medium: {$res->live->error->medium}
+        interval: {$res->live->interval}
+        isPlaying: {$res->live->isPlaying}
+        video:
+            id: {$res->live->video->id}
+}        
+_EOD;
 
+        echo $json;
       }
 }
 else
