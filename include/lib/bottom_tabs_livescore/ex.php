9e4d8ff134030f81c72027bc5b47517ee9a1733d
kinjo_stg <kinjo_stg@iret.co.jp>
2018-04-16 11:42:05 +0900

    競技、速報の親タブ一覧からの新規登録、編集画面から親選択のラジオボタンを削除

--- include/lib/bottom_tabs_livescore/ex.php
+++ include/lib/bottom_tabs_livescore/ex.php
@@ -1,6 +1,5 @@
 <?php
 
-$a[]=array("parent","63","rb",6,"","","","","","","","category_id");
 $a[] = array("textfield", "名称", "name", "120");
 $a[] = array("textfield", "リンク", "link", "120");
 $a[] = array("textfield", "アイコン(PC)", "icon", "120");
