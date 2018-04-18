42909f849d30d7edf9ae62cd9c4e41419912f228
taniguchi <taniguchi@cloudpack.jp>
2018-04-18 16:00:36 +0900

    高校野球を全部野球にかえるバッチ

--- public/api/batch/update_category_baseball.php
+++ public/api/batch/update_category_baseball.php
@@ -10,6 +10,20 @@ $o->query($sql);
 
 $e=$o->affected_rows2();
 
-echo $e;
+echo "m1:".$e."/";
+
+$sql=sprintf("update repo_n set m2 = 113 where m2 = 136");
+$o->query($sql);
+
+$e=$o->affected_rows2();
+
+echo "m2:".$e."/";
+
+$sql=sprintf("update repo_n set m2 = null where m1 = 113 and m2 = 113");
+$o->query($sql);
+
+$e=$o->affected_rows2();
+
+echo "重複削除:".$e."/";
 
 ?>
