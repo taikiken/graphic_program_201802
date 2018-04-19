46e0a8d788d2f3f31b16b9ca8cbf9257b82e46af
taikiken <denkosekka@gmail.com>
2018-04-19 12:44:21 +0900

    vk main file 作成 - empty UNDO_SPBL-509

---
+++ _src/babels/src/vk_spbl_header.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2011-2018 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2018/04/19 - 12:41
+ * buildTime: @@buildTime
+ * @license MIT
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ * @requires React, Sagen.js. es5-promise
+ */
+'use strict';
+
+// -----------------------------------------------
+// VK 用の header 機能を提供します
+// -----------------------------------------------
