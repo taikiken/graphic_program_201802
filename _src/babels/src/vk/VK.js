0b94e3e549d0ad45bbe0ea2508a91b06c2472c1d
taikiken <denkosekka@gmail.com>
2018-04-19 19:21:47 +0900

    設定ファイル追加

---
+++ _src/babels/src/vk/VK.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2011-2018 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2018/04/19 - 19:20
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ *
+ */
+
+/**
+ * VK - 設定ファイル
+ */
+export default class VK {
+  /**
+   * vk class prefix
+   * @returns {string} `SPBL_` prefix で使用します
+   */
+  static get PREFIX() {
+    return 'SPBL_';
+  }
+}
