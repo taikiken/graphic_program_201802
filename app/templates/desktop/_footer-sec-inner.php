16113125258ced50de09c7d032b4d66b828d582e
Yoshiaki Fujimori <info@2244.jp>
2018-04-20 19:16:49 +0900

    parts - パーツのdesktop分のリンク/画像パスにhostつける

--- app/templates/desktop/_footer-sec-inner.php
+++ app/templates/desktop/_footer-sec-inner.php
@@ -4,13 +4,13 @@
   include_once __DIR__."/../_breadcrumb.php"; ?>
   <div class="<?php echo $page['html_prefix']; ?>foot-pr">
     <div class="<?php echo $page['html_prefix']; ?>foot-pr-inner">
-      <figure><img src="/assets/images/common/footer-overview-figure.png" alt=""></figure>
+      <figure><img src="<?php echo $page['site_url_uts']; ?>/assets/images/common/footer-overview-figure.png" alt=""></figure>
 
       <div class="<?php echo $page['html_prefix']; ?>text-block">
-        <h3 class="<?php echo $page['html_prefix']; ?>foot-pr-logo"><img src="/assets/images/common/footer-overview-logo.png" alt="SPORTS BULL アプリ版(iPhone/Android)"></h3>
+        <h3 class="<?php echo $page['html_prefix']; ?>foot-pr-logo"><img src="<?php echo $page['site_url_uts']; ?>/assets/images/common/footer-overview-logo.png" alt="SPORTS BULL アプリ版(iPhone/Android)"></h3>
         <ul class="<?php echo $page['html_prefix']; ?>foot-pr-btn">
-          <li><a href="https://itunes.apple.com/jp/app/undotsushin/id1086719653?l=ja&ls=1&mt=8" target="_blank"><img src="/assets/images/common/footer-overview-btn-applestore.png" alt="App Store" /></a></li>
-          <li><a href="https://play.google.com/store/apps/details?id=com.undotsushin" target="_blank"><img src="/assets/images/common/footer-overview-btn-googleplay.png" alt="Google play"></a></li>
+          <li><a href="https://itunes.apple.com/jp/app/undotsushin/id1086719653?l=ja&ls=1&mt=8" target="_blank"><img src="<?php echo $page['site_url_uts']; ?>/assets/images/common/footer-overview-btn-applestore.png" alt="App Store" /></a></li>
+          <li><a href="https://play.google.com/store/apps/details?id=com.undotsushin" target="_blank"><img src="<?php echo $page['site_url_uts']; ?>/assets/images/common/footer-overview-btn-googleplay.png" alt="Google play"></a></li>
         </ul>
         <p class="<?php echo $page['html_prefix']; ?>foot-pr-text">話題のスポーツニュースがサクサク読める、無料のニュースまとめアプリ「スポーツ・ブル」。高品質なスポーツのニュース、動画をいつでもお楽しみ頂けます。スマートフォンアプリをダウンロードして今日のニュースをチェックしましょう。</p>
       </div><!-- /.text-block -->
@@ -21,17 +21,17 @@
     </div><!-- /.foot-pr-inner -->
   </div><!-- /.foot-pr -->
 
-  <div id="pageTop" class="<?php echo $page['html_prefix']; ?>pagetop fixed blind"><a href="#"><span>このページの先頭へ<img src="/assets/images/sprite/sprite-footer.png" role="presentation"></span></a></div>
+  <div id="pageTop" class="<?php echo $page['html_prefix']; ?>pagetop fixed blind"><a href="#"><span>このページの先頭へ<img src="<?php echo $page['site_url_uts']; ?>/assets/images/sprite/sprite-footer.png" role="presentation"></span></a></div>
 
   <div class="<?php echo $page['html_prefix']; ?>fnav-block">
     <nav class="<?php echo $page['html_prefix']; ?>fnav">
-      <h3 class="<?php echo $page['html_prefix']; ?>fnav-logo"><img src="/assets/images/common/footer-fnav-logo.png" alt="SPORTS BULL"></h3>
+      <h3 class="<?php echo $page['html_prefix']; ?>fnav-logo"><img src="<?php echo $page['site_url_uts']; ?>/assets/images/common/footer-fnav-logo.png" alt="SPORTS BULL"></h3>
       <ul>
         <?php // <li><a href="/about/ads/">広告掲載について</a></li> ?>
-        <li><a href="/about/">サービス紹介</a></li>
-        <li><a href="/about/company/">会社概要</a></li>
-        <li><a href="/about/privacy/">プライバシーポリシー</a></li>
-        <li><a href="/about/terms/">利用規約</a></li>
+        <li><a href="<?php echo $page['site_url_uts']; ?>/about/">サービス紹介</a></li>
+        <li><a href="<?php echo $page['site_url_uts']; ?>/about/company/">会社概要</a></li>
+        <li><a href="<?php echo $page['site_url_uts']; ?>/about/privacy/">プライバシーポリシー</a></li>
+        <li><a href="<?php echo $page['site_url_uts']; ?>/about/terms/">利用規約</a></li>
         <?php // <li><a href="/about/faq/">FAQ</a></li> ?>
         <?php // <li><a href="/about/contact/">お問い合せ</a></li> ?>
       </ul>
