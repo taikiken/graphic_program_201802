09f5b912eb9de859dfcd0a0aee0d62dfd49f3cb9
Yoshiaki Fujimori <info@2244.jp>
2018-04-20 19:50:10 +0900

    parts - prefixつけもれ

--- app/templates/desktop/_header.php
+++ app/templates/desktop/_header.php
@@ -81,16 +81,16 @@ endif;
 ?>
 
 <?php if ( $page['conditional']['header'] ) : ?>
-  <header id="header-container" class="SPBL_common <?php echo $page['html_prefix']; ?>head-sec">
+  <header id="<?php echo $page['html_prefix']; ?>header-container" class="SPBL_common <?php echo $page['html_prefix']; ?>head-sec">
     <div class="<?php echo $page['html_prefix']; ?>head-sec-inner">
       <aside class="<?php echo $page['html_prefix']; ?>head-sec-left">
-        <div id="head-search-container"></div><!-- /.head-search -->
+        <div id="<?php echo $page['html_prefix']; ?>head-search-container"></div><!-- /.head-search -->
       </aside>
 
       <h1><a href="<?php echo $page['site_url_uts']; ?>/">スポーツブル（スポブル）</a></h1>
 
       <aside class="<?php echo $page['html_prefix']; ?>head-sec-right">
-        <div id="user-profile-container"></div><!--/.user-profile-container-->
+        <div id="<?php echo $page['html_prefix']; ?>user-profile-container"></div><!--/.user-profile-container-->
       </aside>
     </div><!-- /.head-sec-inner -->
   </header><!-- /.head-sec -->
@@ -98,9 +98,9 @@ endif;
 
 
 <?php if ( $page['conditional']['gnav'] ) : ?>
-  <nav id="global-nav-container" class="SPBL_common <?php echo $page['html_prefix']; ?>gnav-sec">
+  <nav id="<?php echo $page['html_prefix']; ?>global-nav-container" class="SPBL_common <?php echo $page['html_prefix']; ?>gnav-sec">
     <ul>
-      <li id="home"><a href="<?php echo $page['site_url_uts']; ?>/">TOP</a></li>
+      <li id="<?php echo $page['html_prefix']; ?>home"><a href="<?php echo $page['site_url_uts']; ?>/">TOP</a></li>
 
       <?php foreach( $page['site_tabs'] as $tab ) {
         // https://github.com/undotsushin/undotsushin/issues/645#issuecomment-224162616
