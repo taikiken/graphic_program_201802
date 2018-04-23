caf90b51fdf7a41d6e3bf274945594edbedb10b1
Yoshiaki Fujimori <info@2244.jp>
2018-04-23 10:49:19 +0900

    vk - vk_script.php にfacebook like box用のコード追加

--- app/templates/_footer_vk_script.php
+++ app/templates/_footer_vk_script.php
@@ -18,3 +18,4 @@
 ?>
 <script id="SPBL_vk-header_script" src="/assets/js/vk_spbl_header.bundle.js?v=<?php echo $page['version']; ?>" data-browser="true" data-prefix="SPBL_" data-domain="https://dev.sportsbull.jp"></script>
 
+<script src="<?php echo $page['site_url_uts']; ?>/assets/js/fb-video.js?v=<?php echo $page['version']; ?>"></script>
