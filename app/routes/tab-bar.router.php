ca470946f4820e519a3315054d2913411c64aa48
kinjo_stg <kinjo_stg@iret.co.jp>
2018-04-26 13:01:34 +0900

    競技、速報のURL変更

--- app/routes/tab-bar.router.php
+++ app/routes/tab-bar.router.php
@@ -3,11 +3,11 @@
  * Date: 2018/04/12
  * Time: 15:12
  */
-$app->group('/tab-bar', function () use($app) {
+$app->group('/nav', function () use($app) {
 
   // 速報 - `livescore`
   // ==============================
-  $this->get('/livescore[/]', function ($request, $response, $args) use ($app) {
+  $this->get('/stats[/]', function ($request, $response, $args) use ($app) {
     // /api/v1/bottomtab
     $tab_data = @file_get_contents($app->model->property('file_get_url') . '/api/v1/bottomtab/');
     $tab_response = json_decode($tab_data, true)['response'];
