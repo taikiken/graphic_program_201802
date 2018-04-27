c9a1d97b3535078e11a2f17410a52dfe84309993
Yoshiaki Fujimori <info@2244.jp>
2018-04-27 16:51:51 +0900

    vk - vk scriptのprefix, domain を自ホスト参照にする

--- app/templates/_footer_vk_script.php
+++ app/templates/_footer_vk_script.php
@@ -16,6 +16,6 @@
 
 */
 ?>
-<script id="SPBL_vk-header_script" src="/assets/js/vk_spbl_header.bundle.js?v=<?php echo $page['version']; ?>" data-browser="true" data-prefix="SPBL_" data-domain="https://dev.sportsbull.jp"></script>
+<script id="<?php echo $page['html_prefix']; ?>vk-header_script" src="/assets/js/vk_spbl_header.bundle.js?v=<?php echo $page['version']; ?>" data-browser="true" data-prefix="<?php echo $page['html_prefix']; ?>" data-domain="<?php echo $page['site_url_uts']; ?>"></script>
 
 <script src="<?php echo $page['site_url_uts']; ?>/assets/js/fb-video.js?v=<?php echo $page['version']; ?>"></script>
