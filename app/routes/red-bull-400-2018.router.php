71b5323b0ac882d8cbcc2485921fb61f7c7fcc6f
adachi <yuta.adachi@sportsbull.jp>
2018-05-10 13:46:41 +0900

    description修正

--- app/routes/red-bull-400-2018.router.php
+++ app/routes/red-bull-400-2018.router.php
@@ -10,7 +10,7 @@ $app->group('/red-bull-400-2018',  function () use($app) {
     'og_title'           => 'RED BULL 400 ライブ配信 | '.$app->model->property('title'),
     'og_url'             => $app->model->property('site_url').'red-bull-400-2018/',
     'og_image'           => $app->model->property('site_url').'assets/images/red-bull-400-2018/ogp.png',
-    'og_description'     => 'RED BULL 400をスポーツブルで無料ライブ配信。スポーツブル(スポブル)は、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
+    'og_description'     => 'RED BULL 400をスポーツブルで無料ライブ配信。ブルくんも挑戦！？スポーツブル(スポブル)は、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
     'keywords'           => 'RED BULL,レッドブル,400,スポブル,ライブ配信,スポーツ,メディア,クレイジー,アスリート,ニュース,動画,sports,media,crazy',
     'category'           => $category,
     'template'           => 'category',
