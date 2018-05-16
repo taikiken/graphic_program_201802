5a0f1abe0e752102744c4cf6884f9b0cf1d2f4b2
adachi <yuta.adachi@sportsbull.jp>
2018-05-16 18:49:17 +0900

    2017年秋シーズンのアーカイブページ追加

---
+++ app/templates/stats/ua_kansai/2017a.php
@@ -0,0 +1,251 @@
+<?php
+include __DIR__."/getData.php";
+$dataArray = getData::getSchedule();
+?>
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+  <meta charset="UTF-8">
+  <meta http-equiv="pragma" content="no-cache">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <script src="/assets/js/libs/sagen/sagen.min.js" id="sagen" data-browser="true"></script>
+  <script src="/assets/js/app_divide.bundle.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+  <title>test 日程・結果 - 関西学生アメリカンフットボールリーグ | 速報 &amp; データ | スポーツブル (スポブル)</title>
+  <script src="/assets/js/libs/vendor.react.js"></script>
+  <script src="/assets/js/bundle/main.bundle.js"></script>
+
+  <meta name="keywords" content="関西学生アメリカンフットボールリーグ, 関西アメフト, 日程結果, スポーツ, メディア, ニュース, 動画, sports, media">
+  <meta name="description" content="関西学生アメリカンフットボールリーグ 試合速報&データ見るならスポーツブル(スポブル)で。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。">
+
+  <meta property="fb:app_id" content="842032129256034">
+  <meta property="og:site_name" content="スポーツブル / SPORTS BULL">
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="日程・結果 - 関西学生アメリカンフットボールリーグ | 速報 & データ | スポーツブル (スポブル)">
+  <meta property="og:image" content="https://sportsbull.jp/assets/stats/ua_kansai/images/ogp.jpg">
+  <meta property="og:url" content="https://sportsbull.jp/stats/ua_kansai/">
+  <meta property="og:description" content="関西学生アメリカンフットボールリーグ 試合速報&データ見るならスポーツブル(スポブル)で。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。">
+  <meta property="og:locale" content="ja_JP" />
+
+  <!-- twitter card -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@sportsbull_jp">
+  <meta name="twitter:title" content="日程・結果 - 関西学生アメリカンフットボールリーグ | 速報 & データ | スポーツブル (スポブル)">
+  <meta name="twitter:image" content="https://sportsbull.jp/assets/stats/ua_kansai/images/ogp.jpg">
+  <meta name="twitter:url" content="https://sportsbull.jp/stats/ua_kansai/">
+  <meta name="twitter:description" content="関西学生アメリカンフットボールリーグ 試合速報&データ見るならスポーツブル(スポブル)で。スポーツブルは、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。">
+
+  <?php include_once __DIR__.'/_head.php'; ?>
+
+</head>
+<body>
+
+<!-- ad/531683568/appvador -->
+<div id='div-gpt-ad-1501126889988-0'>
+<style>#div-gpt-ad-1501126889988-0 * { line-height:0; }</style>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1501126889988-0'); });
+</script>
+<script type="text/javascript" src="//cdn.apvdr.com/js/apv-ifbstr.min.js"></script>
+</div>
+<!-- ad/531683568/appvador -->
+
+<div id="whole" class="whole">
+
+  <header class="head-sec">
+    <div class="head-sec-inner">
+      <h1><a href="/">SPORTS BULL</a></h1>
+    </div><!-- /.head-sec-inner -->
+  </header><!-- /.head-sec -->
+
+  <div class="body-sec">
+    <div class="body-sec-inner">
+
+      <section class="main-sec" style="margin-top: 0;">
+
+      <div class="stats__header mb30">
+        <h1 class="stats__header__title"><img class="responsive" src="/assets/stats/ua_kansai/images/heading.jpg" alt="関西アメフト 速報&スタッツ" /></h1>
+      </div><!-- /.stats__header -->
+
+      <div class="stats__banner mb30">
+        <div id="ua-pc-big-banner" style='height:90px; width:728px;'>
+          <!-- /531683568/kansai-amefoot-ad/ua-pc-big-banner -->
+          <script>
+            googletag.cmd.push(function() {
+              googletag.defineSlot('/531683568/kansai-amefoot-ad/ua-pc-big-banner', [728, 90], 'div-gpt-ad-1504503001230-0').addService(googletag.pubads());
+              googletag.pubads().enableSingleRequest();
+              googletag.enableServices();
+            });
+          </script>
+          <div id='div-gpt-ad-1504503001230-0'>
+          <script>
+          googletag.cmd.push(function() { googletag.display('div-gpt-ad-1504503001230-0'); });
+          </script>
+          </div>
+          <!-- // /531683568/kansai-amefoot-ad/ua-pc-big-banner -->
+        </div>
+
+        <div id="ua-sp-big-banner" style='height:50px; width:320px;'>
+          <!-- /531683568/kansai-amefoot-ad/ua-sp-big-banner -->
+          <script>
+            googletag.cmd.push(function() {
+              googletag.defineSlot('/531683568/kansai-amefoot-ad/ua-sp-big-banner', [320, 50], 'div-gpt-ad-1504503049105-0').addService(googletag.pubads());
+              googletag.pubads().enableSingleRequest();
+              googletag.enableServices();
+            });
+          </script>
+          <div id='div-gpt-ad-1504503049105-0'>
+          <script>
+          googletag.cmd.push(function() { googletag.display('div-gpt-ad-1504503049105-0'); });
+          </script>
+          </div>
+          <!-- // /531683568/kansai-amefoot-ad/ua-sp-big-banner -->
+        </div>
+
+        <div id="ua-app-big-banner" style='height:50px; width:320px;'>
+          <!-- /531683568/kansai-amefoot-ad/ua-app-big-banner -->
+          <script>
+            googletag.cmd.push(function() {
+              googletag.defineSlot('/531683568/kansai-amefoot-ad/ua-app-big-banner', [320, 50], 'div-gpt-ad-1504503090397-0').addService(googletag.pubads());
+              googletag.pubads().enableSingleRequest();
+              googletag.enableServices();
+            });
+          </script>
+          <div id='div-gpt-ad-1504503090397-0'>
+          <script>
+          googletag.cmd.push(function() { googletag.display('div-gpt-ad-1504503090397-0'); });
+          </script>
+          </div>
+          <!-- // /531683568/kansai-amefoot-ad/ua-app-big-banner -->
+        </div>
+      </div><!-- /.stats__banner -->
+
+      <nav id="game-category" class="active-0">
+        <ul>
+          <li>公式戦</li>
+          <li>入替戦</li>
+          <li>全日本</li>
+          <li>甲子園ボウル</li>
+        </ul>
+      </nav>
+      <div class="game-category1">
+        <div class="af-schedule">
+            <h2 class="calendar">日程・結果 / Division1</h2>
+            <div class="lastupdate">最終更新日：<?=$dataArray["lastupdate"]?></div>
+            <?=$dataArray["game-category1"]["schedule"]?>
+        </div><!-- af-schedule -->
+
+        <div class="af-match">
+          <?=$dataArray["game-category1"]["standing"]?>
+        </div><!-- af-schedule -->
+      </div><!-- game-category -->
+      <div class="game-category2">
+        <div class="af-schedule">
+            <h2 class="calendar">日程・結果 / 入替戦</h2>
+            <?=$dataArray["game-category2"]["schedule"]?>
+        </div>
+      </div><!-- game-category2 -->
+      <div class="game-category3">
+        <div class="af-schedule">
+            <h2 class="calendar">日程・結果 / 全日本</h2>
+            <?=$dataArray["game-category3"]["schedule"]?>
+        </div>
+      </div><!-- game-category3 -->
+      <div class="game-category4">
+        <div class="af-schedule">
+            <h2 class="calendar">日程・結果 / 甲子園ボウル</h2>
+            <?=$dataArray["game-category4"]["schedule"]?>
+        </div>
+      </div><!-- game-category4 -->
+
+
+      </section><!-- /.main-sec -->
+
+      <section class="side-sec show-for-large">
+        <div id="sidebar-moving-container">
+
+          <div class="sponsor-link">
+            <!-- /531683568/kansai-amefoot-ad/ua-pc-sidebar-rectabgle -->
+            <script>
+              googletag.cmd.push(function() {
+                googletag.defineSlot('/531683568/kansai-amefoot-ad/ua-pc-sidebar-rectabgle', [300, 250], 'div-gpt-ad-1510649945193-0').addService(googletag.pubads());
+                googletag.pubads().enableSingleRequest();
+                googletag.enableServices();
+              });
+            </script>
+            <div id='div-gpt-ad-1510649945193-0' style='height:250px; width:300px;'>
+            <script>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1510649945193-0'); });
+            </script>
+            </div>
+            <!-- // /531683568/kansai-amefoot-ad/ua-pc-sidebar-rectabgle -->
+          </div>
+
+          <div class="app-bnr">
+            <!-- ad/531683568/pc_sidebar_top_2nd -->
+            <script>
+              googletag.cmd.push(function() {
+                googletag.defineSlot('/531683568/pc_sidebar_top_2nd', [300, 120], 'div-gpt-ad-1494939250039-0').addService(googletag.pubads());
+                googletag.pubads().enableSingleRequest();
+                googletag.pubads().collapseEmptyDivs();
+                googletag.enableServices();
+              });
+            </script>
+            <div id='div-gpt-ad-1494939250039-0' style='height:120px; width:300px;'>
+            <script>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1494939250039-0'); });
+            </script>
+            </div>
+            <!-- // ad/531683568/pc_sidebar_top_2nd -->
+          </div>
+          <div class="mt30">
+            <a href="http://amefootlive.jp/kcafl" target="_blank"><img src="/assets/stats/ua_kansai/images/amefootlive_bnr.jpg" alt="関西学生 アメリカンフットボールリーグLIVE!!" width="300px"></a>
+          </div>
+          <!-- sidebar recommend, オススメ記事 -->
+          <div id="widget-recommend-list-container"></div><!--/recommend-->
+
+          <div id="widget-ranking-container"></div><!--/ranking-->
+
+          <div id="sponsor-link-ranking" class="sponsor-link sponsor-link-ranking">
+            <script src="https://ssl.socdm.com/sdk/js/adg-script-loader.js?id=35250&targetID=adg_35250&displayid=2&adType=PC&width=0&height=0&sdkType=3&async=true&tagver=2.0.0"></script>
+          </div>
+
+          <div id="widget-recommend-container"></div><!--/videos-->
+          <div id="sponsor-link-recommend" class="sponsor-link sponsor-link-recommend">
+            <script src="https://ssl.socdm.com/sdk/js/adg-script-loader.js?id=35251&targetID=adg_35251&displayid=2&adType=PC&width=0&height=0&sdkType=3&async=true&tagver=2.0.0"></script>
+          </div>
+
+        </div><!--/#sidebar-moving-->
+      </section>
+    </div>
+  </div><!-- /.body-sec -->
+
+<?php
+// # footer
+// ==============================
+
+  $BREADCRUMB = array(
+    array(
+      'label' => '関西アメフト 日程・結果',
+      'path'  => '/stats/ua_kansai/'
+    ),
+  );
+
+  // footer dom
+  include_once __DIR__.'/../../_footer-responsive.php';
+?>
+
+</div><!-- /.whole -->
+
+<!-- for facebook -->
+<script src="/assets/facebook/init.js?v=<?php echo $page['version']; ?>"></script>
+<!-- // for facebook -->
+
+<script src="/assets/js/libs/jquery2/jquery.min.js"></script>
+<script src="/assets/stats/ua_kansai/js/userAgent.js"></script>
+<script src="/assets/js/global.bundle.js"></script>
+<script src="/assets/stats/ua_kansai/js/amefoot.bundle.js"></script>
+<script src="/assets/stats/ua_kansai/js/index.js"></script>
+<script src="/assets/popup/js/banner_popup_app.bundle.js?v=<?php echo $page['version']; ?>"></script>
+</body>
+</html>
