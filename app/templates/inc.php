d9078bae705e126176cac4f3586fbfb605b2bc18
Yoshiaki Fujimori <info@2244.jp>
2018-05-17 20:00:56 +0900

    parts - 確認用ページのコメント更新

--- app/templates/inc.php
+++ app/templates/inc.php
@@ -3,7 +3,10 @@
 include_once __DIR__."/${page['ua']}/_header.php";
 
 if ( $page['parts'] === 'all' ) :
-  echo "\n".'<div style="height: 800px;"></div>'."\n";
+  echo "\n".'<!-- [contents] -->'."\n";
+  echo '<!-- こちらにページ内容を記述してください *下のdivは不要です -->'."\n";
+  echo '<div style="height: 800px;"></div>'."\n";
+  echo '<!-- //[contents] -->'."\n\n\n\n\n";
 endif;
 
 include_once __DIR__."/${page['ua']}/_footer.php";
