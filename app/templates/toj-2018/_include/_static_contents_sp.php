7f348d2fecf4ef776648e5c8bd422744df40184e
adachi <yuta.adachi@sportsbull.jp>
2018-05-26 21:15:20 +0900

    TOJ@伊豆 終了対応

--- app/templates/toj-2018/_include/_static_contents_sp.php
+++ app/templates/toj-2018/_include/_static_contents_sp.php
@@ -213,7 +213,7 @@
               </div>
             </section>
             <!-- 第7ステージ 伊豆 -->
-            <section class="schedule__unit is-open">
+            <section class="schedule__unit">
               <header class="schedule__header">
                 <p class="date">
                   <picture>
@@ -226,8 +226,8 @@
                   <div class="time"><strong>8:55</strong> 配信開始</div>
                 </div>
                 <div class="buttonBlock">
-                  <p class="button__fulltime  is-inactive"><span class="button__linkNone">フルタイム動画</span></p>
-                  <p class="button__highlight is-inactive"><span class="button__linkNone">ハイライト動画</span></p>
+                  <p class="button__fulltime"><a href="https://sportsbull.jp/p/322092/">フルタイム動画</a></p>
+                  <p class="button__highlight"><a href="https://sportsbull.jp/p/322091/">ハイライト動画</a></p>
                 </div>
               </header>
               <div class="schedule__detail">
@@ -242,7 +242,7 @@
               </div>
             </section>
             <!-- 第8ステージ 東京 -->
-            <section class="schedule__unit">
+            <section class="schedule__unit is-open">
               <header class="schedule__header">
                 <p class="date">
                   <picture>
