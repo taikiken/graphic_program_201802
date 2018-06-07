b6ca1ee42ea4c855be080e3148327c0dc07ff485
adachi <yuta.adachi@sportsbull.jp>
2018-06-07 12:09:50 +0900

    description修正

--- app/routes/uci-mtb-world-cup-2018.router.php
+++ app/routes/uci-mtb-world-cup-2018.router.php
@@ -10,7 +10,7 @@ $app->group('/live/uci-mtb-world-cup-2018',  function () use($app) {
     'og_title'           => 'UCI MOUNTAIN BIKE WORLD CUP 2018 ライブ配信 | '.$app->model->property('title'),
     'og_url'             => $app->model->property('site_url').'uci-mtb-world-cup-2018/',
     'og_image'           => $app->model->property('site_url').'assets/images/live/uci-mtb-world-cup-2018/ogp.png',
-    'og_description'     => 'UCI MOUNTAIN BIKE WORLD CUP 2018をスポーツブルで無料ライブ配信。ブルくんも挑戦！？スポーツブル(スポブル)は、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
+    'og_description'     => 'UCI MOUNTAIN BIKE WORLD CUP 2018をスポーツブルで無料ライブ配信。スポーツブル(スポブル)は、インターネットスポーツメディアです。数十社の良質なスポーツ媒体と連携し、話題のスポーツニュース記事、動画をいち早くお届けします。また、ここでしか見ることの出来ないオリジナル記事や、番組を配信しています。スマートフォンはもちろん、PC、タブレットでもお楽しみいただけます。',
     'keywords'           => 'Red Bull,レッドブル,MTB,UCI,ダウンヒル,スポブル,ライブ配信,スポーツ,メディア,クレイジー,アスリート,ニュース,動画,sports,media,crazy',
     'category'           => $category,
     'template'           => 'category',
