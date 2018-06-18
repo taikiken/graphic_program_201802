ce381f4777474b1cff27e42ac88cb519fe9cd22b
Yoshiaki Fujimori <info@2244.jp>
2018-06-18 15:34:23 +0900

    inhightv - 旧 /inhigh/ は、/inhightv/ に転送

--- app/routes/inhigh.router.php
+++ app/routes/inhigh.router.php
@@ -8,70 +8,25 @@ $app->group('/{slug:inhigh}',  function () use($app) {
   // /inhigh/
   // ==============================
   $this->map(['GET'], '[/]', function ($request, $response, $args) use ($app) {
-    $args['page'] = $app->model->set(array(
-      'title'              => 'インハイ.tv',
-      'og_title'           => 'インハイ.tv | '.$app->model->property('title'),
-      'og_url'             => $app->model->property('site_url').'inhigh/',
-      'path'               => $args,
-      'template'           => 'category',
-      'template_classname' => '',
-    ));
-
-    return $this->renderer->render($response, 'inhigh/index.php', $args);
-
+    return $response->withRedirect('https://inhightv.sportsbull.jp/', 301);
   });
-
 
   // photo
   // ==============================
   $this->get('/photo[/]', function ($request, $response, $args) use ($app) {
-
-    $args['page'] = $app->model->set(array(
-      'title'              => 'インハイ.tv - フォト',
-      'og_title'           => 'インハイ.tv - フォト |'.$app->model->property('title'),
-      'og_url'             => $app->model->property('site_url').'inhigh/photo/',
-      'path'               => $args,
-      'template'           => 'category',
-      'template_classname' => '',
-    ));
-
-    return $this->renderer->render($response, 'inhigh/photo.php', $args);
-
+    return $response->withRedirect('/inhightv/photo/', 301);
   });
-
 
   // highlight
   // ==============================
   $this->get('/highlight[/]', function ($request, $response, $args) use ($app) {
-
-    $args['page'] = $app->model->set(array(
-      'title'              => 'インハイ.tv - ハイライト',
-      'og_title'           => 'インハイ.tv - ハイライト| '.$app->model->property('title'),
-      'og_url'             => $app->model->property('site_url').'inhigh/highlight/',
-      'path'               => $args,
-      'template'           => 'category',
-      'template_classname' => '',
-    ));
-
-    return $this->renderer->render($response, 'inhigh/highlight.php', $args);
-
+    return $response->withRedirect('/inhightv/highlight/', 301);
   });
-
 
   // webview
   // ==============================
   $this->get('/webview[/]', function ($request, $response, $args) use ($app) {
-
-    $args['page'] = $app->model->set(array(
-      'title'              => 'インハイ.tv',
-      'og_title'           => 'インハイ.tv | '.$app->model->property('title'),
-      'path'               => $args,
-      'template'           => 'webview',
-      'template_classname' => '',
-    ));
-
-    return $this->renderer->render($response, 'inhigh/webview.php', $args);
-
+    return $response->withRedirect('/inhightv/webview/', 301);
   });
 
 });
