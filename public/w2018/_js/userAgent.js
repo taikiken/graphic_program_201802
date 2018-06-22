e193d14b074a7dfaf01b39a83d264140b41f4b2d
adachi <yuta.adachi@sportsbull.jp>
2018-06-22 11:38:35 +0900

    修正

--- public/w2018/_js/userAgent.js
+++ public/w2018/_js/userAgent.js
@@ -1,25 +1,37 @@
+// (function() {
+// 	var agent = navigator.userAgent;
+// 	console.log(agent);
+// 	var url = window.location.href ;
+// 	if ( !agent.match(/undotsushin-ios/) || !agent.match(/undotsushin-android/)) {
+// 		var elm = document.querySelector('header.head-sec');
+// 		if (url.match(/sokuhou/)) {//sokuhou
+// 			elm = document.querySelector('header .pageTitle');
+// 			elm.classList.remove("hide");
+// 		}else{//standings,match
+// 			elm.classList.remove("hide");
+// 		}
+// 		elm.classList.remove("hide");
+// 		elm = document.querySelector('#footer');
+// 		elm.classList.remove("hide");
+// 	}else{
+// 		if (url.match(/sokuhou/)) {//sokuhou
+// 			var elm = document.querySelector('body');
+// 			elm.style.marginTop = "-45px";
+// 			elm.style.backgroundPosition = "center 0";
+// 		}
+// 	}
+
 (function() {
 	var agent = navigator.userAgent;
-	var url = window.location.href ;
-	if ( !agent.match(/undotsushin-ios/) && !agent.match(/undotsushin-android/)) {
-		// var elm = document.querySelector('header.head-sec');
-		// if (url.match(/sokuhou/)) {//sokuhou
-		// 	elm = document.querySelector('header .pageTitle');
-		// 	elm.classList.remove("hide");
-		// }else{//standings,match
-		// 	elm.classList.remove("hide");
-		// }
-		// elm.classList.remove("hide");
-		// elm = document.querySelector('#footer');
-		// elm.classList.remove("hide");
-	}else{
-		// if (url.match(/sokuhou/)) {//sokuhou
-		// 	var elm = document.querySelector('body');
-		// 	elm.style.marginTop = "-45px";
-		// 	elm.style.backgroundPosition = "center 0";
-		// }
+	console.log(agent);
+	if ( agent.match(/undotsushin-ios/) || agent.match(/undotsushin-android/)) {
+		$('.hide-app').hide();
+		$('.show-app').show();
 	}
-	
+
+
+
+
 }());
 var ua = navigator.userAgent;
 if (ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
