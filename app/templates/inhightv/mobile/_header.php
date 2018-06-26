ee52dfe7462fcebe0e6d2725d02f6a8adf6d8827
Yoshiaki Fujimori <info@2244.jp>
2018-06-26 15:47:54 +0900

    inhightv - アドセンス非表示

--- app/templates/inhightv/mobile/_header.php
+++ app/templates/inhightv/mobile/_header.php
@@ -4,14 +4,3 @@
   <link rel="stylesheet" href="/assets/css/inhigh/sp_interhigh.css?v=<?php echo $page['version']; ?>">
 
   <script src="/assets/facebook/init.js?v=<?php echo $page['version']; ?>"></script>
-  <script src="/assets/js/inhigh.bundle.js?v=<?php echo $page['version']; ?>"></script>
-
-  <!-- ad/npb-sp-anchor -->
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-8613117509675807",
-          enable_page_level_ads: true
-      });
-  </script>
-  <!-- // ad/npb-sp-anchor -->
